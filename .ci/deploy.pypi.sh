a707eb6fa5365daf6bd1c64074f385dc970f863b
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2016-02-18 11:32:18 +0100

    .ci: Add PyPI deployment script

---
+++ .ci/deploy.pypi.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Do not use `set -x` here as then it displays the PYPIPW in logs
+set -e
+
+# Get environment variables, readily decrypted by rultor
+source ../rultor_secrets.sh
+
+# Ship it!
+echo "Uploading coala to pypi"
+pip3 install twine wheel
+python3 setup.py sdist bdist_wheel
+twine upload dist/* -u "$PYPIUSER" -p "$PYPIPW"
