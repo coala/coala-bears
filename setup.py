281d3761741ffe8626a0bc83a02153b312c312fd
AbdealiJK <abdealikothari@gmail.com>
2016-02-19 10:30:20 +0530

    coala-bears: Add minimal setup.py
    This is a minimal setup.py which just has basic requirements.
    
    Fixes https://github.com/coala-analyzer/coala-bears/issues/7

--- setup.py
+++ setup.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python3
 
+import locale
+import sys
+
 # Start ignoring PyImportSortBear as imports below may yield syntax errors
-from coalib import assert_supported_version, VERSION, get_version, BUS_NAME
+from bears import assert_supported_version
 
 assert_supported_version()
 # Stop ignoring
 
-import datetime
-import locale
-import sys
-from os import getenv
-from subprocess import call
-
 import setuptools.command.build_py
-from coalib.misc.BuildManPage import BuildManPage
-from coalib.output.dbus.BuildDbusService import BuildDbusService
+from bears import Constants
 from setuptools import find_packages, setup
 from setuptools.command.test import test as TestCommand
 
@@ -22,14 +18,6 @@ try:
     locale.getlocale()
 except (ValueError, UnicodeError):
     locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
-
-
-class BuildPyCommand(setuptools.command.build_py.build_py):
-
-    def run(self):
-        self.run_command('build_manpage')
-        self.run_command('build_dbus')
-        setuptools.command.build_py.build_py.run(self)
 
 
 class PyTestCommand(TestCommand):
@@ -41,66 +29,31 @@ class PyTestCommand(TestCommand):
         sys.exit(errno)
 
 
-class BuildDocsCommand(setuptools.command.build_py.build_py):
-    apidoc_command = ('sphinx-apidoc', '-f', '-o', 'docs/API/',
-                      'coalib')
-    doc_command = ('make', '-C', 'docs', 'html')
-
-    def run(self):
-        call(self.apidoc_command)
-        call(self.doc_command)
-
-
-# Generate API documentation only if we are running on readthedocs.org
-on_rtd = getenv('READTHEDOCS', None) != None
-if on_rtd:
-    call(BuildDocsCommand.apidoc_command)
-    if "dev" in VERSION:
-        current_version = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-        call(['python3', '.misc/adjust_version_number.py', 'coalib/VERSION',
-              '-b {}'.format(current_version)])
-        VERSION = get_version()
-
 with open('requirements.txt') as requirements:
     required = requirements.read().splitlines()
 
 with open('test-requirements.txt') as requirements:
     test_required = requirements.read().splitlines()
 
-with open("README.rst") as readme:
-    long_description = readme.read()
-
 
 if __name__ == "__main__":
-    data_files = [('.', ['coala.1']), ('.', [BUS_NAME + '.service'])]
-
-    setup(name='coala',
-          version=VERSION,
-          description='Code Analysis Application (coala)',
+    setup(name='coala-bears',
+          version=Constants.VERSION,
+          description='Bears for coala (Code Analysis Application)',
           author="The coala developers",
-          maintainer="Lasse Schuirmann, Fabian Neuschmidt, Mischa Kr\xfcger"
-                      if not on_rtd else "L.S., F.N., M.K.",
+          maintainer="Lasse Schuirmann, Fabian Neuschmidt, Mischa Kr\xfcger",
           maintainer_email=('lasse.schuirmann@gmail.com, '
                             'fabian@neuschmidt.de, '
                             'makman@alice.de'),
-          url='http://coala-analyzer.org/',
+          url='http://coala.rtfd.org/',
           platforms='any',
-          packages=find_packages(exclude=["build.*", "tests", "tests.*"]),
+          packages=find_packages(exclude=["build.*"]),
           install_requires=required,
           tests_require=test_required,
-          package_data={'coalib': ['default_coafile', "VERSION",
-                                   'bearlib/languages/definitions/*.coalang']},
+          package_data={'bears': ["VERSION"]},
           license="AGPL-3.0",
-          data_files=data_files,
-          long_description=long_description,
-          entry_points={
-              "console_scripts": [
-                  "coala = coalib.coala:main",
-                  "coala-ci = coalib.coala_ci:main",
-                  "coala-dbus = coalib.coala_dbus:main",
-                  "coala-json = coalib.coala_json:main",
-                  "coala-format = coalib.coala_format:main",
-                  "coala-delete-orig = coalib.coala_delete_orig:main"]},
+          long_description="coala-bears is a set of supported bears for "
+                           "coala - the COde AnaLysis Application.",
           # from http://pypi.python.org/pypi?%3Aaction=list_classifiers
           classifiers=[
               'Development Status :: 4 - Beta',
@@ -127,8 +80,4 @@ if __name__ == "__main__":
               'Topic :: Scientific/Engineering :: Information Analysis',
               'Topic :: Software Development :: Quality Assurance',
               'Topic :: Text Processing :: Linguistic'],
-          cmdclass={'build_manpage': BuildManPage,
-                    'build_dbus': BuildDbusService,
-                    'build_py': BuildPyCommand,
-                    'docs': BuildDocsCommand,
-                    'test': PyTestCommand})
+          cmdclass={'test': PyTestCommand})
