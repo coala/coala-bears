6b69ee0c524b7997814305186df57723fa0e4ee3
AbdealiJK <abdealikothari@gmail.com>
2016-02-19 10:30:20 +0530

    coala-bears: Add minimal setup.py
    This is a minimal setup.py which just has basic requirements.
    
    Fixes https://github.com/coala-analyzer/coala-bears/issues/7

---
+++ setup.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+import locale
+import sys
+
+# Start ignoring PyImportSortBear as imports below may yield syntax errors
+from bears import assert_supported_version
+
+assert_supported_version()
+# Stop ignoring
+
+import setuptools.command.build_py
+from bears import Constants
+from setuptools import find_packages, setup
+from setuptools.command.test import test as TestCommand
+
+try:
+    locale.getlocale()
+except (ValueError, UnicodeError):
+    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+
+
+class PyTestCommand(TestCommand):
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main([])
+        sys.exit(errno)
+
+
+with open('requirements.txt') as requirements:
+    required = requirements.read().splitlines()
+
+with open('test-requirements.txt') as requirements:
+    test_required = requirements.read().splitlines()
+
+
+if __name__ == "__main__":
+    setup(name='coala-bears',
+          version=Constants.VERSION,
+          description='Bears for coala (Code Analysis Application)',
+          author="The coala developers",
+          maintainer="Lasse Schuirmann, Fabian Neuschmidt, Mischa Kr\xfcger",
+          maintainer_email=('lasse.schuirmann@gmail.com, '
+                            'fabian@neuschmidt.de, '
+                            'makman@alice.de'),
+          url='http://coala.rtfd.org/',
+          platforms='any',
+          packages=find_packages(exclude=["build.*"]),
+          install_requires=required,
+          tests_require=test_required,
+          package_data={'bears': ["VERSION"]},
+          license="AGPL-3.0",
+          long_description="coala-bears is a set of supported bears for "
+                           "coala - the COde AnaLysis Application.",
+          # from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+          classifiers=[
+              'Development Status :: 4 - Beta',
+
+              'Environment :: Console',
+              'Environment :: MacOS X',
+              'Environment :: Win32 (MS Windows)',
+              'Environment :: X11 Applications :: Gnome',
+
+              'Intended Audience :: Science/Research',
+              'Intended Audience :: Developers',
+
+              'License :: OSI Approved :: GNU Affero General Public License '
+              'v3 or later (AGPLv3+)',
+
+              'Operating System :: OS Independent',
+
+              'Programming Language :: Python :: Implementation :: CPython',
+              'Programming Language :: Python :: 3.3',
+              'Programming Language :: Python :: 3.4',
+              'Programming Language :: Python :: 3.5',
+              'Programming Language :: Python :: 3 :: Only',
+
+              'Topic :: Scientific/Engineering :: Information Analysis',
+              'Topic :: Software Development :: Quality Assurance',
+              'Topic :: Text Processing :: Linguistic'],
+          cmdclass={'test': PyTestCommand})
