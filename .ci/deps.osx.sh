65099d6c9e96cd62156eb5567e6d03f2dd6728f6
AbdealiJK <abdealikothari@gmail.com>
2016-02-19 19:56:17 +0530

    .ci: Add travis.yml for OSX build
    Fixes https://github.com/coala-analyzer/coala-bears/issues/14

---
+++ .ci/deps.osx.sh
@@ -0,0 +1,36 @@
+set -e
+set -x
+
+# Prerequisites for Go
+mkdir $HOME/Go
+export GOPATH=$HOME/Go
+export GOROOT=/usr/local/opt/go/libexec
+export PATH=$PATH:$GOPATH/bin
+export PATH=$PATH:$GOROOT/bin
+
+# Install packages with brew
+brew update >/dev/null
+brew outdated pyenv || brew upgrade --quiet pyenv
+brew upgrade --quiet go
+brew install libffi && brew link libffi --force
+brew install sqlite && brew link sqlite --force
+brew install openssl && brew link openssl --force
+brew install gnu-indent
+brew install go
+
+# Install required go libraries
+go get -u github.com/golang/lint/golint
+go get -u golang.org/x/tools/cmd/goimports
+go get -u sourcegraph.com/sqs/goreturns
+
+# Install required python version for this build
+pyenv install -ks $PYTHON_VERSION
+pyenv global $PYTHON_VERSION
+python --version
+
+# Install packages with pip
+pip install -q -r test-requirements.txt
+pip install -q -r requirements.txt
+
+# Calling setup.py will download checkstyle automatically so tests may succeed
+python setup.py --help
