5c037b8ffe07199519584f61a185ac430b7d8afa
AbdealiJK <abdealikothari@gmail.com>
2016-02-24 18:15:05 +0530

    LanguageToolBear: Remove get_language_tool_results
    get_language_tool_results() was a function which wasn't
    really required. We had to unnecessarily pass all args
    to it from the bear. Although it made it modular, it
    wasn't being used and can be separated if we ever need
    to use it else where.

--- bears/natural_language/LanguageToolBear.py
+++ bears/natural_language/LanguageToolBear.py
@@ -9,33 +9,6 @@ from coalib.results.Result import Result
 from coalib.results.SourceRange import SourceRange
 
 
-def get_language_tool_results(filename, file_contents, locale):
-    joined_text = "".join(file_contents)
-    locale = guess_language(joined_text) if locale == 'auto' else locale
-    locale = 'en-US' if not locale else locale
-
-    tool = LanguageTool(locale)
-    matches = tool.check(joined_text)
-    for match in matches:
-        if not match.replacements:
-            diffs = None
-        else:
-            replaced = correct(joined_text, [match]).splitlines(True)
-            diffs = {filename:
-                     Diff.from_string_arrays(file_contents, replaced)}
-
-        rule_id = match.ruleId
-        if match.subId is not None:
-            rule_id += '[{}]'.format(match.subId)
-
-        message = match.msg + ' (' + rule_id + ')'
-        yield message, diffs, SourceRange.from_values(filename,
-                                                      match.fromy+1,
-                                                      match.fromx+1,
-                                                      match.toy+1,
-                                                      match.tox+1)
-
-
 class LanguageToolBear(LocalBear):
 
     @classmethod
@@ -57,6 +30,29 @@ class LanguageToolBear(LocalBear):
                        guessed. If the language cannot be guessed, 'en-US'
                        is used.
         '''
-        for message, diffs, range in get_language_tool_results(
-                filename, file, locale):
-            yield Result(self, message, diffs=diffs, affected_code=(range,))
+        joined_text = "".join(file)
+        locale = guess_language(joined_text) if locale == 'auto' else locale
+        locale = 'en-US' if not locale else locale
+
+        tool = LanguageTool(locale)
+        matches = tool.check(joined_text)
+        for match in matches:
+            if not match.replacements:
+                diffs = None
+            else:
+                replaced = correct(joined_text, [match]).splitlines(True)
+                diffs = {filename:
+                         Diff.from_string_arrays(file, replaced)}
+
+            rule_id = match.ruleId
+            if match.subId is not None:
+                rule_id += '[{}]'.format(match.subId)
+
+            message = match.msg + ' (' + rule_id + ')'
+            source_range = SourceRange.from_values(filename,
+                                                   match.fromy+1,
+                                                   match.fromx+1,
+                                                   match.toy+1,
+                                                   match.tox+1)
+            yield Result(self, message, diffs=diffs,
+                         affected_code=(source_range,))
