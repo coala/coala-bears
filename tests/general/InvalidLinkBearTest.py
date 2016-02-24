90d5550d7d4382bdf7ac6d052cd7b1535544cbee
AbdealiJK <abdealikothari@gmail.com>
2016-02-24 12:26:04 +0530

    InvalidLinkBearTest: use force_linebreaks=False
    If force_linebreaks is not set to False, LocalBearTestHelper
    will auto add a \n to the end assuming it's a file. So, to avoid
    that we need to add this arg.

--- tests/general/InvalidLinkBearTest.py
+++ tests/general/InvalidLinkBearTest.py
@@ -6,41 +6,48 @@ LinkRedirect = verify_local_bear(InvalidLinkBear,
                                     ["http://httpbin.org/status/200"],),
                                  invalid_files=(
                                     ["http://httpbin.org/status/301"],
-                                    ["http://coala.rtfd.org"]))
+                                    ["http://coala.rtfd.org"]),
+                                 force_linebreaks=False)
 
 InvalidLinkNotFound = verify_local_bear(InvalidLinkBear,
                                         valid_files=(
                                             ["http://httpbin.org/status/202"],),
                                         invalid_files=(
                                             ["http://httpbin.org/status/404"],
-                                            ["http://httpbin.org/status/401"]))
+                                            ["http://httpbin.org/status/401"]),
+                                        force_linebreaks=False)
 
 InvalidLinkServerError = verify_local_bear(InvalidLinkBear,
                                            valid_files=(
                                             ["http://httpbin.org/status/202"],),
                                            invalid_files=(
                                             ["http://httpbin.org/status/500"],
-                                            ["http://httpbin.org/status/503"]))
+                                            ["http://httpbin.org/status/503"]),
+                                           force_linebreaks=False)
 
 LinkDoesNotExist = verify_local_bear(InvalidLinkBear,
                                      valid_files=(
+                                        ["http://coala-analyzer.org/\n"],
                                         ["http://coala-analyzer.org/"],
                                         ["http://not a link dot com"],
                                         ["<http://lwn.net/>"],
                                         ["'https://www.gnome.org/'"],
                                         ["http://coala-analyzer.org/..."]),
                                      invalid_files=(
-                                        ["http://coalaisthebest.com"],))
+                                        ["http://coalaisthebest.com"],),
+                                     force_linebreaks=False)
 
 MarkdownLinks = verify_local_bear(InvalidLinkBear,
                                   valid_files=(
                                         ["[coala](http://coala-analyzer.org/)"],
                                         ["https://en.wikipedia.org/wiki/"
                                          "Hello_(Adele_song)"]),
-                                  invalid_files=())
+                                  invalid_files=(),
+                                  force_linebreaks=False)
 
 SphinxLinks = verify_local_bear(InvalidLinkBear,
                                 valid_files=(
                                     ["|https://github.com/coala-analyzer/"
                                      "coala-bears|"],),
-                                invalid_files=())
+                                invalid_files=(),
+                                force_linebreaks=False)
