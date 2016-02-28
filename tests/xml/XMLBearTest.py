8ee9e3c926b667c6b563501f15897382dbe50610
SanketDG <sanketdasgupta@gmail.com>
2016-02-28 15:21:11 +0530

    XMLBear: Yield results from stderr
    XMLBear now will yield results from stdout as well as stderr
    to report syntax errors
    
    Fixes https://github.com/coala-analyzer/coala-bears/issues/112

--- tests/xml/XMLBearTest.py
+++ tests/xml/XMLBearTest.py
@@ -2,8 +2,7 @@ from bears.xml.XMLBear import XMLBear
 from tests.LocalBearTestHelper import verify_local_bear
 
 
-valid_xml_file = """
-<?xml version="1.0"?>
+valid_xml_file = """<?xml version="1.0"?>
 <a/>
 """.splitlines(keepends=True)
 
@@ -11,6 +10,22 @@ invalid_xml_file = """
 <a>blah</a>
 """.splitlines(keepends=True)
 
-InvalidXMLTest = verify_local_bear(XMLBear,
-                                   valid_files=(valid_xml_file,),
-                                   invalid_files=(invalid_xml_file,),)
+invalid_xml_chars = """<?xml version="1.0"?>
+<a>hey & hi</a>
+""".splitlines(keepends=True)
+
+valid_xml_chars = """<?xml version="1.0"?>
+<a>hey and hi</a>
+""".splitlines(keepends=True)
+
+InvalidXMLCorrectedTest = verify_local_bear(
+                                        XMLBear,
+                                        valid_files=(valid_xml_file,),
+                                        invalid_files=(invalid_xml_file,),
+                                        tempfile_kwargs={"suffix": ".xml"})
+
+InvalidXMLSyntaxTest = verify_local_bear(
+                                        XMLBear,
+                                        valid_files=(valid_xml_chars,),
+                                        invalid_files=(invalid_xml_chars,),
+                                        tempfile_kwargs={"suffix": ".xml"})
