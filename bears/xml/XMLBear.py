c63d1b69ea7e3b88f4a43413bde174b785eb21a3
SanketDG <sanketdasgupta@gmail.com>
2016-02-28 15:21:11 +0530

    XMLBear: Yield results from stderr
    XMLBear now will yield results from stdout as well as stderr
    to report syntax errors
    
    Fixes https://github.com/coala-analyzer/coala-bears/issues/112

--- bears/xml/XMLBear.py
+++ bears/xml/XMLBear.py
@@ -6,11 +6,14 @@ class XMLBear(LocalBear, Lint):
     executable = 'xmllint'
     arguments = '{filename}'
     diff_message = "XML can be formatted better."
+    output_regex = r'(.*\.xml):(?P<line>\d+): (?P<message>.*)\n.*\n.*'
     gives_corrected = True
 
     def process_output(self, output, filename, file):
         if self.stdout_output:  # only yield Result if stdout is not empty
-            return self._process_corrected(output, filename, file)
+            return self._process_corrected(self.stdout_output, filename, file)
+        if self.stderr_output:  # pragma: no cover
+            return self._process_issues(self.stderr_output, filename)
 
     def run(self, filename, file):
         '''
