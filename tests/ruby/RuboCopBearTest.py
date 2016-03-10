acc15da5d477e954dee0531c56ba5d15f6b95c6b
Karan <karansharma1295@gmail.com>
2016-03-10 22:22:08 +0530

    bears/ruby: Add RuboCop Bear
    This bear uses RuboCop which is a static code
    analyzer for Ruby.
    
    Fixes: https://github.com/coala-analyzer/coala-bears/pull/192

--- tests/ruby/RuboCopBearTest.py
+++ tests/ruby/RuboCopBearTest.py
@@ -1,12 +1,12 @@
 from bears.ruby.RuboCopBear import RuboCopBear
 from tests.LocalBearTestHelper import verify_local_bear
 
-good_file = """arr = [1, 2, 3]
+good_file = """arr = [1, 2, 3, 4]
 # good
 arr.each { |elem| puts elem }""".splitlines(keepends=True)
 
 
-bad_file = """arr = [1, 2, 3]
+bad_file = """arr = [1, 2, 3, 4]
 # bad
 for elem in arr do
   puts elem
