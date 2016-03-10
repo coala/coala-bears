1520444923f2c035e4a5e58f0710eeba8b8d570b
Sheikh Araf <arafsheikh@rocketmail.com>
2016-03-10 23:20:32 +0530

    bears/typescript: Add TSLintBear
    Fixes https://github.com/coala-analyzer/coala-bears/issues/201

---
+++ bears/typescript/TSLintBear.py
@@ -0,0 +1,45 @@
+import json
+
+from coalib.bearlib.abstractions.Lint import Lint
+from coalib.bears.LocalBear import LocalBear
+from coalib.misc.Shell import escape_path_argument
+from coalib.results.Result import Result
+from coalib.settings.Setting import path
+
+
+class TSLintBear(LocalBear, Lint):
+    executable = 'tslint'
+
+    def process_output(self, output, filename, file):
+        output = json.loads("".join(output)) if output else []
+        for issue in output:
+            yield Result.from_values(
+                origin="{} ({})".format(self.__class__.__name__,
+                                        issue['ruleName']),
+                message=issue["failure"],
+                file=issue["name"],
+                line=int(issue["startPosition"]["line"]) + 1,
+                end_line=int(issue["endPosition"]["line"]) + 1,
+                column=issue["startPosition"]["character"],
+                end_column=issue["endPosition"]["character"])
+
+    def run(self,
+            filename,
+            file,
+            tslint_config: path="",
+            rules_dir: path=""):
+        '''
+        Checks the code with ``tslint`` on each file separately.
+
+        :param tslint_config: Path to configuration file.
+        :param rules_dir:     Rules directory
+        '''
+        self.arguments = "--format json"
+        if tslint_config:
+            self.arguments += (" --config " +
+                               escape_path_argument(tslint_config))
+        if rules_dir:
+            self.arguments += (" --rules-dir " +
+                               escape_path_argument(rules_dir))
+        self.arguments += " {filename}"
+        return self.lint(filename, file)
