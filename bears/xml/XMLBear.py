83fa5121bfea7d8fb97c0661193834d9dda8c2c1
Vivek Jain <vivek425ster@gmail.com>
2016-03-12 18:45:57 +0530

    XMLBear: Add schema and dtdvalid args
    Added schema and dtdvalid args so that xml can
    be validated by given schema and dtd file.Also
    added tests related to the same.
    
    Fixes https://github.com/coala-analyzer/coala-bears/issues/212

--- bears/xml/XMLBear.py
+++ bears/xml/XMLBear.py
@@ -1,10 +1,24 @@
 from coalib.bearlib.abstractions.Lint import Lint
 from coalib.bears.LocalBear import LocalBear
+from coalib.misc.Shell import escape_path_argument
+from coalib.settings.Setting import path, url
+
+
+def path_or_url(xml_dtd):
+    '''
+    Coverts the setting value to url or path.
+
+    :param xml_dtd: Setting key.
+    :return:        Returns a converted setting value.
+    '''
+    try:
+        return url(xml_dtd)
+    except ValueError:
+        return path(xml_dtd)
 
 
 class XMLBear(LocalBear, Lint):
     executable = 'xmllint'
-    arguments = '{filename}'
     diff_message = "XML can be formatted better."
     output_regex = r'(.*\.xml):(?P<line>\d+): (?P<message>.*)\n.*\n.*'
     gives_corrected = True
@@ -15,8 +29,19 @@ class XMLBear(LocalBear, Lint):
         if self.stderr_output:  # pragma: no cover
             return self._process_issues(self.stderr_output, filename)
 
-    def run(self, filename, file):
+    def run(self, filename, file,
+            xml_schema: path="",
+            xml_dtd: path_or_url=""):
         '''
-        Checks the code with `xmllint`.
+        Checks the code with ``xmllint``.
+
+        :param xml_schema: ``W3C XML Schema`` file used for validation.
+        :param xml_dtd:    ``Document type Definition (DTD)`` file or
+                           url used for validation.
         '''
+        self.arguments = "{filename} "
+        if xml_schema:
+            self.arguments += " -schema " + escape_path_argument(xml_schema)
+        if xml_dtd:
+            self.arguments += " -dtdvalid " + xml_dtd
         return self.lint(filename, file)
--- tests/xml/XMLBearTest.py
+++ tests/xml/XMLBearTest.py
@@ -1,3 +1,5 @@
+import os
+
 from bears.xml.XMLBear import XMLBear
 from tests.LocalBearTestHelper import verify_local_bear
 
@@ -18,14 +20,64 @@ valid_xml_chars = """<?xml version="1.0"?>
 <a>hey and hi</a>
 """.splitlines(keepends=True)
 
-InvalidXMLCorrectedTest = verify_local_bear(
-                                        XMLBear,
-                                        valid_files=(valid_xml_file,),
-                                        invalid_files=(invalid_xml_file,),
-                                        tempfile_kwargs={"suffix": ".xml"})
+dtd_file = os.path.join(os.path.dirname(__file__),
+                        "test_files",
+                        "note.dtd")
 
-InvalidXMLSyntaxTest = verify_local_bear(
-                                        XMLBear,
-                                        valid_files=(valid_xml_chars,),
-                                        invalid_files=(invalid_xml_chars,),
-                                        tempfile_kwargs={"suffix": ".xml"})
+schema_file = os.path.join(os.path.dirname(__file__),
+                           "test_files",
+                           "note.xsd")
+
+valid_xml_path = list(open(os.path.join(
+    os.path.dirname(__file__),
+    "test_files",
+    "note.xml"), 'r'))
+
+valid_xml_url = list(open(os.path.join(
+    os.path.dirname(__file__),
+    "test_files",
+    "concept-valid.xml"), 'r'))
+
+invalid_xml_schema = list(open(os.path.join(
+    os.path.dirname(__file__),
+    "test_files",
+    "xsd-error.xml"), 'r'))
+
+invalid_xml_dtd = list(open(os.path.join(
+    os.path.dirname(__file__),
+    "test_files",
+    "dtd-error.xml"), 'r'))
+
+invalid_xml_url = list(open(os.path.join(
+    os.path.dirname(__file__),
+    "test_files",
+    "concept-invalid.xml"), 'r'))
+
+dtd_url = "http://docs.oasis-open.org/dita/v1.0.1/dtd/concept.dtd"
+
+XMLBearCorrectedTest = verify_local_bear(
+    XMLBear,
+    valid_files=(valid_xml_file, valid_xml_chars),
+    invalid_files=(invalid_xml_file, invalid_xml_chars),
+    tempfile_kwargs={"suffix": ".xml"})
+
+XMLBearSchemaTest = verify_local_bear(
+    XMLBear,
+    valid_files=(valid_xml_path,),
+    invalid_files=(invalid_xml_schema,),
+    settings={'xml_schema': schema_file},
+    tempfile_kwargs={"suffix": ".xml"})
+
+XMLBearDTDPathTest = verify_local_bear(
+    XMLBear,
+    valid_files=(valid_xml_path,),
+    invalid_files=(invalid_xml_dtd,),
+    settings={'xml_dtd': dtd_file},
+    tempfile_kwargs={"suffix": ".xml"})
+
+XMLBearDTDUrlTest = verify_local_bear(
+    XMLBear,
+    valid_files=(valid_xml_url,),
+    invalid_files=(invalid_xml_url,),
+    settings={'xml_dtd': dtd_url},
+    tempfile_kwargs={"suffix": ".xml"})
