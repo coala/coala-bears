1b33010b529f714559430ca9aa3b21b5e64de3f4
Anshuman <anshuman14021@iiitd.ac.in>
2016-03-12 13:33:34 +0530

    Fix error after previous commit

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+
+# Start ignoring PyImportSortBear as imports below may yield syntax errors
+from bears import assert_supported_version
 assert_supported_version()
 # Stop ignoring
 
@@ -9,10 +12,6 @@ from os.path import exists
 from shutil import copyfileobj
 from subprocess import call
 from urllib.request import urlopen
-
-# Start ignoring PyImportSortBear as imports below may yield syntax errors
-from bears import assert_supported_version
-
 
 import setuptools.command.build_py
 from bears import Constants
