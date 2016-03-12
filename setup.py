d60259cc0e3599f6e6b03084a71398ec79c74c90
Anshuman <anshuman14021@iiitd.ac.in>
2016-03-12 13:18:41 +0530

    setup: Change position of urllib import
    When installing via setup.py (using python2), the setup doesn't assert
    the requirement of python3. Instead, it throws an error. This is fixed
    by shifting one the 'from urllib.request import urlopen' import
    statement, as it requires python3.
    
    Fixes https://github.com/coala-analyzer/coala-bears/issues/223

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ import sys
 from os.path import exists
 from shutil import copyfileobj
 from subprocess import call
-from urllib.request import urlopen
 
 # Start ignoring PyImportSortBear as imports below may yield syntax errors
 from bears import assert_supported_version
@@ -13,6 +12,7 @@ from bears import assert_supported_version
 assert_supported_version()
 # Stop ignoring
 
+from urllib.request import urlopen
 import setuptools.command.build_py
 from bears import Constants
 from setuptools import find_packages, setup
