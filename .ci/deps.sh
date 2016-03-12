232cdb7ad4e406dc22cf4223c969a11e3141180e
aachiritoaei <achirialex@gmail.com>
2016-03-12 14:05:21 +0200

    bears/java: Add InferBear
    Adds an InferBear for Java
    
    Fixes https://github.com/coala-analyzer/coala-bears/issues/30

--- .ci/deps.sh
+++ .ci/deps.sh
@@ -13,12 +13,14 @@ esac
 sudo add-apt-repository -y ppa:marutter/rdev
 sudo add-apt-repository -y ppa:staticfloat/juliareleases
 sudo add-apt-repository -y ppa:staticfloat/julia-deps
+sudo add-apt-repository -y ppa:avsm/ppa
 sudo apt-get -y update
 deps="espeak libclang1-3.4 indent mono-mcs chktex hlint r-base julia luarocks"
 deps_python_dbus="libdbus-glib-1-dev libdbus-1-dev"
 deps_python_gi="glib2.0-dev gobject-introspection libgirepository1.0-dev python3-cairo-dev"
 deps_perl="perl libperl-critic-perl"
-sudo apt-get -y install $deps $deps_python_gi $deps_python_dbus $deps_perl
+deps_infer="m4 opam"
+sudo apt-get -y install $deps $deps_python_gi $deps_python_dbus $deps_perl $deps_infer > /dev/null
 
 # Update hlint to latest version (not available in apt)
 wget https://launchpad.net/ubuntu/+source/hlint/1.9.26-1/+build/8831318/+files/hlint_1.9.26-1_amd64.deb
@@ -44,7 +46,7 @@ bundle install
 
 for dep_version in "${dep_versions[@]}" ; do
   pyenv install -ks $dep_version
-  pyenv local $dep_version
+  pyenv local $dep_version 2.7.10
   python --version
   source .ci/env_variables.sh
 
@@ -67,3 +69,15 @@ julia -e "Pkg.add(\"Lint\")"
 
 # Lua commands
 sudo luarocks install luacheck --deps-mode=none
+
+# Infer commands
+if [ ! -e ~/infer-linux64-v0.7.0/infer/bin ]; then 
+	wget -nc -O ~/infer.tar.xz https://github.com/facebook/infer/releases/download/v0.7.0/infer-linux64-v0.7.0.tar.xz
+	tar xf ~/infer.tar.xz -C ~/
+	cd ~/infer-linux64-v0.7.0
+	opam init --y
+	opam update
+	opam pin add --yes --no-action infer .
+	opam install --deps-only --yes infer
+	./build-infer.sh java
+fi
---
+++ bears/java/InferBear.py
@@ -0,0 +1,24 @@
+import re
+
+from coalib.bearlib.abstractions.Lint import Lint
+from coalib.bears.LocalBear import LocalBear
+from coalib.results.RESULT_SEVERITY import RESULT_SEVERITY
+
+
+class InferBear(LocalBear, Lint):
+    executable = 'infer'
+    arguments = '-- javac {filename}'
+    output_regex = re.compile(
+        r'(?P<file_name>.+):'
+        r'(?P<line>.+): '
+        r'(?P<severity>error|warning): '
+        r'(?P<message>.*)')
+    severity_map = {
+        "error": RESULT_SEVERITY.MAJOR,
+        "warning": RESULT_SEVERITY.NORMAL}
+
+    def run(self, filename, file):
+        '''
+        Checks the code with ``infer``.
+        '''
+        return self.lint(filename)
