8f425eb770ccf2e3f67e89c52c6cb86aee4d58d3
Karan <karansharma1295@gmail.com>
2016-03-13 15:33:25 +0530

    bears/r: Add Skip condition for FormatRBear
    prerequisite_command and prerequisite_fail_msg added
    to FormatRBear to check prerequisite.
    
    Fixes https://github.com/coala-analyzer/coala-bears/issues/221

--- bears/r/FormatRBear.py
+++ bears/r/FormatRBear.py
@@ -5,6 +5,8 @@ from coalib.bears.LocalBear import LocalBear
 class FormatRBear(Lint, LocalBear):
     executable = "Rscript"
     arguments = "-e 'library(formatR)' -e 'formatR::tidy_source({filename})'"
+    prerequisite_command = ['Rscript', '-e', "'library(formatR)'"]
+    prerequisite_fail_msg = "Please install formatR for this bear to work."
     diff_message = "Formatting can be improved."
     gives_corrected = True
 
