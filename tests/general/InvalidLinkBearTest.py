6accced18307593076cab2044d1286005d70f7de
SanketDG <sanketdasgupta@gmail.com>
2016-03-16 02:47:40 +0530

    InvalidLinkBear: Refactor
    Appending status codes to every url is not intuitive for someone
    who reads the code. So httpbin.org urls are used in every testcase
    except for non-existent domain.
    
    Repeating tests are removed.
    
    A connection checking variable is introduced to avoid repitition.

--- tests/general/InvalidLinkBearTest.py
+++ tests/general/InvalidLinkBearTest.py
@@ -9,18 +9,23 @@ from coalib.settings.Section import Section
 
 def custom_matcher(request):
     change_url = False
-    try:
-        status_code = int(request.path_url[-3:])
-        if status_code in range(300, 400):
-            change_url = True
-            url = "some_url"
-        resp = requests.Response()
-        if change_url:
-            resp.url = url
-        resp.status_code = status_code
-        return resp
-    except ValueError:
-        raise requests.exceptions.RequestException("")
+    # the connection check url needs to be explicitly
+    # set to 200
+    if request.url == InvalidLinkBear.check_connection_url:
+        status_code = 200
+    else:
+        try:
+            status_code = int(request.path_url[-3:])
+        except ValueError:
+            raise requests.exceptions.RequestException
+    if status_code in range(300, 400):
+        change_url = True
+        url = "some_url"
+    resp = requests.Response()
+    if change_url:
+        resp.url = url
+    resp.status_code = status_code
+    return resp
 
 
 class InvalidLinkBearTest(unittest.TestCase):
@@ -42,73 +47,75 @@ class InvalidLinkBearTest(unittest.TestCase):
                 self.assertNotEqual(out, None)
 
     def test_run(self):
-        # Link Redirect
+        # Valid Links
         valid_file = """
         http://httpbin.org/status/200
+        http://httpbin.org/status/201
+        https://en.wikipedia.org/wiki/200
         """.splitlines()
 
+        self.assertResult(valid_file=valid_file)
+
+        # Link Redirect
         invalid_file = """
         http://httpbin.org/status/301
-        http://coala.rtfd.org/302
+        http://httpbin.org/status/302
         """.splitlines()
 
-        self.assertResult(valid_file=valid_file, invalid_file=invalid_file)
+        self.assertResult(invalid_file=invalid_file)
 
         # Invalid Link Not Found
-        valid_file = """
-        http://httpbin.org/status/202
-        """.splitlines()
         invalid_file = """
         http://httpbin.org/status/404
         http://httpbin.org/status/401
         """.splitlines()
-        self.assertResult(valid_file=valid_file, invalid_file=invalid_file)
+
+        self.assertResult(invalid_file=invalid_file)
 
         # Invalid Link ServerError
-        valid_file = """
-        http://httpbin.org/status/202
-        """.splitlines()
         invalid_file = """
         http://httpbin.org/status/500
         http://httpbin.org/status/503
         """.splitlines()
-        self.assertResult(valid_file=valid_file, invalid_file=invalid_file)
+
+        self.assertResult(invalid_file=invalid_file)
 
         # Link Does Not Exist
-        valid_file = """
-           http://coala-analyzer.org/200\n
-           http://coala-analyzer.org/200
-           http://not a link dot com
-           <http://lwn.net/200>
-           'https://www.gnome.org/200'
-           http://coala-analyzer.org/200.....
-        """.splitlines()
         invalid_file = """
            http://coalaisthebest.com/
         """.splitlines()
 
-        self.assertResult(valid_file=valid_file, invalid_file=invalid_file)
+        self.assertResult(invalid_file=invalid_file)
 
-        # Mark down Links
+        # Test Regex
         valid_file = """
-            [coala](http://coala-analyzer.org/200)
             https://en.wikipedia.org/wiki/Hello_(Adele_song)/200
             "https://github.com/coala-analyzer/coala-bears/issues/200"
+            http://httpbin.org/status/200\n
+            http://httpbin.org/status/201
+            <http://httpbin.org/status/202>
+            'http://httpbin.org/status/203'
+            http://httpbin.org/status/204.....
+            http://not a link dot com
         """.splitlines()
-        invalid_file = """
-            http://coalaisthebest.com/
-        """.splitlines()
+
         self.assertResult(valid_file=valid_file)
 
-        # SphinxLinks
+        # Markdown Links
         valid_file = """
-        |https://github.com/coala-analyzer/coala-bears/200|
+            [httpbin](http://httpbin.org/status/200)
         """.splitlines()
+
+        self.assertResult(valid_file=valid_file)
+
+        # Sphinx Links
+        valid_file = """
+        |http://httpbin.org/status/200|
+        """.splitlines()
+
         self.assertResult(valid_file=valid_file)
 
     def test_check_prerequisites(self):
         with requests_mock.Mocker() as m:
             m.add_matcher(custom_matcher)
-            self.assertIn("You are not", InvalidLinkBear.check_prerequisites())
-            m.head("http://www.google.com")
-            self.assertEqual(InvalidLinkBear.check_prerequisites(), True)
+            self.assertTrue(InvalidLinkBear.check_prerequisites())
