d62f5b2187515ebf35072f6386af5326aa9be0e7
Ibrahim <jarifibrahim@gmail.com>
2016-03-21 15:09:26 +0530

    PyDocStyleBear: Add args for PyDocStyleBear
    Fixes https://github.com/coala-analyzer/coala-bears/issues/261

--- tests/python/PyDocStyleBearTest.py
+++ tests/python/PyDocStyleBearTest.py
@@ -23,7 +23,47 @@ def hello():
 
 '''.splitlines(keepends=True)
 
-PyDocStyleBearTest = verify_local_bear(PyDocStyleBear,
-                                       valid_files=(good_file,),
-                                       invalid_files=(bad_file,),
-                                       tempfile_kwargs={"suffix": ".py"})
+PyDocStyleBearTest = verify_local_bear(
+    PyDocStyleBear,
+    valid_files=(good_file,),
+    invalid_files=(bad_file,),
+    tempfile_kwargs={"suffix": ".py"})
+
+PyDocStyleBearIgnoreAllTest = verify_local_bear(
+    PyDocStyleBear,
+    valid_files=(good_file, bad_file,),
+    invalid_files=(),
+    settings={'pydocstyle_ignore': 'D400, D200, D103'},
+    tempfile_kwargs={"suffix": ".py"})
+
+# Checks if an invalid file yields results when only some errors are ignored.
+PyDocStyleBearIgnoreSomeTest = verify_local_bear(
+    PyDocStyleBear,
+    valid_files=(good_file,),
+    invalid_files=(bad_file,),
+    settings={'pydocstyle_ignore': 'D400, D200'},
+    tempfile_kwargs={"suffix": ".py"})
+
+# Checks if an invalid file yields results when a particular error is selected.
+PyDocStyleBearSelectSomeTest = verify_local_bear(
+    PyDocStyleBear,
+    valid_files=(good_file,),
+    invalid_files=(bad_file,),
+    settings={'pydocstyle_select': 'D200'},
+    tempfile_kwargs={"suffix": ".py"})
+
+# Checks if an invalid file yields nothing when the selected error never occurs.
+PyDocStyleBearSelectAbsentErrorTest = verify_local_bear(
+    PyDocStyleBear,
+    valid_files=(good_file, bad_file,),
+    invalid_files=(),
+    settings={'pydocstyle_select': 'D500'},
+    tempfile_kwargs={"suffix": ".py"})
+
+PyDocStyleBearSelectAndIgnoreTest = verify_local_bear(
+    PyDocStyleBear,
+    valid_files=(good_file, bad_file,),
+    invalid_files=(),
+    settings={'pydocstyle_select': 'D200',
+              'pydocstyle_ignore': 'D400'},
+    tempfile_kwargs={"suffix": ".py"})
