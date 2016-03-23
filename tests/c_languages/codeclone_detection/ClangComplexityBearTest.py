51621be4ae53e317ae368ba6573fc3d531c8a94b
Attila Tovt <uran198@gmail.com>
2016-03-23 18:14:33 +0200

    bears/c_languages: Add ClangComplexityBear
    Fixes https://github.com/coala-analyzer/coala-bears/issues/81

---
+++ tests/c_languages/codeclone_detection/ClangComplexityBearTest.py
@@ -0,0 +1,91 @@
+import os
+import unittest
+from queue import Queue
+from clang.cindex import Index
+
+from coalib.settings.Section import Section
+from coalib.results.Result import Result
+from coalib.results.SourceRange import SourceRange
+from bears.c_languages.codeclone_detection.ClangComplexityBear import (
+    ClangComplexityBear)
+from tests.BearTestHelper import generate_skip_decorator
+from tests.LocalBearTestHelper import execute_bear
+
+
+@generate_skip_decorator(ClangComplexityBear)
+class ClangComplexityBearTest(unittest.TestCase):
+
+    def setUp(self):
+        self.filename = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                     "conditions_samples.c"))
+        self.file = "fake"
+        self.queue = Queue()
+        self.section = Section("test section")
+        self.bear = ClangComplexityBear(self.section, self.queue)
+
+    def test_calculation(self):
+        """
+        Testing that number of decision points and exit points are calculated
+        correctly.
+        """
+        expected = [
+            ('used(int, int)', 3),
+            ('returned(int, int)', 1),
+            ('loopy(int, int)', 5),
+            ('in_condition(int, int)', 1),
+            ('assignation(int, int)', 2),
+            ('arithmetics(int, int)', 1),
+            ('levels(int, int, int)', 10),
+            ('structing(struct test_struct, struct test_struct *)', 1),
+            ('switching(int, int)', 2)]
+
+        root = Index.create().parse(self.filename).cursor
+        complexities_gen = self.bear.complexities(root, self.filename)
+        results = [(cursor.displayname, complexity)
+                   for cursor, complexity in complexities_gen]
+        self.assertSequenceEqual(results, expected)
+
+    def test_output(self):
+        """
+        Validating that the yielded results are correct.
+        """
+        affected_code = (SourceRange.from_values(
+            self.filename,
+            start_line=111,
+            start_column=1,
+            end_line=143,
+            end_column=2),)
+        expected_result = Result(
+            self.bear,
+            "The cyclomatic complexity of function levels(int, int, int) is "
+            "10 which exceeded maximal recommended value of 8.",
+            affected_code=affected_code)
+        with execute_bear(self.bear, self.filename, self.file, 8) as out:
+            self.assertEqual(len(out), 1)
+        self.assertEqual(out[0], expected_result)
+
+    def test_empty_declared_function(self):
+        """
+        Should not take into account and display empty function declarations.
+        """
+        self.filename = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                     "empty_declarations.c"))
+        expected = [('with_body(int *)', 1)]
+
+        root = Index.create().parse(self.filename).cursor
+        complexities_gen = self.bear.complexities(root, self.filename)
+        results = [(cursor.displayname, complexity)
+                   for cursor, complexity in complexities_gen]
+        self.assertSequenceEqual(results, expected)
+
+    def test_file_does_not_exist(self):
+        """
+        Tests that bear throws TranslationUnitLoadError when file does not
+        exist.
+        """
+        from clang.cindex import TranslationUnitLoadError
+
+        generator = self.bear.execute("not_existing", self.file)
+        self.assertNotEqual(generator, None)
+        with self.assertRaises(TranslationUnitLoadError):
+            yield generator
