93860385ee3dfa8c568119aca22e6a4c39879c65
Makman2 <mak95@arcor.de>
2016-04-03 22:25:30 +0200

    JSONFormatBear: Remove Lint dependency
    For generating diff patches only using Lint is an overkill, also the
    current code bypasses 99% of the Lint code.

--- bears/js/JSONFormatBear.py
+++ bears/js/JSONFormatBear.py
@@ -1,43 +1,21 @@
 import json
 from collections import OrderedDict
 
-from coalib.bearlib.abstractions.Lint import Lint
 from coalib.bearlib.spacing.SpacingHelper import SpacingHelper
 from coalib.bears.LocalBear import LocalBear
+from coalib.results.Diff import Diff
 from coalib.results.Result import Result
 
 
-class JSONFormatBear(Lint, LocalBear):
+class JSONFormatBear(LocalBear):
     try:
         DecodeError = json.decoder.JSONDecodeError
     except AttributeError:
         DecodeError = ValueError
 
-    diff_message = ("This file can be reformatted by sorting keys and "
-                    "following indentation.")
-    gives_corrected = True
     LANGUAGES = "JSON"
 
-    def lint(self, filename, file, **kwargs):
-        try:
-            json_content = json.loads(''.join(file),
-                                      object_pairs_hook=OrderedDict)
-        except self.DecodeError as err:
-            return [Result.from_values(
-                self,
-                "This file does not contain parsable JSON. '{adv_msg}'"
-                .format(adv_msg=str(err)),
-                file=filename)]
-
-        new_file = json.dumps(json_content, **kwargs).splitlines(True)
-        # Because of a bug in several python versions we have to correct
-        # whitespace here.
-        output = [line.rstrip(" \n")+"\n" for line in new_file]
-        return self.process_output(output, filename, file)
-
-    def run(self,
-            filename,
-            file,
+    def run(self, filename, file,
             json_sort: bool=False,
             tab_width: int=SpacingHelper.DEFAULT_TAB_WIDTH,
             keep_unicode: bool=False):
@@ -48,8 +26,29 @@ class JSONFormatBear(Lint, LocalBear):
         :param tab_width:    Number of spaces to indent.
         :param keep_unicode: Wether or not to escape unicode values using ASCII.
         """
-        return self.lint(filename,
-                         file,
-                         sort_keys=json_sort,
-                         indent=tab_width,
-                         ensure_ascii=not keep_unicode)
+        try:
+            json_content = json.loads(''.join(file),
+                                      object_pairs_hook=OrderedDict)
+        except self.DecodeError as err:
+            yield Result.from_values(
+                self,
+                "This file does not contain parsable JSON. " + repr(str(err)),
+                file=filename)
+            return
+
+        corrected = json.dumps(json_content,
+                               sort_keys=json_sort,
+                               indent=tab_width,
+                               ensure_ascii=not keep_unicode).splitlines(True)
+        # Because of a bug in several python versions we have to correct
+        # whitespace here.
+        corrected = tuple(line.rstrip(" \n") + "\n" for line in corrected)
+        diff = Diff.from_string_arrays(file, corrected)
+
+        if len(diff) > 0:
+            yield Result(self,
+                         "This file can be reformatted by sorting keys and "
+                         "following indentation.",
+                         affected_code=tuple(d.range(filename)
+                                             for d in diff.split_diff()),
+                         diffs={filename: diff})
