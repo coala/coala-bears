0b13767d3363420a77f28e51d2b7072394b904cd
Makman2 <mak95@arcor.de>
2016-04-03 22:27:26 +0200

    GoVetBear: Refactor to Linter v2 API

--- bears/go/GoVetBear.py
+++ bears/go/GoVetBear.py
@@ -1,18 +1,16 @@
-import re
-
-from coalib.bearlib.abstractions.Lint import Lint
-from coalib.bears.LocalBear import LocalBear
+from coalib.bearlib.abstractions.Linter import linter
 
 
-class GoVetBear(LocalBear, Lint):
-    executable = 'go'
-    arguments = 'vet {filename}'
-    output_regex = re.compile(
-        r'(?P<file_name>.+):(?P<line>\d+): (?P<message>.*)\n')
-    use_stderr = True
+@linter(executable='go',
+        use_stdout=False,
+        use_stderr=True,
+        output_format='regex',
+        output_regex=r'.+:(?P<line>\d+): (?P<message>.*)')
+class GoVetBear:
+    """
+    Checks the code using ``go vet``.
+    """
 
-    def run(self, filename, file):
-        '''
-        Checks the code using `go vet`.
-        '''
-        return self.lint(filename)
+    @staticmethod
+    def create_arguments(filename, file, config_file):
+        return 'vet', filename
