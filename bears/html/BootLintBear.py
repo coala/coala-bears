b540081f57e24d58fbd6c9be863e5b40a2638442
Makman2 <mak95@arcor.de>
2016-04-03 22:26:57 +0200

    BootLintBear: Refactor to Linter v2 API

--- bears/html/BootLintBear.py
+++ bears/html/BootLintBear.py
@@ -1,34 +1,24 @@
-import re
-
-from coalib.bearlib.abstractions.Lint import Lint
-from coalib.bears.LocalBear import LocalBear
+from coalib.bearlib.abstractions.Linter import linter
 from coalib.results.RESULT_SEVERITY import RESULT_SEVERITY
 from coalib.settings.Setting import typed_list
 
 
-class BootLintBear(LocalBear, Lint):
-    executable = 'bootlint'
-    output_regex = re.compile(
-        r'(.+?):*(?P<line>\d*):+'
-        r'(?P<col>\d*)\s(?P<severity>.)\d+'
-        r'\s(?P<message>.+)')
-    severity_map = {
-        "W": RESULT_SEVERITY.NORMAL,
-        "E": RESULT_SEVERITY.MAJOR
-    }
+@linter(executable='bootlint',
+        output_format='regex',
+        output_regex=r'.+:(?P<line>\d*):(?P<column>\d*) (?P<severity>.)\d+ '
+                     r'(?P<message>.+)',
+        severity_map={"W": RESULT_SEVERITY.NORMAL,
+                      "E": RESULT_SEVERITY.MAJOR})
+class BootLintBear:
+    """
+    Checks the code with ``bootlint`` on each file separately.
+    """
 
-    def _get_groupdict(self, match):
-        groups = Lint._get_groupdict(self, match)
-        if groups["line"] == "" and groups["col"] == "":
-            del groups["line"], groups["col"]
-        return groups
-
-    def run(self, filename, file, bootlint_ignore: typed_list(str)=[]):
-        '''
-        Checks the code with ``bootlint`` on each file separately.
-
+    @staticmethod
+    def create_arguments(filename, file, config_file,
+                         bootlint_ignore: typed_list(str)=[]):
+        """
         :param bootlint_ignore: List of checkers to ignore.
-        '''
+        """
         ignore = ','.join(part.strip() for part in bootlint_ignore)
-        self.arguments = '--disable=' + ignore + " {filename}"
-        return self.lint(filename)
+        return '--disable=' + ignore, filename
