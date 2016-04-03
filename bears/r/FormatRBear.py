9c89dc81eac50afd725de87b16c5ab2c4f852423
Makman2 <mak95@arcor.de>
2016-04-03 22:23:24 +0200

    FormatRBear: Refactor to Linter v2 API

--- bears/r/FormatRBear.py
+++ bears/r/FormatRBear.py
@@ -1,24 +1,25 @@
-from coalib.bearlib.abstractions.Lint import Lint
-from coalib.bears.LocalBear import LocalBear
+from coalib.bearlib.abstractions.Linter import linter
 
 
-class FormatRBear(Lint, LocalBear):
-    executable = "Rscript"
-    arguments = ("-e 'library(formatR)' "
-                 "-e 'formatR::tidy_source(\"{filename}\")'")
-    prerequisite_command = ['Rscript', '-e', "'library(formatR)'"]
-    prerequisite_fail_msg = "Please install formatR for this bear to work."
-    diff_message = "Formatting can be improved."
-    gives_corrected = True
+@linter(executable='Rscript',
+        prerequisite_check_command=('Rscript', '-e', '"library(formatR)"'),
+        prerequisite_check_fail_message='Please install formatR for this bear '
+                                        'to work.')
+class FormatRBear:
+    """
+    This bear checks and corrects formatting of R Code using known formatR
+    utility.
+    """
     LANGUAGES = "R"
 
-    def process_output(self, output, filename, file):
-        output = output[:-1] + (output[-1].strip()+"\n",)
-        return Lint.process_output(self, output, filename, file)
+    @staticmethod
+    def create_arguments(filename, file, config_file):
+        return ('-e', 'library(formatR)', '-e',
+                'formatR::tidy_source(' + filename + ')')
 
-    def run(self, filename, file):
-        '''
-        This bear checks and corrects formatting of R Code using
-        known formatR utility.
-        '''
-        return self.lint(filename, file)
+    def process_output(self, output, filename, file):
+        output = output.splitlines(True)
+        output = "".join(output[:-1] + [output[-1].strip() + "\n"])
+        return self.process_corrected_output_format(
+            output, filename, file,
+            diff_message='Formatting can be improved.')
