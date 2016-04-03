c9c3dbffd91af9ce63d0c2a8be7c846398f72656
Makman2 <mak95@arcor.de>
2016-04-03 22:22:44 +0200

    PyUnusedCodeBear: Remove Lint dependency
    For generating diff patches only using Lint is an overkill, also the
    current code bypasses 99% of the Lint code.

--- bears/python/PyUnusedCodeBear.py
+++ bears/python/PyUnusedCodeBear.py
@@ -1,17 +1,12 @@
 import autoflake
 
-from coalib.bearlib.abstractions.Lint import Lint
 from coalib.bears.LocalBear import LocalBear
+from coalib.results.Diff import Diff
+from coalib.results.Result import Result
 
 
-class PyUnusedCodeBear(Lint, LocalBear):
-    diff_message = "This file contains unused source code."
-    gives_corrected = True
+class PyUnusedCodeBear(LocalBear):
     LANGUAGES = ("Python", "Python 2", "Python 3")
-
-    def lint(self, filename, file):
-        output = autoflake.fix_code(''.join(file)).splitlines(True)
-        return self.process_output(output, filename, file)
 
     def run(self, filename, file):
         """
@@ -20,4 +15,10 @@ class PyUnusedCodeBear(Lint, LocalBear):
         - Unneeded pass statements.
         - Unneeded builtin imports. (Others might have side effects.)
         """
-        return self.lint(filename, file)
+        corrected = autoflake.fix_code(''.join(file)).splitlines(True)
+
+        for diff in Diff.from_string_arrays(file, corrected).split_diff():
+            yield Result(self,
+                         "This file contains unused source code.",
+                         affected_code=(diff.range(filename),),
+                         diffs={filename: diff})
