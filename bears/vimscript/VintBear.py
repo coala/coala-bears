d8362183e6a0b8b2f207e43889e15763b467f5a5
Makman2 <mak95@arcor.de>
2016-04-03 22:20:02 +0200

    VintBear: Refactor to Linter v2 API

--- bears/vimscript/VintBear.py
+++ bears/vimscript/VintBear.py
@@ -1,18 +1,13 @@
-import re
-
-from coalib.bearlib.abstractions.Lint import Lint
-from coalib.bears.LocalBear import LocalBear
+from coalib.bearlib.abstractions.Linter import linter
 
 
-class VintBear(LocalBear, Lint):
-    executable = 'vint'
-    arguments = '{filename}'
-    output_regex = re.compile(
-        r'(?P<filename>.+):(?P<line>\d+):'
-        r'(?P<column>\d+): (?P<message>.+)')
+@linter(executable='vint',
+        output_regex=r'.+:(?P<line>\d+):(?P<column>\d+): (?P<message>.+)')
+class VintBear:
+    """
+    Checks vimscript code for possible problems using ``vint-linter``.
+    """
 
-    def run(self, filename, file):
-        '''
-        Checks vimscript code for possible problems using ``vint-linter``.
-        '''
-        return self.lint(filename)
+    @staticmethod
+    def create_arguments(filename, file, config_file):
+        return filename,
