cee2aeb2821d50b5eae0a77695f7e9ab5a85a36b
Makman2 <mak95@arcor.de>
2016-04-03 22:23:51 +0200

    PEP8Bear: Stop using Lint
    Simpler.

--- bears/python/PEP8Bear.py
+++ bears/python/PEP8Bear.py
@@ -1,24 +1,15 @@
 import autopep8
 
-from coalib.bearlib.abstractions.Lint import Lint
 from coalib.bearlib.spacing.SpacingHelper import SpacingHelper
 from coalib.bears.LocalBear import LocalBear
+from coalib.results.Diff import Diff
+from coalib.results.Result import Result
 from coalib.settings.Setting import typed_list
 
 
-class PEP8Bear(Lint, LocalBear):
-    diff_message = "The code does not comply to PEP8."
-    gives_corrected = True
+class PEP8Bear(LocalBear):
 
-    def lint(self, filename, file, apply_config, **kwargs):
-        new_file = autopep8.fix_code(''.join(file), options=kwargs,
-                                     apply_config=apply_config)
-        output = new_file.splitlines(True)
-        return self.process_output(output, filename, file)
-
-    def run(self,
-            filename,
-            file,
+    def run(self, filename, file,
             max_line_length: int=80,
             tab_width: int=SpacingHelper.DEFAULT_TAB_WIDTH,
             pep_ignore: typed_list(str)=(),
@@ -36,10 +27,19 @@ class PEP8Bear(Lint, LocalBear):
         :param local_pep8_config: Set to true if autopep8 should use a config
                                   file as if run normally from this directory.
         """
-        return self.lint(filename,
-                         file,
-                         apply_config=local_pep8_config,
-                         ignore=pep_ignore,
-                         select=pep_select,
-                         max_line_length=max_line_length,
-                         indent_size=tab_width)
+        options = {"ignore": pep_ignore,
+                   "select": pep_select,
+                   "max_line_length": max_line_length,
+                   "indent_size": tab_width}
+
+        corrected = autopep8.fix_code(''.join(file),
+                                      apply_config=local_pep8_config,
+                                      options=options).splitlines(True)
+
+        diffs = Diff.from_string_arrays(file, corrected).split_diff()
+
+        for diff in diffs:
+            yield Result(self,
+                         "The code does not comply to PEP8.",
+                         affected_code=(diff.range(filename),),
+                         diffs={filename: diff})
