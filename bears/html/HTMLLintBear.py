8b1835d4c4bb185b22944dff2af522330c7a33ab
Makman2 <mak95@arcor.de>
2016-04-03 22:26:42 +0200

    HTMLLintBear: Refactor to Linter v2 API

--- bears/html/HTMLLintBear.py
+++ bears/html/HTMLLintBear.py
@@ -1,33 +1,20 @@
-import re
-
-from coalib.bearlib.abstractions.Lint import Lint
-from coalib.bears.LocalBear import LocalBear
-from coalib.results.RESULT_SEVERITY import RESULT_SEVERITY
+from coalib.bearlib.abstractions.Linter import linter
 from coalib.settings.Setting import typed_list
 
 
-class HTMLLintBear(LocalBear, Lint):
-    executable = 'html_lint.py'
-    output_regex = re.compile(
-        r'(?P<line>\d+):(?P<column>\d+):\s'
-        r'(?P<severity>Error|Warning|Info):\s(?P<message>.+)'
-    )
-    severity_map = {
-        "Info": RESULT_SEVERITY.INFO,
-        "Warning": RESULT_SEVERITY.NORMAL,
-        "Error": RESULT_SEVERITY.MAJOR
-    }
+@linter(executable='html_lint.py', output_format='regex',
+        output_regex=r'(?P<line>\d+):(?P<column>\d+): '
+                     r'(?P<severity>Error|Warning|Info): (?P<message>.+)')
+class HTMLLintBear:
+    """
+    Checks the code with ``html_lint.py`` on each file separately.
+    """
 
-    def run(self,
-            filename,
-            file,
-            htmllint_ignore: typed_list(str)=[]):
-        '''
-        Checks the code with `html_lint.py` on each file separately.
-
+    @staticmethod
+    def create_arguments(filename, file, config_file,
+                         htmllint_ignore: typed_list(str)=()):
+        """
         :param htmllint_include: List of checkers to ignore.
-        '''
+        """
         ignore = ','.join(part.strip() for part in htmllint_ignore)
-        self.arguments = '--disable=' + ignore
-        self.arguments += " {filename}"
-        return self.lint(filename)
+        return '--disable=' + ignore, filename
