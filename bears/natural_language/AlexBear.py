9d22538705b36b38318bfd369f294719824b137e
Makman2 <mak95@arcor.de>
2016-04-03 22:24:29 +0200

    AlexBear: Refactor to Linter v2 API

--- bears/natural_language/AlexBear.py
+++ bears/natural_language/AlexBear.py
@@ -1,21 +1,18 @@
-import re
-
-from coalib.bearlib.abstractions.Lint import Lint
-from coalib.bears.LocalBear import LocalBear
+from coalib.bearlib.abstractions.Linter import linter
 
 
-class AlexBear(LocalBear, Lint):
-    executable = 'alex'
-    output_regex = re.compile(
-        r'\s+(?P<line>\d+):(?P<column>\d+)\-'
-        r'(?P<end_line>\d+):(?P<end_column>\d+)'
-        r'\s+(?:(?P<warning>warning))\s+(?P<message>.+)')
-    arguments = "{filename}"
+@linter(executable='alex',
+        output_format='regex',
+        output_regex=r'(?P<line>\d+):(?P<column>\d+)-(?P<end_line>\d+):'
+                     r'(?P<end_column>\d+) (?P<severity>warning) '
+                     r'(?P<message>.+)')
+class AlexBear:
+    """
+    Checks the markdown file with Alex - Catch insensitive, inconsiderate
+    writing.
+    """
     LANGUAGES = "Natural Language"
 
-    def run(self, filename, file):
-        '''
-        Checks the markdown file with Alex - Catch insensitive,
-        inconsiderate writing.
-        '''
-        return self.lint(filename)
+    @staticmethod
+    def create_arguments(filename, file, config_file):
+        return filename,
