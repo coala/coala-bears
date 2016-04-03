d363d31aca5990f666103247af1aeb8a65d4fba7
Makman2 <mak95@arcor.de>
2016-04-03 22:23:39 +0200

    PyCommentedCodeBear: Remove Lint dependency
    For generating diff patches only using Lint is an overkill, also the
    current code bypasses 99% of the Lint code.

--- bears/python/PyCommentedCodeBear.py
+++ bears/python/PyCommentedCodeBear.py
@@ -1,20 +1,21 @@
 import eradicate
 
-from coalib.bearlib.abstractions.Lint import Lint
 from coalib.bears.LocalBear import LocalBear
+from coalib.results.Diff import Diff
+from coalib.results.Result import Result
 
 
-class PyCommentedCodeBear(Lint, LocalBear):
-    gives_corrected = True
-    diff_message = "This file contains commented out source code."
+class PyCommentedCodeBear(LocalBear):
     LANGUAGES = ("Python", "Python 2", "Python 3")
-
-    def lint(self, filename, file):
-        output = list(eradicate.filter_commented_out_code(''.join(file)))
-        return self.process_output(output, filename, file)
 
     def run(self, filename, file):
         """
         Detects commented out source code in Python.
         """
-        return self.lint(filename, file)
+        corrected = tuple(eradicate.filter_commented_out_code(''.join(file)))
+
+        for diff in Diff.from_string_arrays(file, corrected).split_diff():
+            yield Result(self,
+                         "This file contains commented out source code.",
+                         affected_code=(diff.range(filename),),
+                         diffs={filename: diff})
