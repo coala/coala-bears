49fcc7fdd2ef11f753c872de32aa2375263a16d7
Makman2 <mak95@arcor.de>
2016-04-03 22:22:53 +0200

    PyLintBear: Refactor to Linter v2 API

--- bears/python/PyLintBear.py
+++ bears/python/PyLintBear.py
@@ -1,36 +1,34 @@
 import os
-import re
+import shlex
 
-from coalib.bearlib.abstractions.Lint import Lint
-from coalib.bears.LocalBear import LocalBear
-from coalib.misc.Shell import escape_path_argument
+from coalib.bearlib.abstractions.Linter import linter
 from coalib.results.RESULT_SEVERITY import RESULT_SEVERITY
 from coalib.settings.Setting import typed_list
 
 
-class PyLintBear(LocalBear, Lint):
-    executable = 'pylint'
-    output_regex = re.compile(r'(?P<line>\d+)\.(?P<column>\d+)'
-                              r'\|(?P<severity>[WFECRI]): (?P<message>.*)')
-    severity_map = {
-        "F": RESULT_SEVERITY.MAJOR,
-        "E": RESULT_SEVERITY.MAJOR,
-        "W": RESULT_SEVERITY.NORMAL,
-        "C": RESULT_SEVERITY.INFO,
-        "R": RESULT_SEVERITY.INFO,
-        "I": RESULT_SEVERITY.INFO}
+@linter(executable='pylint',
+        output_format='regex',
+        output_regex=r'(?P<line>\d+)\.(?P<column>\d+)\|'
+                     r'(?P<severity>[WFECRI]): (?P<message>.*)',
+        severity_map={'F': RESULT_SEVERITY.MAJOR,
+                      'E': RESULT_SEVERITY.MAJOR,
+                      'W': RESULT_SEVERITY.NORMAL,
+                      'C': RESULT_SEVERITY.INFO,
+                      'R': RESULT_SEVERITY.INFO,
+                      'I': RESULT_SEVERITY.INFO})
+class PyLintBear:
+    """
+    Checks the code with pylint. This will run pylint over each file
+    separately.
+    """
 
-    def run(self,
-            filename,
-            file,
-            pylint_disable: typed_list(str)=None,
-            pylint_enable: typed_list(str)=None,
-            pylint_cli_options: str="",
-            pylint_rcfile: str=""):
-        '''
-        Checks the code with pylint. This will run pylint over each file
-        separately.
-
+    @staticmethod
+    def create_arguments(filename, file, config_file,
+                         pylint_disable: typed_list(str)=None,
+                         pylint_enable: typed_list(str)=None,
+                         pylint_cli_options: str="",
+                         pylint_rcfile: str=""):
+        """
         :param pylint_disable:     Disable the message, report, category or
                                    checker with the given id(s).
         :param pylint_enable:      Enable the message, report, category or
@@ -38,20 +36,18 @@ class PyLintBear(LocalBear, Lint):
         :param pylint_cli_options: Any command line options you wish to be
                                    passed to pylint.
         :param pylint_rcfile:      The rcfile for PyLint.
-        '''
-        self.arguments = ('--reports=n --persistent=n '
-                          '--msg-template="{{line}}.{{column}}|{{C}}: '
-                          '{{msg_id}} - {{msg}}"')
+        """
+        args = ('--report=n', '--persistent=n',
+                '--msg-template="{line}.{column}|{C}:{msg_id} - {msg}"')
         if pylint_disable:
-            self.arguments += " --disable=" + ",".join(pylint_disable)
+            args += ('--disable=' + ','.join(pylint_disable),)
         if pylint_enable:
-            self.arguments += " --enable=" + ",".join(pylint_enable)
+            args += ('--enable=' + ','.join(pylint_enable),)
         if pylint_cli_options:
-            self.arguments += " " + pylint_cli_options
+            args += tuple(shlex.split(pylint_cli_options))
         if pylint_rcfile:
-            self.arguments += " --rcfile=" + escape_path_argument(pylint_rcfile)
+            args += '--rcfile=' + pylint_rcfile
         else:
-            self.arguments += " --rcfile=" + os.devnull
-        self.arguments += " {filename}"
+            args += '--rcfile=' + os.devnull
 
-        return self.lint(filename)
+        return args + (filename,)
