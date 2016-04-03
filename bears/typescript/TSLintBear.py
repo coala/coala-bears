eadc38d20b1fc44311e000400c51ee444ab70dc6
Makman2 <mak95@arcor.de>
2016-04-03 22:20:14 +0200

    TSLintBear: Refactor to Linter v2 API

--- bears/typescript/TSLintBear.py
+++ bears/typescript/TSLintBear.py
@@ -1,17 +1,32 @@
 import json
 
-from coalib.bearlib.abstractions.Lint import Lint
-from coalib.bears.LocalBear import LocalBear
-from coalib.misc.Shell import escape_path_argument
+from coalib.bearlib.abstractions.Linter import linter
 from coalib.results.Result import Result
 from coalib.settings.Setting import path
 
 
-class TSLintBear(LocalBear, Lint):
-    executable = 'tslint'
+@linter(executable='tslint')
+class TSLintBear:
+    """
+    Checks the code with ``tslint`` on each file separately.
+    """
+
+    @staticmethod
+    def create_arguments(filename, file, config_file,
+                         tslint_config: path="", rules_dir: path=""):
+        """
+        :param tslint_config: Path to configuration file.
+        :param rules_dir:     Rules directory
+        """
+        args = ('--format', 'json')
+        if tslint_config:
+            args += ('--config', tslint_config)
+        if rules_dir:
+            args += ('--rules-dir', rules_dir)
+        return args + (filename,)
 
     def process_output(self, output, filename, file):
-        output = json.loads("".join(output)) if output else []
+        output = json.loads(output) if output else []
         for issue in output:
             yield Result.from_values(
                 origin="{} ({})".format(self.__class__.__name__,
@@ -22,24 +37,3 @@ class TSLintBear(LocalBear, Lint):
                 end_line=int(issue["endPosition"]["line"]) + 1,
                 column=int(issue["startPosition"]["character"]) + 1,
                 end_column=int(issue["endPosition"]["character"]) + 1)
-
-    def run(self,
-            filename,
-            file,
-            tslint_config: path="",
-            rules_dir: path=""):
-        '''
-        Checks the code with ``tslint`` on each file separately.
-
-        :param tslint_config: Path to configuration file.
-        :param rules_dir:     Rules directory
-        '''
-        self.arguments = "--format json"
-        if tslint_config:
-            self.arguments += (" --config " +
-                               escape_path_argument(tslint_config))
-        if rules_dir:
-            self.arguments += (" --rules-dir " +
-                               escape_path_argument(rules_dir))
-        self.arguments += " {filename}"
-        return self.lint(filename, file)
