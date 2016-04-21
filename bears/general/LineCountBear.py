04650ca158ebaafbe280dea7432ca6e82f4bdda2
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2016-04-21 22:20:24 +0200

    LineCountBear: Warn on too big files
    Fixes https://github.com/coala-analyzer/coala-bears/issues/334

--- bears/general/LineCountBear.py
+++ bears/general/LineCountBear.py
@@ -5,12 +5,15 @@ from coalib.results.RESULT_SEVERITY import RESULT_SEVERITY
 
 class LineCountBear(LocalBear):
 
-    def run(self, filename, file, *args):
-        '''
-        Counts the lines of each file.
-        '''
-        yield Result.from_values(
-            origin=self,
-            message="This file has {count} lines.".format(count=len(file)),
-            severity=RESULT_SEVERITY.INFO,
-            file=filename)
+    def run(self, filename, file, max_lines_per_file: int):
+        """
+        Checks that files are smaller than a given size.
+
+        :param max_lines_per_file: Number of lines allowed per file.
+        """
+        if len(file) > max_lines_per_file:
+            yield Result.from_values(
+                origin=self,
+                message="This file has {count} lines.".format(count=len(file)),
+                severity=RESULT_SEVERITY.NORMAL,
+                file=filename)
