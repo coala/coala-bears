4699971bc9897cfe0506eb408ea5442554abdf75
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2016-04-25 11:12:59 +0200

    CPPCleanBear: Improve documentation

--- bears/c_languages/CPPCleanBear.py
+++ bears/c_languages/CPPCleanBear.py
@@ -6,7 +6,11 @@ from coalib.bearlib.abstractions.Linter import linter
         output_regex=r'.+:(?P<line>\d+):(?P<message>.*)')
 class CPPCleanBear:
     """
-    Checks code with ``cppclean``.
+    Find problems in C++ source code that slow down development in large code
+    bases. This includes finding unused code, among other features.
+
+    Read more about available routines at
+    <https://github.com/myint/cppclean#features>.
     """
 
     @staticmethod
