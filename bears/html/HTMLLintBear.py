d8815c3544851c93371a82bbadcc5798fb46939a
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2016-04-25 18:21:04 +0200

    HTMLLintBear: Improve documentation

--- bears/html/HTMLLintBear.py
+++ bears/html/HTMLLintBear.py
@@ -11,16 +11,20 @@ from coalib.settings.Setting import typed_list
                      r'(?P<severity>Error|Warning|Info): (?P<message>.+)')
 class HTMLLintBear:
     """
-    Checks the code with ``html_lint.py`` on each file separately.
+    Check HTML source code for invalid or misformatted code.
+
+    See also <https://pypi.python.org/pypi/html-linter>.
     """
 
     _html_lint = which('html_lint.py')
+
+    LANGUAGES = ("HTML",)
 
     @staticmethod
     def create_arguments(filename, file, config_file,
                          htmllint_ignore: typed_list(str)=()):
         """
-        :param htmllint_include: List of checkers to ignore.
+        :param htmllint_ignore: List of checkers to ignore.
         """
         ignore = ','.join(part.strip() for part in htmllint_ignore)
         return HTMLLintBear._html_lint, '--disable=' + ignore, filename
