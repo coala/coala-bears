5f07f2dc648ffea3930c0794d51fbafa3d751288
Makman2 <makman@alice.de>
2016-04-25 23:43:37 +0200

    JuliaLintBear: Improve documentation

--- bears/julia/JuliaLintBear.py
+++ bears/julia/JuliaLintBear.py
@@ -16,8 +16,12 @@ from coalib.results.RESULT_SEVERITY import RESULT_SEVERITY
                                         'install Lint.')
 class JuliaLintBear:
     """
-    Lints Julia code using ``Lint.jl``.
-    https://github.com/tonyhffong/Lint.jl
+    Provide analysis related to common bugs and potential issues in Julia like
+    dead code, undefined variable usage, duplicate keys in dicts, incorrect
+    ADT usage, wrongfully using ellipsis, and much more.
+
+    See <https://lintjl.readthedocs.org/en/stable/> for more information
+    on the analysis provided.
     """
 
     @staticmethod
