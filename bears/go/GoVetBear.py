af2adb17319b0f74c384a4c6249fc34b8cfe060a
Makman2 <makman@alice.de>
2016-04-25 23:19:05 +0200

    GoVetBear: Improve documentation

--- bears/go/GoVetBear.py
+++ bears/go/GoVetBear.py
@@ -8,7 +8,13 @@ from coalib.bearlib.abstractions.Linter import linter
         output_regex=r'.+:(?P<line>\d+): (?P<message>.*)')
 class GoVetBear:
     """
-    Checks the code using ``go vet``.
+    Analyze Go code and raise suspicious constructs, such as printf calls
+    whose arguments do not correctly match the format string, useless
+    assignments, common mistakes about boolean operations, unreachable code,
+    etc.
+
+    This is done using the ``vet`` command. For more information visit
+    <https://golang.org/cmd/vet/>.
     """
 
     @staticmethod
