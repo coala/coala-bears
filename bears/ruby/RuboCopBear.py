3ba75b7c526886d3259896990c5b405615bf9074
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2016-04-25 18:25:37 +0200

    RuboCopBear: Improve documentation

--- bears/ruby/RuboCopBear.py
+++ bears/ruby/RuboCopBear.py
@@ -9,9 +9,12 @@ from coalib.results.Result import Result
         use_stdin=True)
 class RuboCopBear:
     """
-    Checks the code with ``rubocop``. This will run ``rubocop`` over each of
-    the files separately.
+    Check Ruby code for syntactic, formatting as well as semantic problems.
+
+    See <https://github.com/bbatsov/rubocop#cops> for more information.
     """
+
+    LANGUAGES = ("Ruby",)
 
     severity_map = {"error": RESULT_SEVERITY.MAJOR,
                     "warning": RESULT_SEVERITY.NORMAL,
