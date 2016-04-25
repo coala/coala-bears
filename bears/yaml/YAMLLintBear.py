8cd0def5209cd87393063924efec4a0e53a8b80c
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2016-04-25 13:35:47 +0200

    YAMLLintBear: Improve documentation

--- bears/yaml/YAMLLintBear.py
+++ bears/yaml/YAMLLintBear.py
@@ -7,7 +7,10 @@ from coalib.bearlib.abstractions.Linter import linter
                      r'\[(?P<severity>error|warning)\] (?P<message>.+)')
 class YAMLLintBear:
     """
-    Checks the code with ``yamllint`` on each file separately.
+    Check yaml code for errors and possible problems.
+
+    You can read more about capabilities at
+    <http://yamllint.readthedocs.org/en/latest/rules.html>.
     """
 
     @staticmethod
