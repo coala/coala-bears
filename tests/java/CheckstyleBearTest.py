b8f3cd5e80ff3ab4aea4d794ba1d3a0116cf91f3
AbdealiJK <abdealikothari@gmail.com>
2016-04-26 21:07:06 +0530

    CheckstyleBear: Allow custom and known configs
    This commit adds a simple method to allow various known
    configurations of checkstyle in coala and also allows a custom
    config file for checkstyle.

--- tests/java/CheckstyleBearTest.py
+++ tests/java/CheckstyleBearTest.py
@@ -13,13 +13,21 @@ class CheckstyleBearTest(LocalBearTestHelper):
     def setUp(self):
         self.section = Section("test section")
         self.uut = CheckstyleBear(self.section, Queue())
-        self.good_file = os.path.join(os.path.dirname(__file__),
-                                      "test_files",
-                                      "CheckstyleGood.java")
-        self.bad_file = os.path.join(os.path.dirname(__file__),
-                                     "test_files",
-                                     "CheckstyleBad.java")
+        test_files = os.path.join(os.path.dirname(__file__), "test_files")
+        self.good_file = os.path.join(test_files, "CheckstyleGood.java")
+        self.bad_file = os.path.join(test_files, "CheckstyleBad.java")
+        self.empty_config = os.path.join(test_files,
+                                         "checkstyle_empty_config.xml")
 
     def test_run(self):
         self.check_validity(self.uut, [], self.good_file)
         self.check_validity(self.uut, [], self.bad_file, valid=False)
+
+    def test_known_configs(self):
+        self.section["checkstyle_configs"] = "google"
+        self.check_validity(self.uut, [], self.good_file)
+
+    def test_with_custom_configfile(self):
+        self.section["checkstyle_configs"] = self.empty_config
+        self.check_validity(self.uut, [], self.good_file)
+        self.check_validity(self.uut, [], self.bad_file)
