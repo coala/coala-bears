3e2776a2b9206c895432a120f25011e831c383d8
AbdealiJK <abdealikothari@gmail.com>
2016-04-27 06:32:44 +0530

    bears: Add VERSION and __version__
    In coalib, the version finding was moved to bears/__init__
    so that setup.py had to import the least amount of code.
    The same is done to coala-bears now.

--- bears/Constants.py
+++ bears/Constants.py
@@ -1,8 +1,6 @@
 import os
 
+from bears import VERSION
+
 # Path to the bears directory
 bears_root = os.path.dirname(__file__)
-
-VERSION_FILE = os.path.join(bears_root, "VERSION")
-with open(VERSION_FILE, 'r') as ver:
-    VERSION = ver.readline().strip()
