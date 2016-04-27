841ec1d3c31bbd6d9c0f0c20cc706294bcfc3a60
AbdealiJK <abdealikothari@gmail.com>
2016-04-27 07:01:15 +0530

    CheckstyleBear: Download style configs as needed
    Rather than packaging the checkstyle configs with coala, we now
    download them as needed and give an INFO message about us downloading
    it. This trims the fat from coala and allows us to add many more
    known configs without bulking up coala.

--- bears/java/CheckstyleBear.py
+++ bears/java/CheckstyleBear.py
@@ -1,5 +1,9 @@
+import appdirs
 import os
+from shutil import copyfileobj
+from urllib.request import urlopen
 
+from bears import VERSION
 from coalib.bearlib.abstractions.Linter import linter
 from coalib.settings.Setting import path
 
@@ -9,7 +13,7 @@ DIR_PATH = os.path.dirname(os.path.abspath(__file__))
 checkstyle_jar_file = os.path.join(DIR_PATH, 'checkstyle.jar')
 
 known_checkstyles = {
-    "google": os.path.join(DIR_PATH, 'google_checks.xml')}
+    "google": "https://raw.githubusercontent.com/checkstyle/checkstyle/master/src/main/resources/google_checks.xml"}
 
 
 def known_checkstyle_or_path(setting):
@@ -48,7 +52,31 @@ class CheckstyleBear:
             - google - To follow Google's Java configurations. More info at
               <http://checkstyle.sourceforge.net/style_configs.html>.
         """
-        checkstyle_configs = known_checkstyles.get(checkstyle_configs,
-                                                   checkstyle_configs)
+        checkstyle_configs = self.known_configs(checkstyle_configs)
 
         return ('-jar', checkstyle_jar_file, '-c', checkstyle_configs, filename)
+
+    def known_configs(self, key):
+        url = known_checkstyles.get(key, None)
+        if url is None:  # If not known, it must be custom path
+            return key
+
+        filename = os.path.join(self.data_dir, key + "_style.xml")
+        if os.path.exists(filename):
+            return filename
+
+        self.info("Downloading checkstyle configs for {key} from {url}"
+                  .format(key=key, url=url))
+        with urlopen(url) as response, open(filename, 'wb') as out_file:
+            copyfileobj(response, out_file)
+        return filename
+
+    @property
+    def data_dir(self):
+        _data_dir = os.path.abspath(os.path.join(
+            appdirs.user_data_dir('coala-bears', version=VERSION),
+            self.name))
+
+        if not os.path.isdir(_data_dir):  # pragma: no cover
+            os.makedirs(_data_dir)
+        return _data_dir
