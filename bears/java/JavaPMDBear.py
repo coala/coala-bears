c9d7edb43326a2c63cec0e4751fe65713938d969
Maximilian <m0hawk@gmx.de>
2016-04-28 11:51:48 +0200

    JavaPMDBear: Fix doc strings

--- bears/java/JavaPMDBear.py
+++ bears/java/JavaPMDBear.py
@@ -42,58 +42,32 @@ class JavaPMDBear:
                          check_unused: bool = True):
         """
         :param check_best_practices:
-            Activate to check for best practices. See
-            http://pmd.github.io/pmd-5.4.1/pmd-java/rules/index.html#Basic for
-            more information on what this contains.
+            Activate to check for best practices.
         :param check_braces:
-            Check for the right use of braces. See
-            http://pmd.github.io/pmd-5.4.1/pmd-java/rules/index.html#Braces
-            for more information on this matter.
+            Check for the right use of braces.
         :param check_clone_implementation:
-            Check for the right implementation of the ``clone()`` function. See
-            http://pmd.github.io/pmd-5.4.1/pmd-java/rules/index.html#Clone_Implementation
-            for more information on this matter.
+            Check for the right implementation of the ``clone()`` function.
         :param check_code_size:
-            Check for large or complicated code constructs. See
-            http://pmd.github.io/pmd-5.4.1/pmd-java/rules/index.html#Code_Size
-            for more information on this matter.
+            Check for large or complicated code constructs.
         :param check_comments:
-            Check comments for length, content and placement. See
-            http://pmd.github.io/pmd-5.4.1/pmd-java/rules/index.html#Comments
-            for more information on this matter.
+            Check comments for length, content and placement.
         :param check_controversial:
-            Does various checks that are considered controversial. See
-            http://pmd.github.io/pmd-5.4.1/pmd-java/rules/index.html#Controversial
-            for more information on this matter.
+            Does various checks that are considered controversial.
         :param check_design:
             Check for optimal code implementations. Alternate approaches
-            are suggested. See
-            http://pmd.github.io/pmd-5.4.1/pmd-java/rules/index.html#Design
-            for more information on this matter.
+            are suggested.
         :param check_imports:
-            Check for duplicate and unused imports. See
-            http://pmd.github.io/pmd-5.4.1/pmd-java/rules/index.html#Import_Statements
-            for more information on this matter.
+            Check for duplicate and unused imports.
         :param check_naming:
-            Check the names of identifiers against some rules. See
-            http://pmd.github.io/pmd-5.4.1/pmd-java/rules/index.html#Naming
-            for more information on this matter.
+            Check the names of identifiers against some rules.
         :param check_optimizations:
-            Check for best pratices regarding optimization. See
-            http://pmd.github.io/pmd-5.4.1/pmd-java/rules/index.html#Optimization
-            for more information on this matter.
+            Check for best pratices regarding optimization.
         :param check_strings:
-            Check for String, StringBuffer and StringBuilder instances. See
-            http://pmd.github.io/pmd-5.4.1/pmd-java/rules/index.html#String_and_StringBuffer
-            for more information on this matter.
+            Check for String, StringBuffer and StringBuilder instances.
         :param check_unnecessary:
-            Checks for unnecessary code. See
-            http://pmd.github.io/pmd-5.4.1/pmd-java/rules/index.html#Unnecessary
-            for more information on this matter.
+            Checks for unnecessary code.
         :param check_unused:
-            Check for unused code. See
-            http://pmd.github.io/pmd-5.4.1/pmd-java/rules/index.html#Unused_Code
-            for more information on this matter.
+            Check for unused code.
         """
         options = {
             "java-basic": check_best_practices,
@@ -108,8 +82,7 @@ class JavaPMDBear:
             "java-optimizations": check_optimizations,
             "java-strings": check_strings,
             "java-unnecessary": check_unnecessary,
-            "java-unusedcode": check_unused
-        }
+            "java-unusedcode": check_unused}
         rules = ','.join(key for key in options if options[key])
 
         return which("run.sh"), "pmd", "-R", rules, "-d", filename
