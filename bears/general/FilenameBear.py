ded7254f98c89f0424305b06c6ed439ede5d8afb
Makman2 <makman@alice.de>
2016-05-29 10:08:05 +0200

    bears: Add FilenameBear
    Checks for filenames following certain naming conventions.

---
+++ bears/general/FilenameBear.py
@@ -0,0 +1,45 @@
+import os.path
+
+from coalib.bears.LocalBear import LocalBear
+from coalib.results.Diff import Diff
+from coalib.results.Result import Result
+from coalib.bearlib.naming_conventions import (
+    to_camelcase, to_pascalcase, to_snakecase)
+
+
+class FilenameBear(LocalBear):
+    LANGUAGES = "All"
+
+    _naming_convention = {"camel": to_camelcase,
+                          "pascal": to_pascalcase,
+                          "snake": to_snakecase}
+
+    def run(self, filename, file, file_naming_convention: str="snake"):
+        """
+        Checks whether the filename follows a certain naming-convention.
+
+        :param file_naming_convention:
+            The naming-convention. Supported values are:
+            - ``camel`` (``thisIsCamelCase``)
+            - ``pascal`` (``ThisIsPascalCase``)
+            - ``snake`` (``this_is_snake_case``)
+        """
+        head, tail = os.path.split(filename)
+        filename_without_extension, extension = os.path.splitext(tail)
+        try:
+            new_name = self._naming_convention[file_naming_convention](
+                filename_without_extension)
+        except KeyError:
+            self.err("Invalid file-naming-convention provided: " +
+                     file_naming_convention)
+            return
+
+        if new_name != filename_without_extension:
+            diff = Diff(file, rename=os.path.join(head, new_name + extension))
+
+            yield Result(
+                self,
+                "Filename does not follow {} naming-convention.".format(
+                    file_naming_convention),
+                diff.affected_code(filename),
+                diffs={filename: diff})
---
+++ tests/general/FilenameBearTest.py
@@ -0,0 +1,46 @@
+from queue import Queue
+
+from bears.general.FilenameBear import FilenameBear
+from tests.LocalBearTestHelper import LocalBearTestHelper
+from coalib.output.printers.LOG_LEVEL import LOG_LEVEL
+from coalib.settings.Section import Section
+
+
+class SpaceConsistencyBearTest(LocalBearTestHelper):
+
+    def setUp(self):
+        self.section = Section("test section")
+        self.uut = FilenameBear(self.section, Queue())
+
+    def test_invalid_naming_convention(self):
+        self.section["file_naming_convention"] = "INVALID"
+        self.uut.execute("filename", [])
+
+        # Automatic bear messages we want to ignore.
+        self.uut.message_queue.get(timeout=1)
+
+        log = self.uut.message_queue.get(timeout=1)
+        self.assertEqual(log.message,
+                         "Invalid file-naming-convention provided: INVALID")
+        self.assertEqual(log.log_level, LOG_LEVEL.ERROR)
+
+    def test_snake_case(self):
+        self.check_validity(self.uut, [""], filename="/Home/xyz/x_y.py")
+        self.check_validity(self.uut, [""], filename="XYZ/__init__.py")
+
+        self.section["file_naming_convention"] = "snake"
+        self.check_validity(self.uut, [""], filename="/a/camCase", valid=False)
+
+    def test_camel_case(self):
+        self.section["file_naming_convention"] = "camel"
+        self.check_validity(self.uut, [""], filename="/Home/xyz/x_y.py",
+                            valid=False)
+        self.check_validity(self.uut, [""], filename="XYZ/__init__.py")
+        self.check_validity(self.uut, [""], filename="/a/camCase")
+
+    def test_pascal_case(self):
+        self.section["file_naming_convention"] = "pascal"
+        self.check_validity(self.uut, [""], filename="/Home/xyz/x_y.py",
+                            valid=False)
+        self.check_validity(self.uut, [""], filename="XYZ/__Init__.py")
+        self.check_validity(self.uut, [""], filename="/a/PascalCase")
