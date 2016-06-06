09fcb4270428c882019d35f385ae1a5e73969f52
Makman2 <makman@alice.de>
2016-06-05 17:55:53 -0700

    RubySmellBear: Catch more smells
    Fixes https://github.com/coala-analyzer/coala/issues/2181

--- tests/ruby/RubySmellBearTest.py
+++ tests/ruby/RubySmellBearTest.py
@@ -7,13 +7,22 @@ class Something;
 end
 """.splitlines(True)
 
-bad_file = ('class Something; end',)
+bad_file1 = ('class Something; end',)
 
+bad_file2 = """class Dirty
+  # This method smells of :reek:NestedIterators but ignores them
+  def awful(x, y, offset = 0, log = false)
+    puts @screen.title
+    @screen = widgets.map { |w| w.each { |key| key += 3 * x } }
+    puts @screen.contents
+  end
+end
+""".splitlines(True)
 
 RubySmellBearTest = verify_local_bear(RubySmellBear,
                                       valid_files=(good_file, ('',)),
-                                      invalid_files=(bad_file,))
+                                      invalid_files=(bad_file1, bad_file2))
 
 RubySmellBearConfigTest = verify_local_bear(
-    RubySmellBear, valid_files=(good_file, bad_file), invalid_files=(),
+    RubySmellBear, valid_files=(good_file, bad_file1), invalid_files=(),
     settings={'missing_module_description': "nope"})
