c9989bcc8abb89985ada8d054d0187bcfdc63233
AsnelChristian <ngoullasob@gmail.com>
2016-06-11 22:00:38 +0100

    FormatRBear: Add configs to tidy_source
    Fixes https://github.com/coala-analyzer/coala-bears/issues/376

--- bears/r/FormatRBear.py
+++ bears/r/FormatRBear.py
@@ -1,5 +1,10 @@
 from coalib.bearlib.abstractions.Linter import linter
+from coalib.bearlib.spacing.SpacingHelper import SpacingHelper
 from coalib.parsing.StringProcessing import escape
+
+
+def _map_to_r_bool(py_bool):
+    return 'TRUE' if py_bool else 'FALSE'
 
 
 @linter(executable='Rscript',
@@ -14,8 +19,67 @@ class FormatRBear:
     LANGUAGES = "R"
 
     @staticmethod
-    def create_arguments(filename, file, config_file):
+    def create_arguments(filename, file, config_file,
+                         r_keep_comments: bool=True,
+                         r_keep_blank_lines: bool=True,
+                         r_braces_on_next_line: bool=False,
+                         r_use_arrows: bool=False,
+                         tab_width: int=SpacingHelper.DEFAULT_TAB_WIDTH,
+                         r_max_expression_length: int=20):
+        """
+        :param r_keep_comments:
+            Determines whether comments are kept or not.
+        :param r_keep_blank_lines:
+            Determines whether blank lines are kept or not.
+        :param r_braces_on_next_line:
+            Determines whether a brace should be placed on the next line.
+
+            Example:
+            If ``True``,
+            ```
+            if (...) {
+            ```
+            changes to
+            ```
+            if (...)
+            {
+            ```
+            If ``False`` the brace is placed on the same line.
+        :param r_use_arrows:
+            Determines if either the assign operator ``=`` or the arrow ``<-``
+            should be used.
+
+            Example: If  ``True``, ``x = 1`` changes to ``x <- 1``.
+        :param tab_width:
+            Number of spaces for indentation.
+        :param r_max_expression_length:
+            Maximum number of characters for an expression.
+
+            Example: If ``20`` then
+            ```
+            1 + 1 + 1 + 1 + 1 + 1 + 1
+            ```
+            changes to
+            ```
+            1 + 1 + 1 + 1 + 1 + 1 +
+                1
+            ```
+        """
         rcode = ('library(formatR);'
-                 'formatR::tidy_source(source="' +
-                 escape(filename, '"\\') + '")')
+                 'formatR::tidy_source('
+                 'source="' + escape(filename, '"\\') + '",'
+                 'comment={r_keep_comments},'
+                 'blank={r_keep_blank_lines},'
+                 'arrow={r_use_arrows},'
+                 'width.cutoff={r_max_expression_length},'
+                 'brace.newline={r_braces_on_next_line},'
+                 'indent={tab_width}'
+                 ')'.format(tab_width=tab_width,
+                            r_keep_comments=_map_to_r_bool(r_keep_comments),
+                            r_keep_blank_lines=_map_to_r_bool(
+                                r_keep_blank_lines),
+                            r_use_arrows=_map_to_r_bool(r_use_arrows),
+                            r_braces_on_next_line=_map_to_r_bool(
+                                r_braces_on_next_line),
+                            r_max_expression_length=r_max_expression_length))
         return '-e', rcode
--- tests/r/FormatRBearTest.py
+++ tests/r/FormatRBearTest.py
@@ -1,7 +1,7 @@
 from bears.r.FormatRBear import FormatRBear
 from tests.LocalBearTestHelper import verify_local_bear
 
-good_file = """1 + 1
+good_file_1 = """1 + 1
 if (TRUE) {
     x = 1  # inline comments
 } else {
@@ -10,7 +10,65 @@ if (TRUE) {
 }""".splitlines(keepends=True)
 
 
-bad_file = """1+1
+good_file_2 = """1 + 1
+if (TRUE) {
+    x <- 1  # inline comments
+} else {
+    x <- 2
+    print("Oh no... ask the right bracket to go away!")
+}""".splitlines(keepends=True)
+
+
+good_file_3 = """1 + 1
+if (TRUE) {
+    x = 1
+} else {
+    x = 2
+    print("Oh no... ask the right bracket to go away!")
+}""".splitlines(keepends=True)
+
+
+good_file_4 = """1 + 1
+if (TRUE)
+{
+    x = 1  # inline comments
+} else
+{
+    x = 2
+    print("Oh no... ask the right bracket to go away!")
+}""".splitlines(keepends=True)
+
+
+good_file_5 = """1 + 1
+if (TRUE) {
+        x = 1  # inline comments
+} else {
+        x = 2
+        print("Oh no... ask the right bracket to go away!")
+}""".splitlines(keepends=True)
+
+
+good_file_6 = """x = (1 + 1 + 1 + 1 + 1 + 1 +\x20
+    1 + 1 + 1)
+if (TRUE) {
+    x = 1  # inline comments
+} else {
+    x = 2
+    print("Oh no... ask the right bracket to go away!")
+}
+""".splitlines(keepends=True)
+
+
+bad_file_1 = """1+1
+if(TRUE){
+x=1  # inline comments
+}else{x=2;print("Oh no... ask the right bracket to go away!")}
+""".splitlines(keepends=True)
+
+
+bad_file_2 = """1+1
+
+
 if(TRUE){
 x=1  # inline comments
 }else{
@@ -18,6 +76,56 @@ x=2;print("Oh no... ask the right bracket to go away!")}
 """.splitlines(keepends=True)
 
 
-FormatRBearTest = verify_local_bear(FormatRBear,
-                                    valid_files=(good_file,),
-                                    invalid_files=(bad_file,))
+bad_file_3 = """x=(1+1+1+1+1+1+1+1+1)
+if(TRUE){
+x=1  # inline comments
+}else{x=2;print("Oh no... ask the right bracket to go away!")}
+""".splitlines(keepends=True)
+
+
+FormatRBearTest = verify_local_bear(
+    FormatRBear,
+    valid_files=(good_file_1,),
+    invalid_files=(bad_file_1,))
+
+
+FormatRBearRArrowToEqualTest = verify_local_bear(
+    FormatRBear,
+    valid_files=(good_file_2,),
+    invalid_files=(bad_file_1,),
+    settings={"r_use_arrows": "true"})
+
+
+FormatRBearRKeepCommentsTest = verify_local_bear(
+    FormatRBear,
+    valid_files=(good_file_3,),
+    invalid_files=(good_file_1,),
+    settings={"r_keep_comments": "false"})
+
+
+FormatRBearRBraceToNewlineTest = verify_local_bear(
+    FormatRBear,
+    valid_files=(good_file_4,),
+    invalid_files=(bad_file_1,),
+    settings={"r_braces_on_next_line": "true"})
+
+
+FormatRBearRKeepBlanklinesTest = verify_local_bear(
+    FormatRBear,
+    valid_files=(good_file_1,),
+    invalid_files=(bad_file_2,),
+    settings={"r_keep_blank_lines": "false"})
+
+
+FormatRBearRTabwidthTest = verify_local_bear(
+    FormatRBear,
+    valid_files=(good_file_5,),
+    invalid_files=(bad_file_1,),
+    settings={"tab_width": "8"})
+
+
+FormatRBearRWidthcutoffTest = verify_local_bear(
+    FormatRBear,
+    valid_files=(good_file_6,),
+    invalid_files=(bad_file_3,),
+    settings={"r_max_expression_length": "25"})
