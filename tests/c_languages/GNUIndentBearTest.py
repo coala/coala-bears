e297bbb438d48ce24fd1043b716f225ba4b93f88
AsnelChristian <ngoullasob@gmail.com>
2016-06-13 19:14:10 +0100

    GNUIndentBear: Add more configs
    Closes https://github.com/coala-analyzer/coala-bears/issues/373

--- tests/c_languages/GNUIndentBearTest.py
+++ tests/c_languages/GNUIndentBearTest.py
@@ -8,6 +8,7 @@ main ()
     return 0;
 }""".splitlines(keepends=True)
 
+
 test_file2 = """
 int
 main ()
@@ -30,6 +31,61 @@ int main() {
 }""".splitlines(keepends=True)
 
 
+test_file5 = """
+int
+main ()
+{
+
+  int a;
+  return 0;
+}""".splitlines(keepends=True)
+
+
+test_file6 = """
+int
+main ()
+{
+
+    int a;
+
+    return 0;
+}""".splitlines(keepends=True)
+
+
+test_file7 = """
+int
+main ()
+{
+    int a;
+
+    return 0;
+}""".splitlines(keepends=True)
+
+
+test_file8 = """
+if (bool)
+{
+    return a;
+}
+else
+{
+}""".splitlines(keepends=True)
+
+
+test_file9 = """
+if (bool) {
+    return a;
+} else {
+}""".splitlines(keepends=True)
+
+
+test_file10 = """
+int main()
+{
+    return 0;
+}""".splitlines(keepends=True)
+
+
 GNUIndentBearTest = verify_local_bear(
     GNUIndentBear,
     valid_files=(test_file1,),
@@ -49,3 +105,38 @@ GNUIndentBearGNUIndentWidthTest = verify_local_bear(
     valid_files=(test_file2,),
     invalid_files=(test_file1, test_file3, test_file4),
     settings={"use_spaces": "true", "max_line_length": "80", "tab_width": "2"})
+
+
+GNUIndentBearBlankLineAfterDeclarationsTest = verify_local_bear(
+    GNUIndentBear,
+    valid_files=(test_file6,),
+    invalid_files=(test_file5,),
+    settings={"blank_lines_after_declarations": "true"})
+
+
+GNUIndentBearBraceOnIfLineAndCuddlElseTest = verify_local_bear(
+    GNUIndentBear,
+    valid_files=(test_file9,),
+    invalid_files=(test_file8,),
+    settings={"braces_on_if_line": "true", "cuddle_else": "true"})
+
+
+GNUIndentBearDeleteOptionalBlankLinesTest = verify_local_bear(
+    GNUIndentBear,
+    valid_files=(test_file7,),
+    invalid_files=(test_file5,),
+    settings={"delete_optional_blank_lines": "false"})
+
+
+GNUIndentBearGNUStyleTest = verify_local_bear(
+    GNUIndentBear,
+    valid_files=(test_file2,),
+    invalid_files=(test_file4,),
+    settings={"gnu_style": "true"})
+
+
+GNUIndentBearKandRStyleTest = verify_local_bear(
+    GNUIndentBear,
+    valid_files=(test_file10,),
+    invalid_files=(test_file1,),
+    settings={"k_and_r_style": "true"})
