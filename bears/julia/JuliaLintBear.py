3dc1191586f806f3fee9037cfb774f1116565d10
adrian.zatreanu <zgadi_2007@yahoo.com>
2016-06-19 23:38:43 +0300

    JuliaLintBear: Add CAN_DETECT

--- bears/julia/JuliaLintBear.py
+++ bears/julia/JuliaLintBear.py
@@ -27,6 +27,8 @@ class JuliaLintBear:
     AUTHORS = {'The coala developers'}
     AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
     LICENSE = 'AGPL-3.0'
+    CAN_DETECT = {'Unused Code', 'Syntax', 'Simplification', 'Code Duplication',
+                  'Unreachable Code', 'Security', 'Formatting'}
 
     @staticmethod
     def create_arguments(filename, file, config_file):
