1db8addd15830541633a61ee99b4abc0f057f017
adrian.zatreanu <zgadi_2007@yahoo.com>
2016-06-19 22:33:14 +0300

    RubySmellBear: Add CAN_DETECT

--- bears/ruby/RubySmellBear.py
+++ bears/ruby/RubySmellBear.py
@@ -20,6 +20,7 @@ class RubySmellBear:
     AUTHORS = {'The coala developers'}
     AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
     LICENSE = 'AGPL-3.0'
+    CAN_DETECT = {'Smell'}
 
     @staticmethod
     def create_arguments(filename, file, config_file):
