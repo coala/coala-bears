e7d841a4df1ad289a7dd30afdedc896aaefc279f
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2016-06-19 20:32:19 +0200

    CPPCheckBear: Add CAN_DETECT info

--- bears/c_languages/CPPCheckBear.py
+++ bears/c_languages/CPPCheckBear.py
@@ -22,6 +22,7 @@ class CPPCheckBear:
     AUTHORS = {'The coala developers'}
     AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
     LICENSE = 'AGPL-3.0'
+    CAN_DETECT = {'Security', 'Redundancy', 'Smell'}
 
     @staticmethod
     def create_arguments(filename, file, config_file,
