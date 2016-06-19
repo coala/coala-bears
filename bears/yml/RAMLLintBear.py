19df232461679b3156f9d5889d59f095e0b97d60
adrian.zatreanu <zgadi_2007@yahoo.com>
2016-06-19 22:18:04 +0300

    RAMLLintBear: Add CAN_DETECT

--- bears/yml/RAMLLintBear.py
+++ bears/yml/RAMLLintBear.py
@@ -19,6 +19,7 @@ class RAMLLintBear:
     AUTHORS = {'The coala developers'}
     AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
     LICENSE = 'AGPL-3.0'
+    CAN_DETECT = {'Syntax', 'Formatting'}
 
     @staticmethod
     def create_arguments(filename, file, config_file):
