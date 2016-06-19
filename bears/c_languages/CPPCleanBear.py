2b1d1dd0a5f2ea8a67ad40e13b9bd62a3ebbf2ea
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2016-06-19 20:40:08 +0200

    CPPCleanBear: Add CAN_DETECT info

--- bears/c_languages/CPPCleanBear.py
+++ bears/c_languages/CPPCleanBear.py
@@ -19,6 +19,7 @@ class CPPCleanBear:
     AUTHORS = {'The coala developers'}
     AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
     LICENSE = 'AGPL-3.0'
+    CAN_DETECT = {'Smell', 'Unused Code', 'Security'}
 
     @staticmethod
     def create_arguments(filename, file, config_file):
