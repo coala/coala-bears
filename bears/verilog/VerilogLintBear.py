08c30d1ba7d32373e18133827cc52efe3ccf035b
adrian.zatreanu <zgadi_2007@yahoo.com>
2016-06-19 22:22:10 +0300

    VerilogLintBear: Add CAN_DETECT

--- bears/verilog/VerilogLintBear.py
+++ bears/verilog/VerilogLintBear.py
@@ -24,6 +24,7 @@ class VerilogLintBear:
     AUTHORS = {'The coala developers'}
     AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
     LICENSE = 'AGPL-3.0'
+    CAN_DETECT = {'Formatting', 'Simplification', 'Syntax', 'Unused Code'}
 
     @staticmethod
     def create_arguments(filename, file, config_file):
