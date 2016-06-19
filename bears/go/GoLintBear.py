fad957057219f2685732742f05a67a89d8c83827
adrian.zatreanu <zgadi_2007@yahoo.com>
2016-06-19 23:52:00 +0300

    GoLintBear: Add CAN_DETECT

--- bears/go/GoLintBear.py
+++ bears/go/GoLintBear.py
@@ -15,6 +15,7 @@ class GoLintBear:
     AUTHORS = {'The coala developers'}
     AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
     LICENSE = 'AGPL-3.0'
+    CAN_DETECT = {'Formatting'}
 
     @staticmethod
     def create_arguments(filename, file, config_file,
