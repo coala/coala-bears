b1d5c25bc3d85221d7a5a2b4face53dd66293156
adrian.zatreanu <zgadi_2007@yahoo.com>
2016-06-19 23:30:15 +0300

    LanguageToolBear: Add CAN_DETECT

--- bears/natural_language/LanguageToolBear.py
+++ bears/natural_language/LanguageToolBear.py
@@ -15,6 +15,7 @@ class LanguageToolBear(LocalBear):
     AUTHORS = {'The coala developers'}
     AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
     LICENSE = 'AGPL-3.0'
+    CAN_DETECT = {'Spelling', 'Grammar'}
 
     @classmethod
     def check_prerequisites(cls):
