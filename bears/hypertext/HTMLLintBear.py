c3ee5837a57edd68b8d63d35279353aef5ceb9cc
adrian.zatreanu <zgadi_2007@yahoo.com>
2016-06-19 23:46:38 +0300

    HTMLLintBear: Add CAN_DETECT

--- bears/hypertext/HTMLLintBear.py
+++ bears/hypertext/HTMLLintBear.py
@@ -24,6 +24,7 @@ class HTMLLintBear:
     AUTHORS = {'The coala developers'}
     AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
     LICENSE = 'AGPL-3.0'
+    CAN_DETECT = {'Syntax', 'Formatting'}
 
     @staticmethod
     def create_arguments(filename, file, config_file,
