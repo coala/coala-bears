82e7fe24b65f3bed7d635826545bc0197e9fa50c
AsnelChristian <ngoullasob@gmail.com>
2016-06-23 22:46:42 +0100

    CoffeeLintBear: Make coffee_lint configurable
    Closes https://github.com/coala-analyzer/coala-bears/issues/516

--- bears/coffee_script/CoffeeLintBear.py
+++ bears/coffee_script/CoffeeLintBear.py
@@ -22,11 +22,280 @@ class CoffeeLintBear:
     CAN_DETECT = {'Syntax', 'Formatting', 'Smell'}
 
     severity_map = {'warn': RESULT_SEVERITY.NORMAL,
-                    'error': RESULT_SEVERITY.MAJOR}
+                    'error': RESULT_SEVERITY.MAJOR,
+                    'ignore': RESULT_SEVERITY.INFO}
 
     @staticmethod
     def create_arguments(filename, file, config_file):
-        return '--reporter=raw', filename
+        return '--reporter=raw', '-f', config_file, filename
+
+    @staticmethod
+    def generate_config(filename, file,
+                        max_line_length: int=80,
+                        max_line_length_affect_comments: bool=True,
+                        space_before_and_after_arrow: bool=True,
+                        check_braces_spacing: bool=False,
+                        braces_spacing_width: int=1,
+                        spacing_in_empty_braces: int=0,
+                        class_naming_camelCase: bool=True,
+                        spaces_before_and_after_colon: bool=False,
+                        spaces_before_colon: int=0,
+                        spaces_after_colon: int=1,
+                        enforce_newline_at_EOF: bool=True,
+                        use_spaces: bool=True,
+                        tab_width: int=2,
+                        number_of_newlines_after_classes: int=2,
+                        prohibit_embedding_javascript_snippet: bool=True,
+                        no_implicit_braces: bool=False,
+                        no_implicit_parentheses: bool=False,
+                        no_interpolation_in_single_quotes: bool=False,
+                        no_stand_alone_at_sign: bool=True,
+                        disable_throwing_strings: bool=True,
+                        allow_trailing_semicolons: bool=False,
+                        allow_trailing_whitespaces: bool=False,
+                        no_unnecessary_double_quotes: bool=False,
+                        use_english_operator: bool=False,
+                        spaces_around_operators: bool=True,
+                        space_after_comma: bool=True,
+                        cyclomatic_complexity: int=0,
+                        prevent_duplicate_keys: bool=True,
+                        consistent_line_endings_style: str='',
+                        no_this: bool=False,
+                        no_decr_or_incrementation_operators: bool=False,
+                        no_empty_parameter_list: bool=False,
+                        no_empty_functions: bool=False,
+                        enforce_parentheses_on_non_empty_constructors: bool=True
+                        ):
+        """
+        :param max_line_length:
+            Maximum number of characters per line.
+        :param max_line_length_affect_comments:
+            Determines if ``max_line_length`` should also affects comments or
+            not.
+        :param space_before_and_after_arrow:
+            Determines if spaces should be used before and after the arrow.
+        :param check_braces_spacing:
+            Checks if proper spacing is used inside curly braces.
+        :param braces_spacing_width:
+            Determines the number of blank spaces after the opening ``{`` and
+            before the closing brace ``}`` given that there is something within
+            the braces.
+        :param spacing_in_empty_braces:
+            Determines the number of blank spaces after the opening ``{`` and
+            before the closing brace ``}`` given empty content.
+        :param class_naming_camelCase:
+            Checks whether the classes name should be in camel-case or not.
+        :param spaces_before_and_after_colon:
+            Checks the number of spaces before and after colon.
+        :param spaces_before_colon:
+            Determines the number of blank spaces before colon when
+            ``spaces_before_and_after_colon == True``.
+        :param spaces_after_colon:
+            Determines the number of space after colon when
+            ``spaces_before_and_after_colon == True``.
+        :param enforce_newline_at_EOF:
+            Checks if the file ends with a single newline.
+        :param use_spaces:
+            Forbids tabs in indentation and applies two spaces for this purpose.
+        :param tab_width:
+            Length of the tab for indentation.
+        :param number_of_newlines_after_classes:
+            Determines the number of newlines that separate the class definition
+            and the rest of the code.
+        :param prohibit_embedding_javascript_snippet:
+            Prevents some JavaScript elements like ``eval`` to affect
+            CoffeeScript.
+        :param no_implicit_braces:
+            Prohibits implicit braces when declaring object literals.
+
+            Example: If ``no_implicit_braces = True`` then
+            ```
+            1:2, 3:4
+            ```
+            is prohibited, whereas
+            ```
+            {1:2, 3:4}
+            ```
+            is accepted.
+        :param no_implicit_parentheses:
+            Prohibits implicit parentheses.
+        :param no_interpolation_in_single_quotes:
+            Prohibits string interpolation in a single quoted string.
+
+            Example: If ``no_interpolation_in_single_quotes = True`` then
+            ```
+            f = '#{bar}'
+            ```
+            is prohibited, whereas
+            ```
+            f = "#{bar}"
+            ```
+            is correct.
+        :param no_stand_alone_at_sign:
+            Prohibits the use of stand alone  ``@``.
+
+            Example: If ``no_stand_alone_at_sign = True``
+            ```
+            @ notok
+            not(@).ok
+            @::
+            ```
+            are prohibited, whereas
+            ```
+            @alright
+            @(fn)
+            @ok()
+            @[ok]
+            @ok()
+            ```
+            are accepted.
+        :param disable_throwing_strings:
+            Disables throwing string literals or interpolation.
+
+            Example: If ``disable_throwing_strings = True``
+            ```
+            throw 'my error'
+            throw "#{1234}"
+            ```
+            will not be permitted.
+        :param allow_trailing_semicolons:
+            Prohibits trailing semicolons when ``False`` since they are
+            not useful. The semicolon is meaningful only if there's another
+            instruction on the same line.
+
+            Example: If ``allow_trailing_semicolon = False``
+            ```
+            x = '1234'; console.log(x)
+            ```
+            Here the semicolon is meaningful.
+            ```
+            alert('end of line');
+            ```
+            This semicolon is redundant.
+        :param allow_trailing_whitespaces:
+            Checks whether to allow trailing whitespacess in the code or not.
+        :param no_unnecessary_double_quotes:
+            Prohibits double quotes unless string interpolation is used or the
+            strings contain single quotes.
+        :param use_english_operator:
+            Determines if ``and``, ``or``, ``is`` and ``isnt`` should be used
+            instead of ``&&``, ``||``, ``==`` and ``!=``.
+        :param spaces_around_operators:
+            Enforces that operators have spaces around them.
+        :param space_after_comma:
+            Checks if there is a blank space after commas.
+        :param cyclomatic_complexity:
+            Maximum cyclomatic complexity of the file.
+        :param prevent_duplicate_keys:
+            Prevents defining duplicate keys in object literals and classes.
+        :param enforce_parentheses_on_non_empty_constructors:
+            Requires constructors with parameters to include parentheses.
+
+            Example:
+            ```
+            class Foo
+            # Warn about missing parentheses here
+            a = new Foo
+            b = new bar.foo.Foo
+            # The parentheses make it clear no parameters are intended
+            c = new Foo()
+            d = new bar.foo.Foo()
+            e = new Foo 1, 2
+            f = new bar.foo.Foo 1, 2
+            ```
+        :param consistent_line_endings_style:
+            The option to ``line_endings``, its value is either ``unix`` or
+            ``windows``.
+        :param no_this:
+            Prohibits ``this``. Use ``@`` instead.
+        :param no_decr_or_incrementation_operators:
+            Prohibits the use of increment and decrement arithmetic operators.
+        :param no_empty_parameter_list:
+            Prohibits empty parameter lists in function definitions.
+        :param no_empty_functions:
+            Prohibits declaring empty functions.
+        """
+        coffee_configs = {"max_line_length":
+                          {"value": max_line_length,
+                           "level": "error",
+                           "limitComments":
+                               max_line_length_affect_comments}}
+        coffee_configs["arrow_spacing"] = (
+            {"level": "error" if space_before_and_after_arrow else "ignore"})
+        if check_braces_spacing:
+            coffee_configs["braces_spacing"] = (
+                {"level": "error",
+                 "spaces": braces_spacing_width,
+                 "empty_object_spaces": spacing_in_empty_braces})
+        if class_naming_camelCase:
+            coffee_configs["camel_case_classes"] = {"level": "error"}
+        if spaces_before_and_after_colon:
+            coffee_configs["colon_assignment_spacing"] = (
+                {"level": "error",
+                 "spacing": {"left": spaces_before_colon,
+                             "right": spaces_after_colon}})
+        coffee_configs["eol_last"] = (
+            {"level": "error" if enforce_newline_at_EOF else "ignore"})
+        coffee_configs["newlines_after_classes"] = (
+            {"value": number_of_newlines_after_classes,
+             "level": "error"})
+        coffee_configs["no_backticks"] = (
+            {"level": "error"
+                if prohibit_embedding_javascript_snippet else "ignore"})
+        if no_implicit_braces:
+            coffee_configs["no_implicit_braces"] = (
+                {"level": "error", "strict": True})
+        if no_implicit_parentheses:
+            coffee_configs["no_implicit_parens"] = (
+                {"strict": True, "level": "error"})
+        coffee_configs["no_interpolation_in_single_quotes"] = (
+            {"level": "error"
+                if no_interpolation_in_single_quotes else "ignore"})
+        if no_stand_alone_at_sign:
+            coffee_configs["no_stand_alone_at"] = {"level": "error"}
+        if use_spaces:
+            coffee_configs["no_tabs"] = {"level": "error"}
+        coffee_configs["indentation"] = (
+            {"value": tab_width, "level": "error"})
+        coffee_configs["no_throwing_strings"] = (
+            {"level": "error" if disable_throwing_strings else "ignore"})
+        coffee_configs["no_trailing_semicolons"] = (
+            {"level": "error" if not allow_trailing_semicolons else "ignore"})
+        if not allow_trailing_whitespaces:
+            coffee_configs["no_trailing_whitespace"] = (
+                {"level": "error",
+                 "allowed_in_comments": True,
+                 "allowed_in_empty_lines": True})
+        if no_unnecessary_double_quotes:
+            coffee_configs["no_unnecessary_double_quotes"] = {"level": "error"}
+        if use_english_operator:
+            coffee_configs["prefer_english_operator"] = (
+                {"level": "error", "doubleNotLevel": "ignore"})
+        if spaces_around_operators:
+            coffee_configs["space_operators"] = {"level": "error"}
+        if space_after_comma:
+            coffee_configs["spacing_after_comma"] = {"level": "warn"}
+        coffee_configs["cyclomatic_complexity"] = (
+                {"value": cyclomatic_complexity,
+                 "level": ("error" if cyclomatic_complexity else 'ignore')})
+        coffee_configs["duplicate_key"] = (
+            {"level": "error" if prevent_duplicate_keys else "ignore"})
+        if enforce_parentheses_on_non_empty_constructors:
+            coffee_configs["non_empty_constructor_needs_parens"] = (
+                {"level": "error"})
+        if consistent_line_endings_style:
+            coffee_configs["line_endings"] = (
+                {"level": "error", "value": consistent_line_endings_style})
+        if no_this:
+            coffee_configs["no_this"] = {"level": "error"}
+        if no_decr_or_incrementation_operators:
+            coffee_configs["no_plusplus"] = {"level": "error"}
+        if no_empty_parameter_list:
+            coffee_configs["no_empty_param_list"] = {"level": "error"}
+        if no_empty_functions:
+            coffee_configs["no_empty_functions"] = {"level": "error"}
+
+        return json.dumps(coffee_configs)
 
     def process_output(self, output, filename, file):
         output = json.loads(output)
--- tests/coffee_script/CoffeeLintBearTest.py
+++ tests/coffee_script/CoffeeLintBearTest.py
@@ -11,6 +11,200 @@ class Gangster
 """
 
 
+good_file_use_english_operator = """
+1 or 1
+1 and 1
+1 isnt 1
+1 is 1
+"""
+
+
+bad_file_use_english_operator = """
+1 || 1
+1 && 1
+1 != 1
+1 == 1
+x = !y
+"""
+
+
+good_file_tab_width = """
+# Lint your CoffeeScript!
+
+class Gangster
+
+    wasItAGoodDay : () ->
+        yes
+"""
+
+
+good_file_allow_trailing_white_spaces = """
+x = 1234
+y = 1
+"""
+
+
+bad_file_allow_trailing_white_spaces = """
+x = 1234      \ny = 1
+"""
+
+
+good_file_arrow_spacing = """
+x(-> 3)
+x( -> 3)
+"""
+
+
+bad_file_arrow_spacing = """
+{x((a,b)-> 3)
+"""
+
+
+good_file_no_backticks = """
+myFunction(a, b, c)
+"""
+
+
+bad_file_no_backticks = """
+`myFunction a,b,c
+"""
+
+
+good_file_allow_trailing_semiColon = """
+x = '1234'; console.log(x)
+{spacing:true}
+"""
+
+
+bad_file_allow_trailing_semiColon = """
+alert('end of line');
+"""
+
+
+good_file_no_empyty_functions_and_parameter_list = """
+foo = (empty = (-> undefined)) -> undefined
+{  a, b  }
+{  }
+foo = 'bar'
+{  spacing : true  }
+"""
+
+
+bad_file_no_empyty_functions_and_parameter_list = """
+-> =>
+blah = () ->
+y++
+--x
+{a, b}
+{}
+foo = "bar"
+{spacing:true}
+"""
+
+
+good_file_disable_throwing_strings = """
+x++;
+x+=1
+doSomething(foo = ',',bar)
+myFunction a, b, {1:2, 3:4}
+`with(document) alert(height);`
+foo = "#{inter}foo#{polation}"
+@::
+"""
+
+
+bad_file_disable_throwing_strings = """
+throw 'my error'
+throw "#{1234}"
+throw '''
+  long string
+'''
+foo = '#{inter}foo#{polation}'
+myFunction a, b, 1:2, 3:4
+"""
+
+
+good_file_no_stand_alone_at_sign = """
+@[ok]
+@ok()
+doSomething(foo = ',', bar)\nfooBar()
+c = new Foo 1, 2
+throw 'my error'
+throw "#{1234}"
+"""
+
+
+bad_file_no_stand_alone_at_sign = """
+@ notok
+not(@).ok
+@::
+doSomething(foo = ',',bar)\nfooBar()
+"""
+
+
+good_file_no_this = """
+class Y extends X
+  constructor: ->
+    @.hello
+"""
+
+
+bad_file_no_this = """
+class Y extends X
+  constructor: ->
+    this.hello
+"""
+
+
+good_file_enforce_parentheses_on_constructors = """
+g = new Foo(1, 2)
+h = new Foo(
+  config: 'parameter'
+)
+i = new bar.foo.Foo(1, 2)
+j = new bar.foo.Foo(
+  config: 'parameter'
+)
+myFunction a, b, 1:2, 3:4
+foo = '#{bar}'
+"""
+
+
+bad_file_enforce_parentheses_on_constructors = """
+c = new Foo 1, 2
+d = new Foo
+  config: 'parameter'
+e = new bar.foo.Foo 1, 2
+f = new bar.foo.Foo
+  config: 'parameter'
+"""
+
+
+good_file_new_lines_after_classes = """
+class Foo
+  constructor: () ->
+    bla()
+  a: "b"
+  c: "d"
+
+class Bar extends Foo
+  constructor: () ->
+    bla()
+"""
+
+
+bad_file_new_lines_after_classes = """
+class Foo
+  constructor: () ->
+      bla()
+  a: "b"
+  c: "d"
+class Bar extends Foo
+  constructor: () ->
+    bla()
+"""
+
+
 warning_file = """
 # Nested string interpolation
 str = "Book by #{"#{firstName} #{lastName}".toUpperCase()}"
@@ -37,3 +231,123 @@ CoffeeLintBearTest = verify_local_bear(CoffeeLintBear,
                                        invalid_files=(warning_file,
                                                       error_file,
                                                       invalid_file))
+
+
+CoffeeLintBearUseEnglishOperatorTest = verify_local_bear(
+    CoffeeLintBear,
+    valid_files=(good_file_use_english_operator,),
+    invalid_files=(bad_file_use_english_operator,),
+    settings={"use_english_operator": "true",
+              "consistent_line_endings_style": "unix"})
+
+
+CoffeeLintBearTabWidthTest = verify_local_bear(
+    CoffeeLintBear,
+    valid_files=(good_file_tab_width,),
+    invalid_files=(good_file,),
+    settings={"use_spaces": "false", "tab_width": 4})
+
+
+CoffeeLintBearCamelCaseTest = verify_local_bear(
+    CoffeeLintBear,
+    valid_files=(good_file,),
+    invalid_files=(error_file,),
+    settings={"class_naming_camelCase": "true"})
+
+
+CoffeeLintBearAllowTrailingWhiteSpacesTest = verify_local_bear(
+    CoffeeLintBear,
+    valid_files=(good_file_allow_trailing_white_spaces,),
+    invalid_files=(bad_file_allow_trailing_white_spaces,),
+    settings={"allow_trailing_whitespaces": "false",
+              "use_english_operator": "true",
+              "spaces_around_operators": "true"})
+
+
+CoffeLintBearArrowSpacingTest = verify_local_bear(
+    CoffeeLintBear,
+    valid_files=(good_file_arrow_spacing,),
+    invalid_files=(bad_file_arrow_spacing,),
+    settings={"space_before_and_after_arrow": "true"})
+
+
+CoffeeLintBearNoBackticksTest = verify_local_bear(
+    CoffeeLintBear,
+    valid_files=(good_file_no_backticks,),
+    invalid_files=(bad_file_no_backticks,),
+    settings={"prohibit_embedding_javascript_snippet": "true",
+              "space_after_comma": "true",
+              "no_function_call_without_parentheses": "true"})
+
+
+CoffeeLintBearAllowTrailingSemiColonTest = verify_local_bear(
+    CoffeeLintBear,
+    valid_files=(good_file_allow_trailing_semiColon,),
+    invalid_files=(bad_file_allow_trailing_semiColon,),
+    settings={"allow_trailing_semicolons": "false"})
+
+
+CoffeeLintBearNoEmpytyFunctionsAndParameterListTest = verify_local_bear(
+    CoffeeLintBear,
+    valid_files=(good_file_no_empyty_functions_and_parameter_list,),
+    invalid_files=(bad_file_no_empyty_functions_and_parameter_list,),
+    settings={"no_empty_functions": "true",
+              "no_empty_parameter_list": "true",
+              "no_decr_or_incrementation_operators": "true",
+              "check_braces_spacing": "true",
+              "braces_spacing_width": 2,
+              "spacing_in_empty_braces": 2,
+              "no_unnecessary_double_quotes": "true",
+              "spaces_before_and_after_colon": "true",
+              "spaces_before_colon": 1,
+              "no_implicit_parentheses": "true"})
+
+
+CoffeeLintBearDisableThrowingStringsTest = verify_local_bear(
+    CoffeeLintBear,
+    valid_files=(good_file_disable_throwing_strings,),
+    invalid_files=(bad_file_disable_throwing_strings,),
+    settings={"disable_throwing_strings": "true",
+              "allow_trailing_semicolons": "true",
+              "allow_trailing_whitespaces": "true",
+              "spaces_around_operators": "false",
+              "space_after_comma": "false",
+              "no_implicit_braces": "true",
+              "no_implicit_parentheses": "false",
+              "prohibit_embedding_javascript_snippet": "false",
+              "no_interpolation_in_single_quotes": "true",
+              "no_stand_alone_at_sign": "false"})
+
+
+CoffeeLintBearNoStandAloneAtTest = verify_local_bear(
+    CoffeeLintBear,
+    valid_files=(good_file_no_stand_alone_at_sign),
+    invalid_files=(bad_file_no_stand_alone_at_sign,),
+    settings={"no_stand_alone_at_sign": "true",
+              "space_after_comma": "true",
+              "enforce_parentheses_on_non_empty_constructors": "false",
+              "prevent_duplicate_keys": "false",
+              "disable_throwing_strings": "false"})
+
+
+CoffeeLintBearNoThisTest = verify_local_bear(
+    CoffeeLintBear,
+    valid_files=(good_file_no_this,),
+    invalid_files=(bad_file_no_this,),
+    settings={"no_this": "true", "class_naming_camelCase": "false"})
+
+
+CoffeeLintBearEnforceParenthesesOnConstructorsTest = verify_local_bear(
+    CoffeeLintBear,
+    valid_files=(good_file_enforce_parentheses_on_constructors,),
+    invalid_files=(bad_file_enforce_parentheses_on_constructors,),
+    settings={"enforce_parentheses_on_non_empty_constructors": "true",
+              "no_function_call_without_parentheses": "false",
+              "no_interpolation_in_single_quotes": "false"})
+
+
+CoffeeLintBearNewLinesAfterClassesTest = verify_local_bear(
+    CoffeeLintBear,
+    valid_files=(good_file_new_lines_after_classes,),
+    invalid_files=(bad_file_new_lines_after_classes,),
+    settings={"number_of_newlines_after_classes": 1})
