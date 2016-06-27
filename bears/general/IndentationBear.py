4c5d5a22f4c6875901a4d01b0bae831e7dbf02c3
adrian.zatreanu <zgadi_2007@yahoo.com>
2016-06-28 01:49:52 +0300

    IndentationBear: Add attributes

--- bears/general/IndentationBear.py
+++ bears/general/IndentationBear.py
@@ -12,6 +12,11 @@ from bears.general.AnnotationBear import starts_within_ranges
 
 class IndentationBear(LocalBear):
 
+    AUTHORS = {'The coala developers'}
+    AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
+    LICENSE = 'AGPL-3.0'
+    CAN_FIX = {'Formatting'}
+
     def run(self,
             filename,
             file,
