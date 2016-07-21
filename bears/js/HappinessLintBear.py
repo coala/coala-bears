0e057fc459d4115b6bfef6e04f60a3a2a0df2cd5
Karan <karansharma1295@gmail.com>
2016-07-21 12:12:57 +0530

    bears/js: Add HappinessLintBear
    This bear uses `happiness` to
    lint `.js` files.
    
    Closes http://github.com/coala-analyzer/coala-bears/issues/588

---
+++ bears/js/HappinessLintBear.py
@@ -0,0 +1,23 @@
+from coalib.bearlib.abstractions.Linter import linter
+from coalib.bears.requirements.NpmRequirement import NpmRequirement
+
+
+@linter(executable='happiness',
+        output_format='regex',
+        output_regex=r'\s.+:(?P<line>\d+):(?P<column>\d+):(?P<message>.+)')
+class HappinessLintBear:
+    """
+    Checks JavaScript files for semantic and syntax errors using ``happiness``.
+
+    See <https://github.com/JedWatson/happiness/> for more information.
+    """
+    LANGUAGES = {"JavaScript"}
+    REQUIREMENTS = {NpmRequirement('happiness', '7.1.2')}
+    AUTHORS = {'The coala developers'}
+    AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
+    LICENSE = 'AGPL-3.0'
+    CAN_DETECT = {'Syntax'}
+
+    @staticmethod
+    def create_arguments(filename, file, config_file):
+        return filename,
---
+++ tests/js/HappinessLintBearTest.py
@@ -0,0 +1,19 @@
+from bears.js.HappinessLintBear import HappinessLintBear
+from tests.LocalBearTestHelper import verify_local_bear
+
+
+good_file = """
+var x = 2;
+if (6 > 7 !== true) console.log('done');
+console.log(x);
+"""
+
+bad_file = """
+if (options.quiet !== true)
+  console.log('done')
+var x=2
+"""
+
+HappinessLintBear = verify_local_bear(HappinessLintBear,
+                                      valid_files=(good_file,),
+                                      invalid_files=(bad_file,))
