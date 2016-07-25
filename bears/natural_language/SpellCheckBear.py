b3be8f36a8b138af4680ab5135fa24245cd5c6be
Karan <karansharma1295@gmail.com>
2016-07-25 15:34:48 +0530

    natural_language: Add SpellCheckBear
    This bear uses ``scspell`` to check for spelling mistakes.
    
    Closes https://github.com/coala-analyzer/coala-bears/issues/629

---
+++ bears/natural_language/SpellCheckBear.py
@@ -0,0 +1,24 @@
+from coalib.bearlib.abstractions.Linter import linter
+from coalib.bears.requirements.PipRequirement import PipRequirement
+
+
+@linter(executable='scspell',
+        use_stderr=True,
+        output_format='regex',
+        output_regex=r'(?P<filename>.*):(?P<line>.\d*):\s*(?P<message>.*)')
+class SpellCheckBear:
+    """
+    Lints files to check for incorrect spellings using ``scspell``.
+
+    See <https://pypi.python.org/pypi/scspell> for more information.
+    """
+    LANGUAGES = {"Natural Language"}
+    REQUIREMENTS = {PipRequirement('scspell3k', '2.0')}
+    AUTHORS = {'The coala developers'}
+    AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
+    LICENSE = 'AGPL-3.0'
+    CAN_DETECT = {'Spelling'}
+
+    @staticmethod
+    def create_arguments(filename, file, config_file):
+        return '--report-only', filename
