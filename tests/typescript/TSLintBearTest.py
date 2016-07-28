c84c2dc01a1885ba3e1e1beb84c4b3d9b61dfcef
AsnelChristian <ngoullasob@gmail.com>
2016-07-28 12:30:24 +0100

    TSLintBear: Add configs for angular
    Adds codelyzer in the process.
    codelyzer is a devdependency of tslint, for handling angularjs.

--- tests/typescript/TSLintBearTest.py
+++ tests/typescript/TSLintBearTest.py
@@ -19,14 +19,27 @@ let t = findTitle('mytitle');
 t.innerHTML = 'New title';
 """
 
+
+bad_file_component_class_suffix = """
+@Component({
+  selector: 'sg-foo-bar'
+})
+class Test {};
+"""
+
+
+good_file_component_class_suffix = """
+@Component({
+  selector: 'sg-foo-bar'
+})
+class TestComponent {};
+"""
+
+
 tslintconfig = os.path.join(os.path.dirname(__file__),
                             "test_files",
                             "tslint.json")
 
-TSLintBearWithoutConfig = verify_local_bear(TSLintBear,
-                                            valid_files=(good_file,),
-                                            invalid_files=(bad_file,),
-                                            tempfile_kwargs={"suffix": ".ts"})
 
 TSLintBearTestWithConfig = verify_local_bear(TSLintBear,
                                              valid_files=(bad_file,),
@@ -35,8 +48,15 @@ TSLintBearTestWithConfig = verify_local_bear(TSLintBear,
                                                        tslintconfig},
                                              tempfile_kwargs={"suffix": ".ts"})
 
-TSLintBearOtherOptions = verify_local_bear(TSLintBear,
-                                           valid_files=(good_file,),
-                                           invalid_files=(bad_file,),
-                                           settings={"rules_dir": "/"},
-                                           tempfile_kwargs={"suffix": ".ts"})
+TSLintBearOtherOptions = verify_local_bear(
+    TSLintBear,
+    valid_files=(good_file_component_class_suffix,),
+    invalid_files=(bad_file_component_class_suffix,),
+    settings={"rules_dir": "/"},
+    tempfile_kwargs={"suffix": ".ts"})
+
+TSLintBearCodelyzerTest = verify_local_bear(
+    TSLintBear,
+    valid_files=(good_file_component_class_suffix,),
+    invalid_files=(bad_file_component_class_suffix,),
+    tempfile_kwargs={"suffix": ".ts"})
