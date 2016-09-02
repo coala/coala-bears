fccf7ff107a133695633bf7dd2c1e254002e8882
oreqizer <oreqizer@gmail.com>
2016-09-02 17:54:22 +0200

    ESLintBear: Add filename context
    Adds information about current file location for ESLint.
    Aids in relative imports.
    
    Fixes https://github.com/coala-analyzer/coala-bears/issues/741

--- tests/js/ESLintBearTest.py
+++ tests/js/ESLintBearTest.py
@@ -24,6 +24,18 @@ test_bad = """function addOne(i) {
 };
 """
 
+test_import_good = """
+import test from "./test";
+
+test();
+"""
+
+test_import_bad = """
+import test from "../test";
+
+test();
+"""
+
 test_syntax_error = '{<!@3@^ yeah!/\n'
 
 test_dir = os.path.join(os.path.dirname(__file__), "test_files")
@@ -47,3 +59,11 @@ ESLintBearWithUnloadablePluginTest = verify_local_bear(
     invalid_files=(),
     settings={"eslint_config": os.path.join(test_dir,
                                             "eslintconfig_badplugin.json")})
+ESLintBearImportTest = verify_local_bear(
+    ESLintBear,
+    valid_files=(test_import_good, ),
+    invalid_files=(test_import_bad, ),
+    filename=os.path.join(test_dir, 'test.js'),
+    create_tempfile=False,
+    settings={"eslint_config": os.path.join(test_dir,
+                                            "eslintconfig_import.json")})
