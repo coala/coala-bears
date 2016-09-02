1da70cdea23bfea603deb39fd6459773785e3017
Stanislav Komanec <stanislav@komanec.eu>
2016-09-02 18:37:24 +0200

    KeywordBear: Remove TODO
    coala now ask us if we want to remove TODO notes
    
    Closes https://github.com/coala-analyzer/coala-bears/issues/761

--- bears/general/KeywordBear.py
+++ bears/general/KeywordBear.py
@@ -2,7 +2,68 @@ import re
 
 from coalib.bearlib import deprecate_settings
 from coalib.bears.LocalBear import LocalBear
+from coalib.results.Diff import Diff
 from coalib.results.Result import RESULT_SEVERITY, Result
+from coalib.results.SourceRange import SourceRange
+
+from bears.general.AnnotationBear import AnnotationBear
+
+
+def _get_comments(dependency_results):
+    if not dependency_results:
+        return
+
+    annotation_bear_results = dependency_results.get('AnnotationBear')
+    if (not annotation_bear_results or
+            not isinstance(annotation_bear_results, list)):
+        return
+
+    for result in annotation_bear_results:
+        yield from result.contents.get('comments', [])
+
+
+def generate_diff(comments, file, filename,
+                  line, line_number, pos):
+    todo_source_range = SourceRange.from_values(filename, line_number,
+                                                pos + 1)
+    affected_comment_sourcerange = [
+        c for c in comments if todo_source_range in c]
+
+    affected_len = len(affected_comment_sourcerange)
+
+    if affected_len == 0:
+        return {}
+    assert affected_len == 1, "More than 1 affected comment source ranges"
+
+    comment_sourcerange = affected_comment_sourcerange[0]
+
+    comment_start = comment_sourcerange.start.column
+    comment_end = comment_sourcerange.end.column
+    in_multi_line_comment = (
+        comment_sourcerange.start.line != comment_sourcerange.end.line
+    )
+
+    line_before_todo_comment = line[:comment_start - 1].rstrip()
+    line_behind_todo_comment = line[comment_end:].rstrip()
+
+    line_replacement = line_before_todo_comment + line_behind_todo_comment
+
+    diff = Diff(file)
+    if line_replacement and not in_multi_line_comment:
+        diff.change_line(
+            line_number,
+            line,
+            line_replacement + '\n')
+    elif line_replacement and in_multi_line_comment:
+        text_replacement = line[pos:]
+        diff.change_line(
+            line_number,
+            line,
+            line.replace(text_replacement, '').rstrip() + '\n')
+    else:
+        diff.delete_line(line_number)
+
+    return {filename: diff}
 
 
 class KeywordBear(LocalBear):
@@ -11,9 +72,14 @@ class KeywordBear(LocalBear):
     AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
     LICENSE = 'AGPL-3.0'
     CAN_DETECT = {'Documentation'}
+    BEAR_DEPS = {AnnotationBear}
 
     @deprecate_settings(keywords='ci_keywords')
-    def run(self, filename, file, keywords: list):
+    def run(self,
+            filename,
+            file,
+            keywords: list,
+            dependency_results: dict=None):
         '''
         Checks the code files for given keywords.
 
@@ -25,15 +91,25 @@ class KeywordBear(LocalBear):
             '(' + '|'.join(re.escape(key) for key in keywords) + ')',
             re.IGNORECASE)
 
-        for line_number, line in enumerate(file):
+        comments = _get_comments(dependency_results)
+
+        for line_number, line in enumerate(file, start=1):
             for keyword in keywords_regex.finditer(line):
+                diffs = generate_diff(
+                    comments,
+                    file,
+                    filename,
+                    line,
+                    line_number,
+                    keyword.start())
                 yield Result.from_values(
                     origin=self,
                     message="The line contains the keyword '{}'."
                             .format(keyword.group()),
                     file=filename,
-                    line=line_number + 1,
+                    line=line_number,
                     column=keyword.start() + 1,
-                    end_line=line_number + 1,
+                    end_line=line_number,
                     end_column=keyword.end() + 1,
-                    severity=RESULT_SEVERITY.INFO)
+                    severity=RESULT_SEVERITY.INFO,
+                    diffs=diffs)
--- tests/general/KeywordBearTest.py
+++ tests/general/KeywordBearTest.py
@@ -1,5 +1,13 @@
+from collections import namedtuple
+from queue import Queue
+import unittest
+
 from bears.general.KeywordBear import KeywordBear
-from tests.LocalBearTestHelper import verify_local_bear
+from coalib.results.HiddenResult import HiddenResult
+from coalib.results.SourceRange import SourceRange
+from coalib.settings.Section import Section
+from coalib.settings.Setting import Setting
+from tests.LocalBearTestHelper import verify_local_bear, execute_bear
 
 test_file = """
 test line fix me
@@ -14,3 +22,166 @@ KeywordBearTest = verify_local_bear(
     invalid_files=("test line todo",
                    "test line warNING"),
     settings={"keywords": "todo, warning"})
+
+
+class KeywordBearDiffTest(unittest.TestCase):
+
+    def setUp(self):
+        self.section = Section("")
+        self.section.append(Setting('language', 'python3'))
+        self.section.append(Setting('keywords', 'TODO'))
+        self.uut = KeywordBear(self.section, Queue())
+
+        self.annotation_bear_result_type = namedtuple('result', ['contents'])
+        self.dep_results = {'AnnotationBear': HiddenResult(
+            'AnnotationBear', {'comments': ()})}
+
+    def test_empty_keyword(self):
+        text = ['a == b']
+
+        with execute_bear(self.uut, "F", text,
+                          dependency_results=self.dep_results) as result:
+            self.assertEqual(result, [])
+
+    def test_keyword_in_comment(self):
+        dep_results = {
+            'AnnotationBear': {}
+        }
+        text = ['# todo 123']
+        with execute_bear(self.uut, "F", text,
+                          dependency_results=dep_results) as result:
+            self.assertEqual(result[0].diffs, {})
+            self.assertEqual(result[0].affected_code[0].start.line, 1)
+            self.assertEqual(len(result), 1)
+
+        dep_results = {'AnnotationBear': HiddenResult(
+            'AnnotationBear', {'comments': 123})}
+        with execute_bear(self.uut, "F", text,
+                          dependency_results=dep_results) as result:
+            self.assertEqual(result[0].diffs, {})
+            self.assertEqual(result[0].affected_code[0].start.line, 1)
+            self.assertEqual(len(result), 1)
+
+        dep_results = {'AnnotationBear': HiddenResult(
+            'AnnotationBear', 123)}
+        with execute_bear(self.uut, "F", text,
+                          dependency_results=dep_results) as result:
+            self.assertEqual(result[0].diffs, {})
+            self.assertEqual(result[0].affected_code[0].start.line, 1)
+            self.assertEqual(len(result), 1)
+
+    def test_keyword_not_in_comment(self):
+        text = ['# comment 123\n', 'a = "TODO"\n']
+        comments = [SourceRange.from_values("F", 1, 1, 1, 40)]
+        dep_results = {
+            'AnnotationBear': [
+                self.annotation_bear_result_type({'comments': comments})
+            ]
+        }
+
+        with execute_bear(self.uut, "F", text,
+                          dependency_results=dep_results) as result:
+            self.assertEqual(len(result[0].diffs), 0)
+
+    def test_keyword_diff(self):
+        text = ['# todo 123\n']
+        comments = [SourceRange.from_values("F", 1, 1, 1, 10)]
+        dep_results = {
+            'AnnotationBear': [
+                self.annotation_bear_result_type({'comments': comments})
+            ]
+        }
+
+        with execute_bear(self.uut, "F", text,
+                          dependency_results=dep_results) as result:
+            self.assertEqual(result[0].diffs['F'].unified_diff,
+                             '--- \n'
+                             '+++ \n'
+                             '@@ -1 +0,0 @@\n'
+                             '-# todo 123\n')
+
+        text = ['test = 55 # todo 123\n']
+        comments = [SourceRange.from_values("F", 1, 11, 1, 23)]
+        dep_results = {
+            'AnnotationBear': [
+                self.annotation_bear_result_type({'comments': comments})
+            ]
+        }
+        with execute_bear(self.uut, "F", text,
+                          dependency_results=dep_results) as result:
+            self.assertEqual(result[0].diffs['F'].unified_diff,
+                             '--- \n'
+                             '+++ \n'
+                             '@@ -1 +1 @@\n'
+                             '-test = 55 # todo 123\n'
+                             '+test = 55\n')
+
+    def test_keyword_outside_of_comment(self):
+        text = ['todo = 123\n']
+        with execute_bear(self.uut, "F", text,
+                          dependency_results=self.dep_results) as result:
+            self.assertEquals(result[0].diffs, {})
+
+    def test_keyword_between_code(self):
+        self.section.append(Setting('language', 'c'))
+        self.section.append(Setting('keywords', 'todo'))
+
+        text = ['int a=0; /* TODO: Test */ int b=1;\n']
+
+        comments = [SourceRange.from_values("F", 1, 10, 1, 25)]
+        dep_results = {
+            'AnnotationBear': [
+                self.annotation_bear_result_type({'comments': comments})
+            ]
+        }
+
+        with execute_bear(self.uut, "F", text,
+                          dependency_results=dep_results) as result:
+            self.assertEqual(result[0].diffs['F'].unified_diff,
+                             '--- \n'
+                             '+++ \n'
+                             '@@ -1 +1 @@\n'
+                             '-int a=0; /* TODO: Test */ int b=1;\n'
+                             '+int a=0; int b=1;\n')
+
+        text = ['int a = 0; /* TODO test\n',
+                'another test\n',
+                '*/\n']
+        comments = [SourceRange.from_values("F", 1, 12, 3, 2)]
+        dep_results = {
+            'AnnotationBear': [
+                self.annotation_bear_result_type({'comments': comments})
+            ]
+        }
+
+        with execute_bear(self.uut, "F", text,
+                          dependency_results=dep_results) as result:
+            self.assertEqual(result[0].diffs['F'].unified_diff,
+                             '--- \n'
+                             '+++ \n'
+                             '@@ -1,3 +1,3 @@\n'
+                             '-int a = 0; /* TODO test\n'
+                             '+int a = 0; /*\n'
+                             ' another test\n'
+                             ' */\n')
+
+        text = ['/* TODO\n',
+                'test\n',
+                '*/\n']
+        comments = [SourceRange.from_values("F", 1, 1, 3, 2)]
+        dep_results = {
+            'AnnotationBear': [
+                self.annotation_bear_result_type({'comments': comments})
+            ]
+        }
+
+        with execute_bear(self.uut, "F", text,
+                          dependency_results=dep_results) as result:
+            self.assertEqual(result[0].diffs['F'].unified_diff,
+                             '--- \n'
+                             '+++ \n'
+                             '@@ -1,3 +1,3 @@\n'
+                             '-/* TODO\n'
+                             '+/*\n'
+                             ' test\n'
+                             ' */\n')
