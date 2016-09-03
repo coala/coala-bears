fa70e9240283d92f64113adc571121e7cf263574
AbdealiJK <abdealikothari@gmail.com>
2016-09-03 13:17:27 +0530

    AlexBear: Check if wrong alex is installed
    Alex has a name clash with another tool in the haskell toolkit. Hence,
    we check here by reading the `--help` of `alex` and checking whether
    this is indeed the correct tool or not.
    
    Fixes https://github.com/coala-analyzer/coala-bears/issues/776

--- bears/natural_language/AlexBear.py
+++ bears/natural_language/AlexBear.py
@@ -1,3 +1,6 @@
+import subprocess
+import sys
+
 from coalib.bearlib.abstractions.Linter import linter
 from coalib.bears.requirements.NpmRequirement import NpmRequirement
 
@@ -17,6 +20,30 @@ class AlexBear:
     AUTHORS = {'The coala developers'}
     AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
     LICENSE = 'AGPL-3.0'
+
+    @classmethod
+    def check_prerequisites(cls):
+        parent_prereqs = super().check_prerequisites()
+        if parent_prereqs is not True:  # pragma: no cover
+            return parent_prereqs
+
+        incorrect_pkg_msg = (
+            "Please ensure that the package that has been installed is the "
+            "one to 'Catch insensitive, inconsiderate writing'. This can be "
+            "verified by running `alex --help` and seeing what it does.")
+        try:
+            output = subprocess.check_output(("alex", "--help"),
+                                             stderr=subprocess.STDOUT)
+        except (OSError, subprocess.CalledProcessError):
+            return ("The `alex` package could not be verified. " +
+                    incorrect_pkg_msg)
+        else:
+            output = output.decode(sys.getfilesystemencoding())
+            if "Catch insensitive, inconsiderate writing" in output:
+                return True
+            else:
+                return ("The `alex` package that's been installed seems to "
+                        "be incorrect. " + incorrect_pkg_msg)
 
     @staticmethod
     def create_arguments(filename, file, config_file):
--- tests/natural_language/AlexBearTest.py
+++ tests/natural_language/AlexBearTest.py
@@ -1,4 +1,8 @@
+import unittest
+from unittest.mock import patch
+
 from bears.natural_language.AlexBear import AlexBear
+from tests.BearTestHelper import generate_skip_decorator
 from tests.LocalBearTestHelper import verify_local_bear
 
 good_file = "Their network looks good."
@@ -9,3 +13,26 @@ bad_file = "His network looks good."
 AlexBearTest = verify_local_bear(AlexBear,
                                  valid_files=(good_file,),
                                  invalid_files=(bad_file,))
+
+
+@generate_skip_decorator(AlexBear)
+@patch('bears.natural_language.AlexBear.subprocess.check_output')
+class AlexBearPrereqTest(unittest.TestCase):
+
+    def test_unverified_alex_installed(self, check_output_mock):
+        check_output_mock.side_effect = OSError
+        self.assertIn('The `alex` package could not be verified',
+                      AlexBear.check_prerequisites())
+
+    def test_wrong_alex_installed(self, check_output_mock):
+        check_output_mock.return_value = b'Unexpected output from package'
+        self.assertIn("The `alex` package that's been installed seems to "
+                      "be incorrect",
+                      AlexBear.check_prerequisites())
+
+    def test_right_alex_installed(self, check_output_mock):
+        check_output_mock.return_value = (
+            b'Some text here\n'
+            b'  Catch insensitive, inconsiderate writing\n'
+            b'Usage instructions and examples here ....')
+        self.assertTrue(AlexBear.check_prerequisites())
