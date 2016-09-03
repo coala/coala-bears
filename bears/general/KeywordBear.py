bfd61fb7a0c4456ce812a227f3b1962b2c727879
AbdealiJK <abdealikothari@gmail.com>
2016-09-03 15:41:11 +0530

    KeywordBear: Simplify and optimize using regex
    Earlier we looped over each keyword and over each file. We now use regex
    with the OR condition, so that just 1 regex parse over the whole file is
    needed.

--- bears/general/KeywordBear.py
+++ bears/general/KeywordBear.py
@@ -1,3 +1,5 @@
+import re
+
 from coalib.bearlib import deprecate_settings
 from coalib.bears.LocalBear import LocalBear
 from coalib.results.Result import RESULT_SEVERITY, Result
@@ -19,32 +21,19 @@ class KeywordBear(LocalBear):
             A list of keywords to search for (case insensitive).
             Usual examples are TODO and FIXME.
         '''
-        results = list()
-
-        for i in range(len(keywords)):
-            keywords[i] = keywords[i].lower()
+        keywords_regex = re.compile(
+            '(' + '|'.join(re.escape(key) for key in keywords) + ')',
+            re.IGNORECASE)
 
         for line_number, line in enumerate(file):
-            for keyword in keywords:
-                results += self.check_line_for_keyword(line.lower(),
-                                                       filename,
-                                                       line_number,
-                                                       keyword)
-
-        return results
-
-    def check_line_for_keyword(self, line, filename, line_number, keyword):
-        pos = line.find(keyword)
-        if pos != -1:
-            return [Result.from_values(
-                origin=self,
-                message="The line contains the keyword `{}`."
-                        .format(keyword),
-                file=filename,
-                line=line_number + 1,
-                column=pos + 1,
-                end_line=line_number + 1,
-                end_column=pos + len(keyword) + 1,
-                severity=RESULT_SEVERITY.INFO)]
-
-        return []
+            for keyword in keywords_regex.finditer(line):
+                yield Result.from_values(
+                    origin=self,
+                    message="The line contains the keyword '{}'."
+                            .format(keyword.group()),
+                    file=filename,
+                    line=line_number + 1,
+                    column=keyword.start() + 1,
+                    end_line=line_number + 1,
+                    end_column=keyword.end() + 1,
+                    severity=RESULT_SEVERITY.INFO)
