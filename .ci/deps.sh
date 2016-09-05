31aaabecb7ceff07ef1a811644c5bad3d990355d
Bence Nagy <bence@underyx.me>
2016-09-06 01:50:27 +0200

    ci: Cache Bear.setup_dependencies files
    Fixes https://github.com/coala-analyzer/coala-bears/issues/769

--- .ci/deps.sh
+++ .ci/deps.sh
@@ -105,3 +105,8 @@ fi
 # Tailor (Swift) commands
 curl -fsSL https://tailor.sh/install.sh | sed 's/read -r CONTINUE < \/dev\/tty/CONTINUE=y/' > install.sh
 sudo bash install.sh
+
+# making coala cache the dependencies downloaded upon first run
+echo '' > dummy
+coala-ci --bears CheckstyleBear --files dummy --no-config --bear-dirs bears || true
+coala-ci --bears ScalaLintBear --files dummy --no-config --bear-dirs bears || true
