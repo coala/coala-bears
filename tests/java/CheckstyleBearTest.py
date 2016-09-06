ddc96427382e07de727549142401ce1f043a9d3a
Bence Nagy <bence@underyx.me>
2016-09-06 02:42:46 +0200

    CheckstyleBearTest: Add missing tests

--- tests/java/CheckstyleBearTest.py
+++ tests/java/CheckstyleBearTest.py
@@ -1,18 +1,21 @@
 import os
+import pathlib
+from tempfile import NamedTemporaryFile
 from queue import Queue
 
-from bears.java.CheckstyleBear import CheckstyleBear
+from bears.java import CheckstyleBear
 from tests.BearTestHelper import generate_skip_decorator
 from tests.LocalBearTestHelper import LocalBearTestHelper
 from coalib.settings.Section import Section
+from coalib.settings.Setting import path, Setting
 
 
-@generate_skip_decorator(CheckstyleBear)
+@generate_skip_decorator(CheckstyleBear.CheckstyleBear)
 class CheckstyleBearTest(LocalBearTestHelper):
 
     def setUp(self):
         self.section = Section("test section")
-        self.uut = CheckstyleBear(self.section, Queue())
+        self.uut = CheckstyleBear.CheckstyleBear(self.section, Queue())
         test_files = os.path.join(os.path.dirname(__file__), "test_files")
         self.good_file = os.path.join(test_files, "CheckstyleGood.java")
         self.bad_file = os.path.join(test_files, "CheckstyleBad.java")
@@ -31,3 +34,20 @@ class CheckstyleBearTest(LocalBearTestHelper):
         self.section["checkstyle_configs"] = self.empty_config
         self.check_validity(self.uut, [], self.good_file)
         self.check_validity(self.uut, [], self.bad_file)
+
+
+def known_checkstyle_test(monkeypatch):
+    monkeypatch.setattr(CheckstyleBear, 'known_checkstyles', {'such': 'style'})
+    assert CheckstyleBear.known_checkstyle_or_path('such') == 'such'
+
+
+def known_path_test(monkeypatch):
+    monkeypatch.setattr(CheckstyleBear, 'known_checkstyles', {'such': 'style'})
+    with NamedTemporaryFile() as coafile, NamedTemporaryFile() as style_file:
+        coafile_path = pathlib.Path(coafile.name)
+        style_path = pathlib.Path(style_file.name)
+        setting = Setting(
+            'style_path', style_path.name, origin=str(coafile_path))
+        assert (
+            CheckstyleBear.known_checkstyle_or_path(setting) == str(style_path)
+        )
