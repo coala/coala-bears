358ca1cff07fbd641c97ecbcea576d231365c7f2
Bence Nagy <bence@underyx.me>
2016-09-09 18:35:25 +0200

    ci: Execute pytest tests parallelly

--- .ci/tests.sh
+++ .ci/tests.sh
@@ -6,7 +6,7 @@ source .ci/env_variables.sh
 args=()
 
 if [ "$system_os" == "LINUX" ] ; then
-  args+=('--cov' '--cov-fail-under=100' '--doctest-modules')
+  args+=('--cov' '--cov-fail-under=100' '--doctest-modules' '-n' '2')
 fi
 
 python3 -m pytest "${args[@]}"
