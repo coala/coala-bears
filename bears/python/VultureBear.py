4b626e75c5e1cca36eb664c6df007c60aac3d219
Adrianzatreanu <zgadi_2007@yahoo.com>
2016-10-10 17:59:43 +0300

    VultureBear: Make it a ``GlobalBear``
    Fixes https://github.com/coala/coala-bears/issues/817

--- bears/python/VultureBear.py
+++ bears/python/VultureBear.py
@@ -1,16 +1,12 @@
-from coalib.bearlib.abstractions.Linter import linter
+import re
+from shutil import which
+
+from coalib.bears.GlobalBear import GlobalBear
 from coalib.bears.requirements.PipRequirement import PipRequirement
+from coalib.misc.Shell import run_shell_command
 
 
-@linter(executable='vulture',
-        output_format='regex',
-        output_regex=r'(?P<filename>.*):(?P<line>.*):\s*(?P<message>.*)')
-class VultureBear:
-    """
-    Checks Python code for unused variables and functions using ``vulture``.
-
-    See <https://bitbucket.org/jendrikseipp/vulture> for more information.
-    """
+class VultureBear(GlobalBear):
     LANGUAGES = {"Python", "Python 3"}
     REQUIREMENTS = {PipRequirement('happiness', '0.10.0')}
     AUTHORS = {'The coala developers'}
@@ -19,6 +15,26 @@ class VultureBear:
     ASCIINEMA_URL = 'https://asciinema.org/a/82256'
     CAN_DETECT = {'Unused Code'}
 
-    @staticmethod
-    def create_arguments(filename, file, config_file):
-        return filename,
+    executable = "vulture"
+    arguments = (executable,)
+    output_regex = re.compile(
+        r'(?P<filename>.*):(?P<line>.*):\s*(?P<message>.*)')
+
+    @classmethod
+    def check_prerequisites(cls):  # pragma: no cover
+        if which("bash") is None:
+            return "bash is not installed."
+        if which("vulture") is None:
+            return ("Vulture is missing. Make sure to install it using "
+                    "``pip3 install happiness``.")
+        else:
+            return True
+
+    def run(self, filename):
+    """
+    Checks Python code for unused variables and functions using ``vulture``.
+
+    See <https://bitbucket.org/jendrikseipp/vulture> for more information.
+    """
+    self.arguments = self.executable + ' {filename}'
+    stdout_output, _ = run_shell_command(self.arguments)
