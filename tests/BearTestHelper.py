08984472855f9b0089496b2ca0cc54bc235bb473
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2016-11-21 17:22:48 +0100

    Deprecate coalib testing modules
    That's duplicate code. Completely. Highly dangerous, because people
    might start changing things in one place but not the other.
    
    All the imports are wrong :/ deprecating for now!

--- tests/BearTestHelper.py
+++ tests/BearTestHelper.py
@@ -1,16 +1,6 @@
-from unittest.case import skip, skipIf
+import logging
 
+logging.warning('This module is deprecated. Use '
+                '`coalib.testing.BearTestHelper` instead.')
 
-def generate_skip_decorator(bear):
-    """
-    Creates a skip decorator for a `unittest` module test from a bear.
-
-    `check_prerequisites` is used to determine a test skip.
-
-    :param bear: The bear whose prerequisites determine the test skip.
-    :return:     A decorator that skips the test if appropriate.
-    """
-    result = bear.check_prerequisites()
-
-    return (skip(result) if isinstance(result, str)
-            else skipIf(not result, '(No reason given.)'))
+from coalib.testing.BearTestHelper import *
