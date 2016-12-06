c71263bb1dfd4ab302c4086d137a114b836cda25
CormacGG <glee8412@pacificu.edu>
2016-12-05 18:30:43 -0800

    LocalBearTestHelper.py: Remove unused imports
    removed unused import
    "from coala.coalib.testing.LocalBearTestHelper.py import *"
    
    closes https://github.com/coala/coala-bears/issues/1057

--- tests/LocalBearTestHelper.py
+++ tests/LocalBearTestHelper.py
@@ -2,5 +2,3 @@ import logging
 
 logging.warning('This module is deprecated. Use '
                 '`coalib.testing.LocalBearTestHelper` instead.')
-
-from coalib.testing.LocalBearTestHelper import *
