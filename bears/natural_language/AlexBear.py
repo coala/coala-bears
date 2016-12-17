cd89a1d6cc63f0d02aaf954805ffaf350993d918
Cosmin <manolescucosmin112@gmail.com>
2016-12-17 12:46:25 +0200

    AlexBear.py: Change AlexBear docstring
    The docstring mentions that this bear
    only supports English.
    It also includes a link to Alex for
    instructions and tests.
    
    Closes https://github.com/coala/coala-bears/issues/1138

--- bears/natural_language/AlexBear.py
+++ bears/natural_language/AlexBear.py
@@ -14,6 +14,9 @@ class AlexBear:
     """
     Checks the markdown file with Alex - Catch insensitive, inconsiderate
     writing.
+
+    Be aware that Alex and this bear only work on English text.
+    For more information, consult <https://www.npmjs.com/package/alex>.
     """
     LANGUAGES = {'Natural Language'}
     REQUIREMENTS = {NpmRequirement('alex', '3')}
