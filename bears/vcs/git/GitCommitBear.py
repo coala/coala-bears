94a0a194f38c89b468e994bc4b0606d7947a3b41
Naveen Kumar Sangi <nkprince007@gmail.com>
2016-12-30 10:55:05 +0530

    GitCommitBear.py: Require issue ref in commit body
    By default, this ensures that the git commit
    contains a short reference to an issue.
    If keyword regex is chosen, it verifies
    the presence of matching expression.
    
    Fixes https://github.com/coala/coala-bears/issues/1112

--- bears/vcs/git/GitCommitBear.py
+++ bears/vcs/git/GitCommitBear.py
@@ -3,6 +3,8 @@ import re
 import shutil
 import os
 
+from urllib.parse import urlparse
+
 from coalib.bears.GlobalBear import GlobalBear
 from dependency_management.requirements.PipRequirement import PipRequirement
 from coala_utils.ContextManagers import change_directory
@@ -20,6 +22,12 @@ class GitCommitBear(GlobalBear):
     LICENSE = 'AGPL-3.0'
     ASCIINEMA_URL = 'https://asciinema.org/a/e146c9739ojhr8396wedsvf0d'
     CAN_DETECT = {'Formatting'}
+    SUPPORTED_KEYWORD_REGEX = {
+        'github': (r'[Cc]lose[sd]?|[Rr]esolve[sd]?'
+                   r'|[Ff]ix(?:e[sd])?\s+'),
+        'gitlab': (r'(?:[Cc]los(?:e[sd]?|ing)|[Rr]esolv(?:e[sd]?|ing)'
+                   r'|[Ff]ix(?:e[sd]|ing))?\s+')
+    }
 
     @classmethod
     def check_prerequisites(cls):
@@ -41,13 +49,39 @@ class GitCommitBear(GlobalBear):
             omit={'self', 'body'})
 
     @classmethod
+    def get_issue_checks_metadata(cls):
+        return FunctionMetadata.from_function(
+            cls.check_issue_reference,
+            omit={'self', 'body'})
+
+    @classmethod
     def get_metadata(cls):
         return FunctionMetadata.merge(
             FunctionMetadata.from_function(
                 cls.run,
                 omit={'self', 'dependency_results'}),
             cls.get_shortlog_checks_metadata(),
-            cls.get_body_checks_metadata())
+            cls.get_body_checks_metadata(),
+            cls.get_issue_checks_metadata())
+
+    @staticmethod
+    def get_host_from_remotes():
+        """
+        Retrieve the first host from the list of git remotes.
+        """
+        remotes, _ = run_shell_command(
+            "git config --get-regex '^remote.*.url$'")
+
+        remotes = [url.split()[-1] for url in remotes.splitlines()]
+        if len(remotes) == 0:
+            return None
+
+        url = remotes[0]
+        if 'git@' in url:
+            netloc = re.findall(r'@(\S+):', url)[0]
+        else:
+            netloc = urlparse(url)[1]
+        return netloc.split('.')[0]
 
     def run(self, allow_empty_commit_message: bool = False, **kwargs):
         """
@@ -79,6 +113,9 @@ class GitCommitBear(GlobalBear):
         yield from self.check_body(
             stdout[1:],
             **self.get_body_checks_metadata().filter_parameters(kwargs))
+        yield from self.check_issue_reference(
+            '\n'.join(stdout[1:]),
+            **self.get_issue_checks_metadata().filter_parameters(kwargs))
 
     def check_shortlog(self, shortlog,
                        shortlog_length: int=50,
@@ -203,3 +240,74 @@ class GitCommitBear(GlobalBear):
             yield Result(self, 'Body of HEAD commit contains too long lines. '
                                'Commit body lines should not exceed {} '
                                'characters.'.format(body_line_length))
+
+    def check_issue_reference(self, body,
+                              body_close_issue: bool=False,
+                              body_close_issue_full_url: bool=False,
+                              body_close_issue_on_last_line: bool=False):
+        """
+        Check for matching issue related references and URLs.
+
+        :param body:
+            Body of the commit message of HEAD.
+        :param body_close_issue:
+            Whether to check for the presence of issue reference within
+            the commit body by retrieving host information from git
+            configuration. GitHub and GitLab support auto closing issues with
+            commit messages. Checks for matching keywords in the commit body.
+            By default, if none of ``body_close_issue_full_url`` and
+            ``body_close_issue_on_last_line`` are enabled, checks for presence
+            of short references like ``closes #213``. Otherwise behaves
+            according to other chosen flags.
+            More on keywords follows.
+            [GitHub](https://help.github.com/articles/closing-issues-via-commit-messages/)
+            [GitLab](https://docs.gitlab.com/ce/user/project/issues/automatic_issue_closing.html)
+        :param body_close_issue_full_url:
+            Checks the presence of issue close reference with a full URL
+            related to some issue. Works along with ``body_close_issue``.
+        :param body_close_issue_on_last_line:
+            When enabled, checks for issue close reference presence on the
+            last line of the commit body. Works along with
+            ``body_close_issue``.
+        """
+        if not body_close_issue:
+            return
+
+        host = self.get_host_from_remotes()
+        if host not in self.SUPPORTED_KEYWORD_REGEX:
+            return
+
+        if body_close_issue_on_last_line:
+            body = body.splitlines()[-1]
+            result_message = ('Body of HEAD commit does not contain any {} '
+                              'reference in the last line.')
+        else:
+            result_message = ('Body of HEAD commit does not contain any {} '
+                              'reference.')
+
+        if body_close_issue_full_url:
+            result_message = result_message.format('full issue')
+            issue_keyword_regex = r'https?://{}\S+/issues/\w+'.format(
+                re.escape(host))
+        else:
+            result_message = result_message.format('issue')
+            issue_keyword_regex = r'#\w+'
+
+        concat_keywords = [',', 'and']
+        concat_regex = '|'.join(re.escape(kw) for kw in concat_keywords)
+        joint_regex = r'({1}(?:\s*(?:{0})\s*{1})*)'.format(concat_regex,
+                                                           issue_keyword_regex)
+
+        matches = re.findall(self.SUPPORTED_KEYWORD_REGEX[host] + joint_regex,
+                             body)
+
+        if len(matches) == 0:
+            yield Result(self, result_message)
+            return
+
+        issue_regex = re.compile(r'(?:#|\D+/)[1-9][0-9]*')
+        for match in matches:
+            for issue in re.split(r'\s*(?:{})\s*'.format(concat_regex), match):
+                if not issue_regex.fullmatch(issue):
+                    yield Result(self, 'Invalid issue number at '
+                                       '{}'.format(issue))
