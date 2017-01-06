0abe3d79905f5e99f66adf7fa5974dcfa116764e
Lilian Besson <Naereen@users.noreply.github.com>
2017-01-06 11:21:04 +0100

    Jinja2Bear: Improve documentation
    Related to https://github.com/coala/coala/issues/3488

--- bears/jinja2/Jinja2Bear.py
+++ bears/jinja2/Jinja2Bear.py
@@ -312,17 +312,18 @@ class Jinja2Bear(LocalBear):
             variable_spacing: int=1,
             control_spacing: int=1):
         """
-        Check Jinja2 templates for syntax, formatting and documentation issues.
+        Check `Jinja2 templates <http://jinja.pocoo.org>`_ for syntax,
+        formatting and documentation issues.
         The following aspects are being looked at:
 
         * Variable spacing:
             Variable tags should be padded with one space on each side, like
             this: ``{{ var_name }}``. This can be set to any number of spaces
-            via the setting variable_spacing. Malformatted variable tags are
-            detected and fixes suggested.
+            via the setting ``variable_spacing``.
+            Malformatted variable tags are detected and fixes suggested.
         * Control spacing:
-            Like variable spacing, but for control blocks, i.e. if and for
-            constructs. Looks at both start and end block.
+            Like variable spacing, but for control blocks, i.e. ``if`` and
+            ``for`` constructs. Looks at both start and end block.
         * Control labels:
             It is good practice to label the end of an ``if`` or ``for``
             construct with a comment equal to the content of the start,
