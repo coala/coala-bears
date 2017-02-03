346ac836493c9834eb5e4e89bea98482037bc60b
John Vandenberg <jayvdb@gmail.com>
2017-02-04 06:48:50 +0700

    RubySyntaxBear: Use DistributionRequirement
    RubySyntaxBear only depends on the ruby executable.
    
    Removes 'ruby' from Gemfile, added in 35c242b0.

--- bears/ruby/RubySyntaxBear.py
+++ bears/ruby/RubySyntaxBear.py
@@ -1,5 +1,7 @@
 from coalib.bearlib.abstractions.Linter import linter
 from dependency_management.requirements.GemRequirement import GemRequirement
+from dependency_management.requirements.DistributionRequirement import (
+    DistributionRequirement)
 
 
 @linter(executable='ruby',
@@ -14,7 +16,7 @@ class RubySyntaxBear:
     Checks the code with ``ruby -wc`` on each file separately.
     """
     LANGUAGES = {'Ruby'}
-    REQUIREMENTS = {GemRequirement('ruby', '2.1.5')}
+    REQUIREMENTS = {DistributionRequirement(apt_get='ruby')}
     AUTHORS = {'The coala developers'}
     AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
     LICENSE = 'AGPL-3.0'
