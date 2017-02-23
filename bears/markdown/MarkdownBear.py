f3411d16f16e35692726b7589a61d762e8d1b67d
John Vandenberg <jayvdb@gmail.com>
2017-02-24 05:29:40 +0700

    MarkdownBear: Restrict remark-lint to ~5
    Use remark-lint 5.4 if available.
    
    Fixes https://github.com/coala/coala-bears/issues/1444

--- bears/markdown/MarkdownBear.py
+++ bears/markdown/MarkdownBear.py
@@ -21,7 +21,7 @@ class MarkdownBear:
 
     LANGUAGES = {'Markdown'}
     REQUIREMENTS = {NpmRequirement('remark-cli', '2'),
-                    NpmRequirement('remark-lint', '>=5.1.0')}
+                    NpmRequirement('remark-lint', '5')}
     AUTHORS = {'The coala developers'}
     AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
     LICENSE = 'AGPL-3.0'
