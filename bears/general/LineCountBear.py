34f1be48490397b9d4e6781406e4b766c09ddc23
yash trivedi <yashtrivedi618@gmail.com>
2017-03-06 23:00:38 +0530

    LineCountBear.py: Add exclude_blank_lines option
    Add option to exclude blank lines. The setting is disabled by default.
    
    Closes https://github.com/coala/coala-bears/issues/1478

--- bears/general/LineCountBear.py
+++ bears/general/LineCountBear.py
@@ -1,3 +1,5 @@
+import re
+
 from coalib.bears.LocalBear import LocalBear
 from coalib.results.Result import Result
 from coalib.results.RESULT_SEVERITY import RESULT_SEVERITY
@@ -10,14 +12,24 @@ class LineCountBear(LocalBear):
     LICENSE = 'AGPL-3.0'
     CAN_DETECT = {'Formatting'}
 
-    def run(self, filename, file, max_lines_per_file: int):
+    def _get_blank_line_count(self, file):
+        num_blank_lines = len(
+            list(filter(lambda x: re.match(r'^\s*$', x), file)))
+        return num_blank_lines
+
+    def run(self, filename, file, max_lines_per_file: int,
+            exclude_blank_lines: bool=False):
         """
         Count the number of lines in a file and ensure that they are
         smaller than a given size.
 
         :param max_lines_per_file: Number of lines allowed per file.
+        :param exclude_blank_lines: "True" if blank lines are to be excluded.
         """
         file_length = len(file)
+        if exclude_blank_lines:
+            num_blank_lines = self._get_blank_line_count(file)
+            file_length = file_length - num_blank_lines
         if file_length > max_lines_per_file:
             yield Result.from_values(
                 origin=self,
--- tests/general/LineCountBearTest.py
+++ tests/general/LineCountBearTest.py
@@ -25,3 +25,18 @@ class LineCountBearTest(LocalBearTestHelper):
             filename='default')
         self.check_validity(self.uut, ['1 line'])
         self.check_validity(self.uut, [])  # Empty file
+
+    def test_exclude_blank_lines(self):
+        self.section.append(Setting('exclude_blank_lines', True))
+        self.section.append(Setting('max_lines_per_file', 2))
+        self.check_results(
+            self.uut, ['line 1', ' ', 'line 2',
+                       'line 3', '\n', '\t', ' line 4',
+                       'line 5 ', ' line 6 ', '\t\tline 7',
+                       '', '\t \n ', ' \t\n '],
+            [Result.from_values('LineCountBear',
+                                'This file had 7 lines, which is 5 lines more '
+                                'than the maximum limit specified.',
+                                severity=RESULT_SEVERITY.NORMAL,
+                                file='default')],
+            filename='default')
