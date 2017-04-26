f27f7ae0c6866b11ceba531342da88a2f6c77148
yash-nisar <yash.nisar@somaiya.edu>
2017-04-26 13:41:53 +0530

    RubySyntaxBear: Add ``CondaRequirement``
    Add ``CondaRequirement`` for the ``ruby`` package and add
    ``AnyOneOfRequirements`` to combine multiple requirements,
    returning ``true`` if any one of them is installed, both being
    supported by ``dependency_management`` 0.4.0
    
    Closes https://github.com/coala/coala-bears/issues/1656

--- bears/ruby/RubySyntaxBear.py
+++ bears/ruby/RubySyntaxBear.py
@@ -1,4 +1,8 @@
 from coalib.bearlib.abstractions.Linter import linter
+from dependency_management.requirements.AnyOneOfRequirements import (
+    AnyOneOfRequirements)
+from dependency_management.requirements.CondaRequirement import (
+    CondaRequirement)
 from dependency_management.requirements.DistributionRequirement import (
     DistributionRequirement)
 
@@ -15,7 +19,13 @@ class RubySyntaxBear:
     Checks the code with ``ruby -wc`` on each file separately.
     """
     LANGUAGES = {'Ruby'}
-    REQUIREMENTS = {DistributionRequirement('ruby')}
+    REQUIREMENTS = {
+        AnyOneOfRequirements(
+            [DistributionRequirement('ruby'),
+             CondaRequirement('ruby', '2.2.3', 'bioconda'),
+             ],
+        ),
+    }
     AUTHORS = {'The coala developers'}
     AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
     LICENSE = 'AGPL-3.0'
