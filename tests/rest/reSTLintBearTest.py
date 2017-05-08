1f357ad9f47a58907c1421897f1dc5bccb8490c1
Sanket Dasgupta <sanketdasgupta@gmail.com>
2017-05-09 04:19:12 +0530

    restLintBear: Ignore unknown errors
    Fixes https://github.com/coala/coala-bears/issues/1716

--- tests/rest/reSTLintBearTest.py
+++ tests/rest/reSTLintBearTest.py
@@ -4,7 +4,16 @@ from coalib.testing.LocalBearTestHelper import verify_local_bear
 good_file = 'test\n====\n'
 bad_file = 'test\n==\n'
 
+good_file2 = ':doc:`here <../Users/Tutorial>`'
+bad_file2 = 'test\n==\n'
 
 reSTLintBearTest = verify_local_bear(reSTLintBear,
                                      valid_files=(good_file,),
                                      invalid_files=(bad_file,))
+
+
+reSTLintBearSettingsTest = verify_local_bear(
+                                reSTLintBear,
+                                valid_files=(good_file2,),
+                                invalid_files=(bad_file2,),
+                                settings={'ignore_unknown_roles': True})
