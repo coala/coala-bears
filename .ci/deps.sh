b624e2261d647cdd6cff588c8cfe7dac9711d9c3
John Vandenberg <jayvdb@gmail.com>
2017-05-25 13:06:58 +0700

    .ci/deps.sh: Use infer 0.10.0

--- .ci/deps.sh
+++ .ci/deps.sh
@@ -149,16 +149,17 @@ julia -e "Pkg.add(\"Lint\")"
 sudo luarocks install luacheck --deps-mode=none
 
 # Infer commands
-if [ ! -e ~/infer-linux64-v0.7.0/infer/bin ]; then
-  wget -nc -O ~/infer.tar.xz https://github.com/facebook/infer/releases/download/v0.7.0/infer-linux64-v0.7.0.tar.xz
+if [ ! -e ~/infer-linux64-v0.10.0/infer/bin ]; then
+  wget -nc -O ~/infer.tar.xz https://github.com/facebook/infer/releases/download/v0.10.0/infer-linux64-v0.10.0.tar.xz
   tar xf ~/infer.tar.xz -C ~/
-  cd ~/infer-linux64-v0.7.0
+  cd ~/infer-linux64-v0.10.0
   opam init --y
   opam update
   opam pin add --yes --no-action atdgen 1.10.0
   opam pin add --yes --no-action reason 1.13.5
   opam pin add --yes --no-action infer .
   opam install --deps-only --yes infer
+  # Compile Infer
   ./build-infer.sh java
 fi
 
