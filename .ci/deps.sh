4aa4c9e0de7f2f240b59b6f7c297cc27cc8905ad
John Vandenberg <jayvdb@gmail.com>
2017-05-25 21:00:32 +0700

    .ci/deps.sh: Upgrade reason to latest 1.13.5
    Building infer 0.7.0 fails when building dependency reason 0.4.0.
    infer 0.7.0 requires =0.4.0, however InferBear works by forcing
    upgrading to 1.13.5
    
    Fixes https://github.com/coala/coala-bears/issues/1763

--- .ci/deps.sh
+++ .ci/deps.sh
@@ -156,6 +156,7 @@ if [ ! -e ~/infer-linux64-v0.7.0/infer/bin ]; then
   opam init --y
   opam update
   opam pin add --yes --no-action atdgen 1.10.0
+  opam pin add --yes --no-action reason 1.13.5
   opam pin add --yes --no-action infer .
   opam install --deps-only --yes infer
   ./build-infer.sh java
