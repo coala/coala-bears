59beb8b3434b3f0dc3e3b78aeb5d2138359b68e4
John Vandenberg <jayvdb@gmail.com>
2017-05-30 13:11:50 +0700

    VintBear: Deactivate bear
    The bear is broken, and it breaks all coala, due to
    its requirement chardet~=2.3.0 which is incompatible
    with requests adding requirement chardet~=3.0.2.
    
    Related to https://github.com/coala/coala-bears/issues/1785

--- .ci/tests.sh
+++ .ci/tests.sh
@@ -9,4 +9,10 @@ if [ "$system_os" == "LINUX" ] ; then
   args+=('--cov' '--cov-fail-under=100' '--doctest-modules')
 fi
 
+mv bears/vimscript/VintBear.py bears/vimscript/VintBear._py
+mv tests/vimscript/VintBearTest.py tests/vimscript/VintBearTest._py
+
 python3 -m pytest "${args[@]}"
+
+mv bears/vimscript/VintBear._py bears/vimscript/VintBear.py
+mv tests/vimscript/VintBearTest._py tests/vimscript/VintBearTest.py
