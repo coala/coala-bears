2126bb67f57acd7c17f947860abb566c07a5d8d6
Lukas Herman <lukas@localhost.localdomain>
2017-06-18 21:24:49 -0400

    WriteGoodLintBear.py: Change back ticks to double quotes
    This change fixes the confusion between words referring to standard
    prose and words referring not specific technical/informative term
    
    Closes https://github.com/coala/coala-bears/issues/1851

--- bears/natural_language/WriteGoodLintBear.py
+++ bears/natural_language/WriteGoodLintBear.py
@@ -11,7 +11,7 @@ from coala_utils.param_conversion import negate
         )
 class WriteGoodLintBear:
     """
-    Lints the text files using ``write-good`` for improving proses.
+    Lints the text files using "write-good" for improving proses.
 
     See <https://github.com/btford/write-good> for more information.
     """
@@ -42,20 +42,20 @@ class WriteGoodLintBear:
                          allow_extra_words: bool=True,
                          allow_cliche_phrases: bool=True):
         """
-        Using ``False`` will enable the check.
+        Using "False" will enable the check.
 
         :param allow_passive_voice:     Allows passive voice.
-        :param allow_so_beginning:      Allows ``So`` at the beginning of
+        :param allow_so_beginning:      Allows "So" at the beginning of
                                         the sentence.
         :param allow_adverbs:           Allows adverbs that can weaken the
-                                        meaning, such as: ``really``,
-                                        ``very``, ``extremely``, etc.
+                                        meaning, such as: "really",
+                                        "very", "extremely", etc.
         :param allow_repeated_words:    Allows lexical illusions – cases
                                         where a word is repeated.
-        :param allow_there_is:          Allows ``There is`` or ``There are``
+        :param allow_there_is:          Allows "There is" or "There are"
                                         at the beginning of the sentence.
-        :param allow_ambiguous_words:   Allows ``weasel words`` for example
-                                        ``often``, ``probably``
+        :param allow_ambiguous_words:   Allows "weasel words" for example
+                                        "often", "probably"
         :param allow_extra_words:       Allows wordy phrases and unnecessary
                                         words.
         :param allow_cliche_phrases:    Allows common cliche phrases in the
