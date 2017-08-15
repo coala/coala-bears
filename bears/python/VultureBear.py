58a39999a8cc6bf8cf1ce28c5c0745ce39bb8aa0
RJ722 <rahul722j@gmail.com>
2017-08-15 14:31:55 +0530

    VultureBear: Make use of the new vulture API
    The new vulture API provides better integration with coala. It
    natively supports `item.confidence`. Also, it gives precise
    information about the location of dead code (`first_line, `last_line`)

--- bears/python/VultureBear.py
+++ bears/python/VultureBear.py
@@ -3,42 +3,26 @@ from coalib.results.Result import Result
 from dependency_management.requirements.PipRequirement import PipRequirement
 from vulture import Vulture
 
-CONFIDENCE_MAP = {
-    'attribute': 70,
-    'class': 70,
-    'function': 70,
-    'import': 95,
-    'property': 70,
-    'variable': 70,
-}
-
 
 def _find_unused_code(filenames):
     """
     :param filenames: List of filenames to check.
     :return: Generator of Result objects.
     """
-
-    def file_lineno(item):
-        return (item.filename.lower(), item.lineno)
-
     vulture = Vulture()
     vulture.scavenge(filenames)
-    for item in sorted(
-            vulture.unused_funcs + vulture.unused_imports +
-            vulture.unused_props + vulture.unused_vars +
-            vulture.unused_attrs, key=file_lineno):
-        message = 'Unused {0}: {1}'.format(item.typ, item)
+    for item in vulture.get_unused_code():
         yield Result.from_values(origin='VultureBear',
-                                 message=message,
+                                 message=item.message,
                                  file=item.filename,
-                                 line=item.lineno,
-                                 confidence=CONFIDENCE_MAP[item.typ])
+                                 line=item.first_lineno,
+                                 end_line=item.last_lineno,
+                                 confidence=item.confidence)
 
 
 class VultureBear(GlobalBear):
     LANGUAGES = {'Python', 'Python 3'}
-    REQUIREMENTS = {PipRequirement('vulture', '0.14.0')}
+    REQUIREMENTS = {PipRequirement('vulture', '0.25.0')}
     AUTHORS = {'The coala developers'}
     AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
     LICENSE = 'AGPL-3.0'
