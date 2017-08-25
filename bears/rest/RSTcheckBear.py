801314affa51cb947123d02a39b3fb59df16cbfb
Umesh Singla <umeshksingla@gmail.com>
2017-08-26 04:16:07 +0530

    RSTcheckBear: Allow ignoring roles and directives
    Thanks @mridubhatnagar for helping.
    Closes https://github.com/coala/coala-bears/issues/1723

--- bears/rest/RSTcheckBear.py
+++ bears/rest/RSTcheckBear.py
@@ -1,6 +1,7 @@
 from coalib.bearlib.abstractions.Linter import linter
 from dependency_management.requirements.PipRequirement import PipRequirement
 from coalib.results.RESULT_SEVERITY import RESULT_SEVERITY
+from coalib.settings.Setting import typed_list
 
 
 @linter(executable='rstcheck',
@@ -32,16 +33,28 @@ class RSTcheckBear:
 
     @staticmethod
     def create_arguments(filename, file, config_file,
-                         code_block_language_ignore: list=()):
+                         code_block_language_ignore: list=(),
+                         directive_ignore: typed_list(str)=(),
+                         role_ignore: typed_list(str)=()):
         """
         :param code_block_language_ignore:
             Comma seperated list for languages of which code blocks
             should be ignored. Code block of following languages
             can be detected: ``bash``, ``c``, ``cpp``, ``json``,
             ``python``, ``rst``.
+        :param directive_ignore:
+             Comma separated list of directives to be ignored. Can be
+             used to ignore custom directives.
+        :param role_ignore:
+             Comma separated list of roles to be ignored. Can be used
+             to ignore custom roles.
         """
         args = ()
         if code_block_language_ignore:
             args = ('--ignore-language=' +
                     ','.join(code_block_language_ignore),)
+        if directive_ignore:
+            args += ('--ignore-directives=' + ','.join(directive_ignore),)
+        if role_ignore:
+            args += ('--ignore-roles=' + ','.join(role_ignore),)
         return args + (filename,)
