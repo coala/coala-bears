06bf60d80c98194d06f1c7531479e97895464b9e
aasiya13 <sitharapitiyage@gmail.com>
2017-09-28 15:28:10 +0530

    add remove function

--- tests/general/TextLintBearTest.py
+++ tests/general/TextLintBearTest.py
@@ -19,6 +19,10 @@ def load_testfile(name):
     return open(get_testfile_path(name)).readlines()
 
 
+def close_testfile(name):
+    load_testfile(name).close()
+
+
 @generate_skip_decorator(TextLintBear)
 class TextLintBearTest(LocalBearTestHelper):
 
