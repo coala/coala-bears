77c05d74489c409f0047bedfbc9d9f1cee5c3b0a
Swapnil Agarwal <swapnilagarwal@live.com>
2017-11-05 04:33:47 +0530

    InferBear: Add support for C language
    Bear falls back to Java if language is not supported.
    
    Closes https://github.com/coala/coala-bears/issues/541

--- bears/java/InferBear.py
+++ bears/java/InferBear.py
@@ -9,13 +9,23 @@ class InferBear:
     """
     Checks the code with ``infer``.
     """
-    LANGUAGES = {'Java'}
+    LANGUAGES = {'Java', 'C'}
     AUTHORS = {'The coala developers'}
     AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
     LICENSE = 'AGPL-3.0'
     ASCIINEMA_URL = 'https://asciinema.org/a/1g2k0la7xo5az9t8f1v5zy66q'
     CAN_DETECT = {'Security'}
 
-    @staticmethod
-    def create_arguments(filename, file, config_file):
-        return '-npb', '--', 'javac', filename
+    def create_arguments(self, filename, file, config_file, language: str):
+        """
+        :param language: The language to analyze (Java, or C).
+        """
+        language_args = {
+            'java': ('javac',),
+            'c': ('gcc', '-c')
+        }
+        if language.lower() not in language_args:
+            self.err('Invalid language given! Falling back to Java.')
+            language = 'java'
+
+        return ('-npb', '--') + language_args[language.lower()] + (filename,)
--- tests/java/InferBearTest.py
+++ tests/java/InferBearTest.py
@@ -2,7 +2,9 @@ from bears.java.InferBear import InferBear
 from coalib.testing.LocalBearTestHelper import verify_local_bear
 
 
-good_file = """
+# All examples taken from http://fbinfer.com/docs/hello-world.html
+
+good_java_file = """
 class InferGood {
     int test() {
         String s = null;
@@ -11,7 +13,7 @@ class InferGood {
 }
 """
 
-bad_file = """
+bad_java_file = """
 class InferBad {
     int test() {
         String s = null;
@@ -20,8 +22,38 @@ class InferBad {
 }
 """
 
+good_c_file = """
+#include <stdlib.h>
 
-InferBearTest = verify_local_bear(InferBear,
-                                  valid_files=(good_file,),
-                                  invalid_files=(bad_file,),
-                                  tempfile_kwargs={'suffix': '.java'})
+void test() {
+  int *s = NULL;
+  if (s != NULL) {
+    *s = 42;
+  }
+}
+"""
+
+bad_c_file = """
+#include <stdlib.h>
+
+void test() {
+  int *s = NULL;
+  *s = 42;
+}
+"""
+
+
+InferBearJavaTest = verify_local_bear(
+    InferBear, valid_files=(good_java_file,), invalid_files=(bad_java_file,),
+    tempfile_kwargs={'suffix': '.java'}, settings={'language': 'JAVA'})
+
+
+# InferBear falls back to java if no valid language is given
+InferBearFallbackLanguageTest = verify_local_bear(
+    InferBear, valid_files=(good_java_file,), invalid_files=(bad_java_file,),
+    tempfile_kwargs={'suffix': '.java'}, settings={'language': 'unicorn'})
+
+
+InferBearCTest = verify_local_bear(
+    InferBear, valid_files=(good_c_file,), invalid_files=(bad_c_file,),
+    tempfile_kwargs={'suffix': '.c'}, settings={'language': 'c'})
