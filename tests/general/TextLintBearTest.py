b8b2302cfe5776b0c94badf9ef23e53d1e11fe5a
akspr7 <akshatgarg15.11@gmail.com>
2017-11-24 23:53:26 +0530

    TextLintBearTest.py: Close test files properly

--- tests/general/TextLintBearTest.py
+++ tests/general/TextLintBearTest.py
@@ -16,7 +16,6 @@ def get_testfile_path(name):
 
 
 def load_testfile(name):
-    #return open(get_testfile_path(name)).readlines()
     with open(get_testfile_path(name)) as fl:
         return fl.readlines()
 
