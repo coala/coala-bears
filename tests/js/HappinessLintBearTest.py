28e0fd5ff877c8b8702b270f0eba9f1251e010a6
newbazz <krutamhathi14@gmail.com>
2017-11-30 23:39:01 +0530

    HappinessLintBear: Throw exception when use_spaces
    Throw error if use_space is true as tabbed
    identation is to be used.
    
    Closes https://github.com/coala/coala-bears/issues/1754

--- tests/js/HappinessLintBearTest.py
+++ tests/js/HappinessLintBearTest.py
@@ -1,5 +1,12 @@
+import re
+from queue import Queue
+
 from bears.js.HappinessLintBear import HappinessLintBear
 from coalib.testing.LocalBearTestHelper import verify_local_bear
+from coalib.settings.Section import Section
+from coalib.settings.Setting import Setting
+from coalib.testing.LocalBearTestHelper import LocalBearTestHelper
+from coalib.testing.BearTestHelper import generate_skip_decorator
 
 
 good_file = """
@@ -17,3 +24,18 @@ var x=2
 HappinessLintBearTest = verify_local_bear(HappinessLintBear,
                                           valid_files=(good_file,),
                                           invalid_files=(bad_file,))
+
+
+@generate_skip_decorator(HappinessLintBear)
+class HappinessLintBearConfigTest(LocalBearTestHelper):
+
+    USE_SPACE_ERR_MSG = re.escape('"use_spaces=True" is incompatible with '
+                                  'HappinessLintBear, set it to false.')
+
+    def test_validate_use_spaces(self):
+        section = Section('name')
+        section.append(Setting('use_spaces', True))
+        bear = HappinessLintBear(section, Queue())
+
+        with self.assertRaisesRegex(ValueError, self.USE_SPACE_ERR_MSG):
+            bear.run(bad_file, use_spaces=True)
