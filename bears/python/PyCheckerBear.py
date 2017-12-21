2537754cc34ccfb463fcecc1d287af080c5675b0
khanchi97 <sushilkhanchi97@gmail.com>
2017-12-21 17:18:19 +0530

    New Bear: PyCheckerBear
    A wrapper for ``pychecker`` to find bugs in Python files.
    
    Closes https://github.com/coala/coala-bears/issues/2151

---
+++ bears/python/PyCheckerBear.py
@@ -0,0 +1,41 @@
+import shlex
+
+from coalib.bearlib.abstractions.Linter import linter
+from dependency_management.requirements.DistributionRequirement import (
+        DistributionRequirement)
+
+
+@linter(executable='pychecker',
+        output_format='regex',
+        output_regex=r'(?P<filename>\w+\.py):(?P<line>\d+): '
+                     r'(?P<message>.*)')
+class PyCheckerBear:
+    """
+    Find bugs in your Python source code.
+
+    The code for each function, class, and method is checked for possible
+    problems. Checks for unused globals and locals(module or variable), unused
+    method arguments and using a variable before setting it or if you are
+    redefining a function/class/method in the same scope.
+    """
+    LANGUAGES = {'Python', 'Python 2'}
+    REQUIREMENTS = {DistributionRequirement(apt_get='pychecker')}
+    AUTHORS = {'The coala developers'}
+    AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
+    LICENSE = 'AGPL-3.0'
+    CAN_DETECT = {'Unused method/function arguments',
+                  'No doc string', 'Redefining'}
+    SEE_MORE = 'http://pychecker.sourceforge.net/'
+
+    @staticmethod
+    def create_arguments(filename, file, config_file,
+                         pychecker_cli_options: str=''):
+        """
+        :param pychecker_cli_options: Command line options you wish to be
+                                      passed to pychecker.
+        """
+        args = ()
+        if pychecker_cli_options:
+            args += tuple(shlex.split(pychecker_cli_options))
+
+        return args + (filename,)
---
+++ tests/python/pychecker_test_files/good_file.py
@@ -0,0 +1,5 @@
+def func(a, b, c):
+    return a+b+c
+
+
+result = func(1, 2, 3)
