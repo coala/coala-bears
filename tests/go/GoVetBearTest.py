e9c5031a6a36760d2540ac7ca3f6e8b647a937f3
manankalra <manankalra29@gmail.com>
2018-01-26 01:19:59 +0530

    GoVetBear: Implement all flags
    Implements all the flags supported by Vet.
    Flags can only be used with `tool vet` as
    `vet` is commonly used to examine packages.
    
    Closes https://github.com/coala/coala-bears/issues/358

--- tests/go/GoVetBearTest.py
+++ tests/go/GoVetBearTest.py
@@ -6,6 +6,7 @@ from unittest.case import skipIf
 from bears.go.GoVetBear import GoVetBear
 from coalib.testing.LocalBearTestHelper import LocalBearTestHelper
 from coalib.settings.Section import Section
+from coalib.settings.Setting import Setting
 
 
 @skipIf(which('go') is None, 'go is not installed')
@@ -22,5 +23,34 @@ class GoVetBearTest(LocalBearTestHelper):
                                      'vet_bad.go')
 
     def test_run(self):
+        self.check_validity(self.uut, [], self.good_file)
+        self.check_invalidity(self.uut, [], self.bad_file)
+
+    def test_args(self):
+        self.section.append(Setting('disallow_assembly_go_mismatches', True))
+        self.section.append(Setting('disallow_copying_locks', True))
+        self.section.append(Setting('disallow_unreachable_code', True))
+        self.check_validity(self.uut, [], self.good_file)
+        self.check_invalidity(self.uut, [], self.bad_file)
+
+    def test_all_args(self):
+        self.section.append(Setting('disallow_assembly_go_mismatches', True))
+        self.section.append(Setting('check_useless_assignments', True))
+        self.section.append(Setting('check_sync_package_mistakes', True))
+        self.section.append(Setting('check_boolean_operator_mistakes', True))
+        self.section.append(Setting('disallow_bad_build_tags', True))
+        self.section.append(Setting('disallow_cgo_pointer_violations', True))
+        self.section.append(Setting('disallow_unkeyed_composites', True))
+        self.section.append(Setting('disallow_copying_locks', True))
+        self.section.append(Setting('check_method_signatures', True))
+        self.section.append(Setting('disallow_nil_function_comparisons', True))
+        self.section.append(Setting('check_printf_calls', True))
+        self.section.append(Setting('check_range_loop_variables', True))
+        self.section.append(Setting('check_shadow_variables', True))
+        self.section.append(Setting('check_shifts', True))
+        self.section.append(Setting('check_struct_tag_format', True))
+        self.section.append(Setting('disallow_unreachable_code', True))
+        self.section.append(Setting('disallow_unsafe_pointers', True))
+        self.section.append(Setting('disallow_unused_results', True))
         self.check_validity(self.uut, [], self.good_file)
         self.check_invalidity(self.uut, [], self.bad_file)
