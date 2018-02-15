45de414b58f87a247ddab69ea569f81aaaae86a5
MacBox7 <ajankit2304@gmail.com>
2018-02-15 12:30:03 +0530

    PEP8Bear.py: Add asciinema
    Closes https://github.com/coala/coala-bears/issues/873

--- bears/python/PEP8Bear.py
+++ bears/python/PEP8Bear.py
@@ -16,6 +16,7 @@ class PEP8Bear(LocalBear):
     AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
     LICENSE = 'AGPL-3.0'
     CAN_FIX = {'Formatting'}
+    ASCIINEMA_URL = 'https://asciinema.org/a/165394'
 
     @deprecate_settings(indent_size='tab_width')
     def run(self, filename, file,
