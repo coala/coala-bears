549c06cb5e8cec4f577550da21b5dfa5f2e56c35
Harsh Kumar Bhartiya <ilikegamess@gmail.com>
2018-03-07 01:36:16 +0530

    .circleci/config.yml: Update CircleCI Syntax
    This updates CircleCI syntax from 1.0 to 2.0 and fixes uncached
    build failures due to deps.opam.sh
    
    Closes https://github.com/coala/coala-bears/issues/2298
    Closes https://github.com/coala/coala-bears/issues/2006

--- .ci/deps.pip.sh
+++ .ci/deps.pip.sh
@@ -4,17 +4,17 @@ set -x
 TERM=dumb
 
 # Choose the python versions to install deps for
-case $CIRCLE_NODE_INDEX in
- 0) dep_versions=( "3.4.3" "3.5.1" ) ;;
- 1) dep_versions=( "3.4.3" ) ;;
- -1) dep_versions=( ) ;;  # set by .travis.yml
- *) dep_versions=( "3.5.1" ) ;;
+case $CIRCLE_JOB in
+  "python-3.5") dep_versions=( "3.4.3" "3.5.1" );;
+  "python-3.4") dep_versions=( "3.4.3" );;
+  *)  dep_versions=( "3.5.1" );;
 esac
+
+pyenv install -ks 2.7.10
 
 for dep_version in "${dep_versions[@]}" ; do
   pyenv install -ks $dep_version
   pyenv local $dep_version 2.7.10
-  python --version
   source .ci/env_variables.sh
 
   pip install pip==9.0.1
