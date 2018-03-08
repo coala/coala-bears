4aad1ae6216cddef5d61644c114543842abd3f75
rohilla21 <kritirohilla567@gmail.com>
2018-03-08 11:28:04 +0530

    CMakeLintBear: Implement filter parameter
    Closes https://github.com/coala/coala-bears/issues/326

--- bears/cmake/CMakeLintBear.py
+++ bears/cmake/CMakeLintBear.py
@@ -1,6 +1,7 @@
 from coalib.bearlib.abstractions.Linter import linter
 from dependency_management.requirements.PipRequirement import PipRequirement
 from coalib.settings.Setting import path
+from coalib.settings.Setting import typed_list
 
 
 @linter(executable='cmakelint',
@@ -21,12 +22,17 @@ class CMakeLintBear:
 
     @staticmethod
     def create_arguments(filename, file, config_file,
+                         filters: typed_list(str) = [],
                          cmakelint_config: path = '',
                          ):
         """
+        :param filters: The filter to be applied on cmake file.
         :param cmakelint_config: The location of the cmakelintrc config file.
         """
         args = ()
         if cmakelint_config:
             args += ('--config=' + cmakelint_config,)
+        if filters:
+            args += ('--filter=' + ','.join(filters),)
+
         return args + (filename,)
