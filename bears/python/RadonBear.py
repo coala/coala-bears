8343eeab7a940a4b9a90cd9986dd9cfeeefdd550
MacBox7 <ajankit2304@gmail.com>
2018-03-12 12:45:01 +0530

    RadonBear.py: Change bears settings
    Radon specific settings are marked
    deprecated when used and cyclomatic_complexity
    setting is added which is generic to
    other complexity analysis bears.
    
    Closes https://github.com/coala/coala-bears/issues/772

--- bears/python/RadonBear.py
+++ bears/python/RadonBear.py
@@ -1,3 +1,4 @@
+import logging
 import radon.complexity
 import radon.visitors
 
@@ -18,6 +19,7 @@ class RadonBear(LocalBear):
     CAN_DETECT = {'Complexity'}
 
     def run(self, filename, file,
+            cyclomatic_complexity: int = None,
             radon_ranks_info: typed_list(str) = (),
             radon_ranks_normal: typed_list(str) = ('C', 'D'),
             radon_ranks_major: typed_list(str) = ('E', 'F'),
@@ -25,32 +27,48 @@ class RadonBear(LocalBear):
         """
         Uses radon to compute complexity of a given file.
 
-        :param radon_ranks_info:   The ranks (given by radon) to
-                                   treat as severity INFO.
-        :param radon_ranks_normal: The ranks (given by radon) to
-                                   treat as severity NORMAL.
-        :param radon_ranks_major:  The ranks (given by radon) to
-                                   treat as severity MAJOR.
+        :param cyclomatic_complexity: Maximum cyclomatic complexity
+                                      that is considered to be normal.
+        :param radon_ranks_info:      The ranks (given by radon) to
+                                      treat as severity INFO.
+        :param radon_ranks_normal:    The ranks (given by radon) to
+                                      treat as severity NORMAL.
+        :param radon_ranks_major:     The ranks (given by radon) to
+                                      treat as severity MAJOR.
         """
         severity_map = {
             RESULT_SEVERITY.INFO: radon_ranks_info,
             RESULT_SEVERITY.NORMAL: radon_ranks_normal,
             RESULT_SEVERITY.MAJOR: radon_ranks_major
         }
+
+        if cyclomatic_complexity is None:
+            logging.warning('The settings `radon_ranks_info`, '
+                            '`radon_ranks_normal` and `radon_ranks_major`'
+                            ' are deprecated. Please use '
+                            '`cyclomatic_complexity` instead.')
+
         for visitor in radon.complexity.cc_visit(''.join(file)):
             rank = radon.complexity.cc_rank(visitor.complexity)
             severity = None
             for result_severity, rank_list in severity_map.items():
                 if rank in rank_list:
                     severity = result_severity
-            if severity is None:
+
+            if (cyclomatic_complexity is None and
+                    severity is None):
                 continue
+            elif (cyclomatic_complexity and
+                    visitor.complexity <= cyclomatic_complexity):
+                severity = RESULT_SEVERITY.INFO
+            else:
+                severity = RESULT_SEVERITY.MAJOR
 
             col = visitor.col_offset if visitor.col_offset else None
             visitor_range = SourceRange.from_values(
                 filename, visitor.lineno, col, visitor.endline)
             message = '{} has a cyclomatic complexity of {}'.format(
-                visitor.name, rank)
+                visitor.name, visitor.complexity)
 
             yield Result(self, message, severity=severity,
                          affected_code=(visitor_range,))
--- tests/python/RadonBearTest.py
+++ tests/python/RadonBearTest.py
@@ -1,4 +1,10 @@
+from queue import Queue
+
+from coalib.results.Result import Result
+from coalib.results.RESULT_SEVERITY import RESULT_SEVERITY
+from coalib.settings.Section import Section
 from bears.python.RadonBear import RadonBear
+from coalib.testing.LocalBearTestHelper import LocalBearTestHelper
 from coalib.testing.LocalBearTestHelper import verify_local_bear
 
 test_file1 = """
@@ -15,13 +21,14 @@ class class1():
 test_file3 = 'def f():\n' + ('    assert True\n' * 50)
 
 
-RadonBearDefaultsTest = verify_local_bear(
+# Tests for deprecated settings
+RadonBearRanksDefaultsTest = verify_local_bear(
     RadonBear,
     valid_files=(test_file1, test_file2),
     invalid_files=(test_file3,))
 
 
-RadonBearNoReportsTest = verify_local_bear(
+RadonBearRanksNoReportsTest = verify_local_bear(
     RadonBear,
     valid_files=(test_file1, test_file2, test_file3),
     invalid_files=(),
@@ -30,10 +37,44 @@ RadonBearNoReportsTest = verify_local_bear(
               'radon_ranks_major': ''})
 
 
-RadonBearReportsTest = verify_local_bear(
+RadonBearRanksReportsTest = verify_local_bear(
     RadonBear,
     valid_files=(),
     invalid_files=(test_file1, test_file2),
     settings={'radon_ranks_info': '',
               'radon_ranks_normal': 'A',
               'radon_ranks_major': ''})
+
+
+# Tests for new settings
+class RadonBearTest(LocalBearTestHelper):
+    def setUp(self):
+        self.section = Section('test')
+        self.uut = RadonBear(self.section, Queue())
+
+    def test_cyclomatic_complexity(self):
+        # Test for info results
+        self.check_results(
+            self.uut,
+            test_file3.splitlines(True),
+            [Result.from_values('RadonBear',
+                                'f has a cyclomatic complexity of 51',
+                                severity=RESULT_SEVERITY.INFO,
+                                file='test_file3',
+                                line=1,
+                                end_line=1)],
+            filename='test_file3',
+            settings={'cyclomatic_complexity': 52})
+
+        # Test for major results
+        self.check_results(
+            self.uut,
+            test_file3.splitlines(True),
+            [Result.from_values('RadonBear',
+                                'f has a cyclomatic complexity of 51',
+                                severity=RESULT_SEVERITY.MAJOR,
+                                file='test_file3',
+                                line=1,
+                                end_line=1)],
+            filename='test_file3',
+            settings={'cyclomatic_complexity': 10})
