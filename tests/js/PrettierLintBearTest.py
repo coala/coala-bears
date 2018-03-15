b97087cd40d5515ff75289afbef8f923ce1e467e
Saksham Bansal <sakshambansal1999@yahoo.com>
2018-03-15 20:18:18 +0530

    PrettierLintBear.py Add more tests
    Closes https://github.com/coala/coala-bears/issues/2290

--- tests/js/PrettierLintBearTest.py
+++ tests/js/PrettierLintBearTest.py
@@ -14,6 +14,38 @@ foo(reallyLongArg(), omgSoManyParameters(), IShouldRefactorThis(), isThereSeriou
 """
 # Stop ignoring
 
+bad_semicolon = """
+var x;
+"""
+
+good_quotes = """const a = () => "Foo bar";
+
+function b(object, key) {
+  return object["key"];
+}"""
+
+bad_quotes = """const a = () => 'Foo bar';"""
+
+bad_parentheses = """function b(){
+"""
+
+good_parentheses = """function b() {}
+"""
+
+bad_indent = """if (x) {
+x = 0
+}"""
+
+good_indent = """if (x) {
+  x = 0;
+}"""
+
 PrettierLintBear = verify_local_bear(PrettierLintBear,
-                                     valid_files=(good_file,),
-                                     invalid_files=(bad_file,))
+                                     valid_files=(good_quotes,
+                                                  good_file,
+                                                  good_parentheses,
+                                                  good_indent,),
+                                     invalid_files=(bad_file,
+                                                    bad_quotes,
+                                                    bad_parentheses,
+                                                    bad_indent,))
