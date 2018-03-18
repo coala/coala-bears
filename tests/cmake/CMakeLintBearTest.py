a3bb382a82d01b3a873e120645a139bfa296b5e8
rohilla21 <kritirohilla567@gmail.com>
2018-03-18 22:09:40 +0530

    CMakeLintBear: Implement line length
    Add infinite line length feature when
    max_line_length is zero.
    
    Closes https://github.com/coala/coala-bears/issues/2329

--- tests/cmake/CMakeLintBearTest.py
+++ tests/cmake/CMakeLintBearTest.py
@@ -8,6 +8,15 @@ good_file = 'project(FooBar C)\nset(VERSION 0)\n'
 
 bad_file_mixes_case = 'ProJeCt(FooBar C)\nseT(VERSION 0)\n'
 
+good_file_line_length = ('configure_file\n' +
+                         '("${PROJECT_SOURCE_DIR}/configuration.h.in"\n' +
+                         '"${PROJECT_BINARY_DIR}/configuration.h"\n)'
+                         )
+
+bad_file_line_length = ('configure_file' +
+                        '("${PROJECT_SOURCE_DIR}/configuration.h.in"' +
+                        '"${PROJECT_BINARY_DIR}/configuration.h" )')
+
 
 conf_file = os.path.join(os.path.dirname(__file__),
                          'test_files',
@@ -27,3 +36,21 @@ CMakeLintBearConfigTest = verify_local_bear(
     invalid_files=(),
     tempfile_kwargs={'suffix': '.cmake'},
     settings={'cmakelint_config': conf_file})
+
+
+CMakeLintBearInfiniteLineLengthTest = verify_local_bear(
+    CMakeLintBear,
+    valid_files=(good_file_line_length,),
+    invalid_files=(bad_file_line_length,),
+    tempfile_kwargs={'suffix': '.cmake'},
+    settings={'max_line_length': '50',
+              'cmakelint_config': conf_file})
+
+
+CMakeLintBearInfiniteLineLengthTest = verify_local_bear(
+    CMakeLintBear,
+    valid_files=(good_file_line_length, bad_file_line_length),
+    invalid_files=(),
+    tempfile_kwargs={'suffix': '.cmake'},
+    settings={'max_line_length': '0',
+              'cmakelint_config': conf_file})
