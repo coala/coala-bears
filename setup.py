afe0be4c70860e30a71fa9046f3ba8c052ab6a87
John Vandenberg <jayvdb@gmail.com>
2018-05-04 07:29:12 +0700

    setup.py: Sync moban
    Related to https://gitlab.com/coala/mobans/merge_requests/3

--- setup.py
+++ setup.py
@@ -21,11 +21,24 @@ VERSION = '0.12.0.dev99999999999999'
 
 
 class PyTestCommand(TestCommand):
+    """
+    From https://pytest.org/latest/goodpractices.html
+    """
+    user_options = [('pytest-args=', 'a', 'Arguments to pass to py.test')]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
 
     def run_tests(self):
         # import here, cause outside the eggs aren't loaded
         import pytest
-        errno = pytest.main([])
+        errno = pytest.main(self.pytest_args)
         sys.exit(errno)
 
 
