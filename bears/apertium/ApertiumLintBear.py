b922f0c702eed18af87fdae2345aacfa9a7182de
John Vandenberg <jayvdb@gmail.com>
2018-05-27 00:22:57 +0700

    lxml 4.2.1

--- bears/apertium/ApertiumLintBear.py
+++ bears/apertium/ApertiumLintBear.py
@@ -26,7 +26,7 @@ class ApertiumLintBear:
     """
     LANGUAGES = {'Apertium'}
     REQUIREMENTS = {PipRequirement('apertium-lint', '0.29'),
-                    PipRequirement('lxml', '==3.6.0')}
+                    PipRequirement('lxml', '==4.2.1')}
     AUTHORS = {'The coala developers'}
     AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
     LICENSE = 'AGPL-3.0'
