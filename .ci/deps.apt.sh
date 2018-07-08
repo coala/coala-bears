e20fc3ce028930613fe7dff026c35b60c40a8563
John Vandenberg <jayvdb@gmail.com>
2018-07-08 19:10:03 +0700

    CI: Install flawfinder to ~/.local/bin
    Removes unnecessary use of sudo.
    
    Related to https://github.com/coala/coala-bears/issues/2592

--- .ci/deps.apt.sh
+++ .ci/deps.apt.sh
@@ -69,4 +69,8 @@ if [[ "$CIRCLE_BUILD_IMAGE" == "ubuntu-14.04" ]]; then
 fi
 
 # Change environment for flawfinder from python to python2
-sudo sed -i '1s/.*/#!\/usr\/bin\/env python2/' /usr/bin/flawfinder
+if [ ! -e ~/.local/bin/flawfinder ]; then
+  cp /usr/bin/flawfinder ~/.local/bin/flawfinder
+  sed -i '1s/.*/#!\/usr\/bin\/env python2/' ~/.local/bin/flawfinder
+  chmod +x ~/.local/bin/flawfinder
+fi
