d63fee103e772d1f8c359f4e3409d61c31e18405
sangamcse <ksangam68@gmail.com>
2018-07-08 21:09:52 +0530

    RubyFastererBear.py: Add `fasterer`
    This adds a `Local Bear` for `Ruby`, wrapping
    `fasterer`.
    
    Closes https://github.com/coala/coala-bears/issues/444

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem "brakeman", "~>4.1.1", require: false
 gem "csvlint", "~>0.4.0", require: false
+gem "fasterer", "~>0.4.1", require: false
 gem "haml_lint", "~>0.27.0", require: false
 gem "puppet-lint", "~>2.1.1", require: false
 gem "reek", "~>4.6", require: false
---
+++ bears/ruby/RubyFastererBear.py
@@ -0,0 +1,27 @@
+from coalib.bearlib.abstractions.Linter import linter
+from dependency_management.requirements.GemRequirement import GemRequirement
+
+
+@linter(executable='fasterer',
+        output_format='regex',
+        output_regex=r'(?P<message>.*\.).*:\s(?P<line>\d+)')
+class RubyFastererBear:
+    """
+    The ``RubyFastererBear`` will suggest some speed improvements which you
+    can check in details at the <https://github.com/JuanitoFatas/fast-ruby>.
+
+    It uses ``fasterer``. See <https://www.rubydoc.info/gems/fasterer/0.4.1>
+    for more info.
+    """
+
+    LANGUAGES = {'Ruby'}
+    REQUIREMENTS = {GemRequirement('fasterer', '0.4.1')}
+    AUTHORS = {'The coala developers'}
+    AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
+    LICENSE = 'AGPL-3.0'
+    CAN_DETECT = {'Complexity'}
+    SEE_MORE = 'https://github.com/DamirSvrtan/fasterer'
+
+    @staticmethod
+    def create_arguments(filename, file, config_file):
+        return filename,
