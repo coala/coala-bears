cc1243d162e1e2c360f06726e7ae835cf8bf74f5
John Vandenberg <jayvdb@gmail.com>
2018-07-08 21:05:30 +0700

    CI: Use luarocks install --local
    Avoids using sudo.
    
    Related to https://github.com/coala/coala-bears/issues/2592

--- .ci/deps.sh
+++ .ci/deps.sh
@@ -46,7 +46,7 @@ fi
 julia -e "Pkg.add(\"Lint\")"
 
 # Lua commands
-sudo luarocks install luacheck --deps-mode=none
+luarocks install --local --deps-mode=none luacheck
 
 # PHPMD installation
 if [ ! -e ~/.local/bin/phpmd ]; then
