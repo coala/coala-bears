5afe24b0fa9529d338ba7630d99f8a0e4b74c50a
pareksha <pareksha.manchanda@gmail.com>
2018-07-17 10:05:46 +0530

    PEP8Bear: Migrate bear to aspects
    This migrates `PEP8Bear` to its respective
    aspects and tastes.
    
    Closes https://github.com/coala/coala-bears/issues/2653

--- tests/python/PEP8BearTest.py
+++ tests/python/PEP8BearTest.py
@@ -4,13 +4,16 @@ from bears.python.PEP8Bear import PEP8Bear
 from coalib.testing.LocalBearTestHelper import LocalBearTestHelper
 from coalib.settings.Section import Section
 from coalib.settings.Setting import Setting
+from coalib.bearlib.aspects import (
+    AspectList,
+    get as get_aspect,
+)
 
 
 class PEP8BearTest(LocalBearTestHelper):
 
     def setUp(self):
         self.section = Section('name')
-        self.section.append(Setting('max_line_length', '79'))
         self.uut = PEP8Bear(self.section, Queue())
 
     def test_valid(self):
@@ -50,3 +53,18 @@ class PEP8BearTest(LocalBearTestHelper):
     def test_invalid(self):
         self.check_invalidity(self.uut, [''])
         self.check_invalidity(self.uut, ['a=1+1'])
+
+    def test_aspects(self):
+        self.section.aspects = AspectList([
+            get_aspect('LineLength')('Python', max_line_length=10),
+        ])
+        self.check_validity(self.uut, ['a = 1 + 1'])
+        self.check_invalidity(self.uut, ['a = 1 + 1 + 1 + 1 + 1 + 1 + 1'])
+
+    def test_settings_over_aspects(self):
+        self.section.aspects = AspectList([
+            get_aspect('LineLength')('Python', max_line_length=50),
+        ])
+        self.section.append(Setting('max_line_length', '10'))
+        self.check_validity(self.uut, ['a = 1 + 1'])
+        self.check_invalidity(self.uut, ['a = 1 + 1 + 1 + 1 + 1 + 1 + 1'])
