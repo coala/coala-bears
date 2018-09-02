2eaf2a028bb06b501f202def4ace3170be41292f
John Vandenberg <jayvdb@gmail.com>
2018-09-02 11:16:11 +0700

    TSLintBearTest.py: Support tslint 3-5
    tslint 5 requires a configuration file, so
    provide a different config file for each test.
    
    Related to https://github.com/coala/coala-bears/issues/1072

--- tests/typescript/TSLintBearTest.py
+++ tests/typescript/TSLintBearTest.py
@@ -19,19 +19,27 @@ let t = findTitle('mytitle');
 t.innerHTML = 'New title';
 """
 
-tslintconfig = os.path.join(os.path.dirname(__file__),
-                            'test_files',
+tslint_config_dir = os.path.join(os.path.dirname(__file__), 'test_files')
+
+tslintconfig = os.path.join(tslint_config_dir,
                             'tslint.json')
 
-TSLintBearWithoutConfigTest = verify_local_bear(
+tslintconfig_single = os.path.join(tslint_config_dir,
+                                   'tslint-single.json')
+
+TSLintDoubleQuotesTest = verify_local_bear(
     TSLintBear,
     valid_files=(good_file,),
     invalid_files=(bad_file,),
+    settings={
+        'tslint_config': tslintconfig,
+        'rules_dir': tslint_config_dir,
+    },
     tempfile_kwargs={'suffix': '.ts'})
 
-TSLintBearWithConfigTest = verify_local_bear(
+TSLintSingleQuotesTest = verify_local_bear(
     TSLintBear,
     valid_files=(bad_file,),
     invalid_files=(good_file,),
-    settings={'tslint_config': tslintconfig},
+    settings={'tslint_config': tslintconfig_single},
     tempfile_kwargs={'suffix': '.ts'})
