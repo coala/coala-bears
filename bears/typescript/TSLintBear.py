5b981463f03ae98d86ecedd01f6a2912707aeb01
John Vandenberg <jayvdb@gmail.com>
2018-09-02 11:16:11 +0700

    TSLintBear.py: Support tslint 3-5
    Updates the tslint dependency to be any version
    from v3 to v5, as the tests now work on them all.
    
    Closes https://github.com/coala/coala-bears/issues/1072

--- bears/typescript/TSLintBear.py
+++ bears/typescript/TSLintBear.py
@@ -17,7 +17,7 @@ class TSLintBear:
     """
 
     LANGUAGES = {'TypeScript'}
-    REQUIREMENTS = {NpmRequirement('tslint', '3'),
+    REQUIREMENTS = {NpmRequirement('tslint', '>=3.0 <6.0'),
                     NpmRequirement('typescript', '>=1.7.3')}
     AUTHORS = {'The coala developers'}
     AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
