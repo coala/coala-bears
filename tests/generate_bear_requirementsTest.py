3185d2771320a4966e6404c33a7a0f43e2aab0e2
John Vandenberg <jayvdb@gmail.com>
2018-11-07 18:42:25 +0700

    Reorder and chdir back

--- tests/generate_bear_requirementsTest.py
+++ tests/generate_bear_requirementsTest.py
@@ -5,6 +5,15 @@ from generate_bear_requirements import main
 
 class RunTest(unittest.TestCase):
 
-    def test_run(self):
+
+    def setUp(self):
+        self.orig_dir = os.getcwd()
+
+    def test_dependency_yaml_check(self):
         rv = main(['--update', '--check'])
         self.assertEqual(rv, None)
+
+    def tearDown(self):
+        shutil.rmtree(os.path.join(self.BEARS_UPLOAD_PATH))
+
+        os.chdir(self.orig_dir)
