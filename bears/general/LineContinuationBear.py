0b20f85be35b75a22ccac4b35756bb563a2c5d08
abhishalya <abhinavkaushlya@gmail.com>
2018-11-08 12:12:04 +0530

    LineContinuationBear: Add line continuation bear
    Closes https://github.com/coala/coala-bears/issues/2427

---
+++ bears/general/LineContinuationBear.py
@@ -0,0 +1,49 @@
+import logging
+
+from coalib.bears.LocalBear import LocalBear
+from coalib.results.Result import Result
+from coalib.bearlib.languages.Language import Language, UnknownLanguageError
+
+
+class LineContinuationBear(LocalBear):
+    def run(self,
+            filename,
+            file,
+            language,
+            ):
+        """
+        It is a generic bear which will look for any lines which
+        end with the explicit use of line continuation operators,
+        which is considered as a bad practice. The solution
+        would be to use implicit type of line continuation methods.
+
+        Note that this requires the coalang to know the tokens
+        used for line continuation and hence any language not
+        having the line continuation attribute will result in an error.
+
+        :param filename:
+            Name of the file that needs to be checked.
+        :param file:
+            File that needs to be checked in the form of a list of strings.
+        :param language:
+            Language to be used for finding the line continuation tokens.
+        """
+        try:
+            lang = Language[language]
+            line_continuation = lang.get_default_version().line_continuation
+        except (UnknownLanguageError, AttributeError):
+            logging.error('Language {} is not yet supported.'.format(language))
+            return
+
+        for line_number, line in enumerate(file):
+            if len(line) > 1:
+                if line[-2] in line_continuation:
+                    yield Result.from_values(
+                        origin=self,
+                        message='Explicit line continuation is not allowed.',
+                        file=filename,
+                        line=line_number + 1,
+                        column=len(line) - 1,
+                        end_line=line_number + 1,
+                        end_column=len(line),
+                        )
---
+++ tests/general/LineContinuationBearTest.py
@@ -0,0 +1,46 @@
+import logging
+from queue import Queue
+
+from bears.general.LineContinuationBear import LineContinuationBear
+from coalib.testing.LocalBearTestHelper import LocalBearTestHelper
+from coalib.testing.LocalBearTestHelper import execute_bear
+from coalib.results.Result import Result
+from coalib.settings.Section import Section
+from coalib.settings.Setting import Setting
+
+
+good_file = """
+a = some_function(
+    '1' + '2')
+"""
+
+
+class LineContinuationBearTest(LocalBearTestHelper):
+
+    def setUp(self):
+        self.section = Section('name')
+        self.uut = LineContinuationBear(self.section, Queue())
+
+    def test_good_file(self):
+        self.section.append(Setting('language', 'Python'))
+        self.check_validity(self.uut, good_file.splitlines())
+
+    def test_bad_file(self):
+        self.section.append(Setting('language', 'Python'))
+        self.check_results(
+            self.uut, ['a = 1 + \\', '2'],
+            [Result.from_values('LineContinuationBear',
+                                'Explicit line continuation is not allowed.',
+                                line=1, column=9, end_line=1, end_column=10,
+                                file='default')],
+            filename='default')
+
+    def test_lang_exception(self):
+        self.section.append(Setting('language', 'BlaBlaBla'))
+        ERROR_MESSAGE = 'ERROR:root:Language BlaBlaBla is not yet supported.'
+        logger = logging.getLogger()
+
+        with self.assertLogs(logger, 'ERROR') as log:
+            with execute_bear(self.uut, filename='F', file='') as result:
+                self.assertEqual(len(log.output), 1)
+                self.assertEqual(log.output[0], ERROR_MESSAGE)
