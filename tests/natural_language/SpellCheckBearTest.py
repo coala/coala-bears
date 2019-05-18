616771d016ce32975c3121f83f7feb24376f667c
John Vandenberg <jayvdb@gmail.com>
2019-05-18 12:06:47 +0700

    SpellCheckBearTest: Enable on Windows

--- tests/natural_language/SpellCheckBearTest.py
+++ tests/natural_language/SpellCheckBearTest.py
@@ -1,6 +1,3 @@
-import platform
-import unittest
-
 from bears.natural_language.SpellCheckBear import SpellCheckBear
 from coalib.testing.LocalBearTestHelper import verify_local_bear
 
@@ -9,9 +6,6 @@ good_file = 'This is correct spelling.'
 bad_file = 'tihs si surly som incoreclt speling.'
 
 
-SpellCheckLintBearTest = unittest.skipIf(
-    platform.system() == 'Windows',
-    "SpellCheckBear doesn't work on windows")(
-        verify_local_bear(SpellCheckBear,
-                          valid_files=(good_file,),
-                          invalid_files=(bad_file,)))
+SpellCheckLintBearTest = verify_local_bear(SpellCheckBear,
+                                           valid_files=(good_file,),
+                                           invalid_files=(bad_file,))
