9cbfdc5f9cfeb5207fe4b5b830629fc867b8db75
John Vandenberg <jayvdb@gmail.com>
2019-07-16 13:08:02 +0700

    __init__.py: Allow Python 3.4.2

--- bears/__init__.py
+++ bears/__init__.py
@@ -19,8 +19,8 @@ __version__ = VERSION
 
 
 def assert_supported_version():
-    if sys.version_info < (3, 4, 4):
-        print('coala supports only python 3.4.4 or later.')
+    if sys.version_info < (3, 4, 2):
+        print('coala supports only python 3.4.2 or later.')
         exit(4)
 
 
