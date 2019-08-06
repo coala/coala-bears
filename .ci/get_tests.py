4a8cc74f1a4daabf929994ea818c7de88ec85a2a
John Vandenberg <jayvdb@gmail.com>
2019-08-06 07:37:33 +0700

    get_tests.py: Disable one test on Travis Windows
    XMLBearDTDUrlTest.test_valid_files is consistently failing only
    on Travis Windows, but not AppVeyor.
    
    Related to https://github.com/coala/coala-bears/issues/2944

--- .ci/get_tests.py
+++ .ci/get_tests.py
@@ -139,6 +139,12 @@ def get_pytest_deselected_tests(args, tests):
     if 'tests/php/PHPMessDetectorBearTest.py' in tests:
         not_list.append('test_cleancode_violation')
 
+    # https://github.com/coala/coala-bears/issues/2944
+    if 'tests/xml2/XMLBearTest.py' in tests:
+        if 'win' in args and os.environ.get('TRAVIS'):
+            not_list.append(
+                'tests/xml2/XMLBearTest.py::XMLBearDTDUrlTest::test_valid_files')
+
     return not_list
 
 
